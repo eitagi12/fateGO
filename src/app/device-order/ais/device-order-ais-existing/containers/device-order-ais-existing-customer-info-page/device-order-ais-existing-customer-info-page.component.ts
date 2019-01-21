6a10332c1b9ce080fb31db04e7125b8b16684d14
kawinpop <kawinp96@wintl.postbox.in.th>
2019-01-21 15:17:14 +0700

    [Update] component

--- src/app/device-order/ais/device-order-ais-existing/containers/device-order-ais-existing-customer-info-page/device-order-ais-existing-customer-info-page.component.ts
+++ src/app/device-order/ais/device-order-ais-existing/containers/device-order-ais-existing-customer-info-page/device-order-ais-existing-customer-info-page.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Transaction, Customer } from 'src/app/shared/models/transaction.model';
+import { CustomerInfo, HomeService } from 'mychannel-shared-libs';
+import { Router } from '@angular/router';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
+import {
+  ROUTE_DEVICE_ORDER_AIS_EXISTING_VALIDATE_CUSTOMER_ID_CARD_PAGE,
+  ROUTE_DEVICE_ORDER_AIS_EXISTING_ELIGIBLE_MOBILE_PAGE
+} from '../../constants/route-path.constant';
+import { WIZARD_DEVICE_ORDER_AIS } from 'src/app/device-order/constants/wizard.constant';
+
 
 @Component({
   selector: 'app-device-order-ais-existing-customer-info-page',
@@ -7,9 +17,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeviceOrderAisExistingCustomerInfoPageComponent implements OnInit {
 
-  constructor() { }
+  wizards = WIZARD_DEVICE_ORDER_AIS;
+
+  transaction: Transaction;
+  customerInfo: CustomerInfo;
+
+  constructor(
+    private router: Router,
+    private homeService: HomeService,
+    private transactionService: TransactionService,
+  ) {
+    this.transaction = this.transactionService.load();
+  }
 
   ngOnInit() {
+    const customer: Customer = this.transaction.data.customer;
+    this.customerInfo = {
+      titleName: customer.titleName,
+      firstName: customer.firstName,
+      lastName: customer.lastName,
+      idCardNo: customer.idCardNo,
+      idCardType: 'บัตรประชาชน',
+      birthdate: customer.birthdate,
+      mobileNo: customer.mainMobile,
+    };
+  }
+
+  onBack() {
+    this.router.navigate([ROUTE_DEVICE_ORDER_AIS_EXISTING_VALIDATE_CUSTOMER_ID_CARD_PAGE]);
+  }
+
+  onNext() {
+    this.router.navigate([ROUTE_DEVICE_ORDER_AIS_EXISTING_ELIGIBLE_MOBILE_PAGE]);
+  }
+
+  onHome() {
+    this.homeService.goToHome();
+  }
+
+  ngOnDestroy(): void {
+    this.transactionService.update(this.transaction);
   }
 
 }
