aa6dd48a5524d86fe7f4d9b665e4ae603b5cffd5
meths418 <meths418@saraf.postbox.in.th>
2019-01-31 18:57:18 +0700

    edit filter is cash back

--- src/app/buy-product/pipes/privilege-to-trade-slider.pipe.ts
+++ src/app/buy-product/pipes/privilege-to-trade-slider.pipe.ts
@@ -11,10 +11,12 @@ export class PrivilegeToTradeSliderPipe implements PipeTransform {
     }
     return (privilege.trades || []).map((trade: any) => {
       const banks = (trade.banks || []).length > 0;
+      const filterIsCashBack = banks
+        ? trade.banks.filter(bank => bank.remark !== '' && bank.remark !== null && bank.remark) : [];
       return {
         description: banks ? 'ผ่อนชำระค่าเครื่อง' : 'ชำระเต็มจำนวน',
         installmentType: banks ? 'wallet' : 'bath',
-        isCashBack: true,
+        isCashBack: filterIsCashBack.length > 0 || false,
         value: trade
       };
     });
