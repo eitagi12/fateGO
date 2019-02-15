85a803b0592325b5539e14165e93e0678f8a29fa
kawinpop <kawinp96@wintl.postbox.in.th>
2019-02-15 14:04:35 +0700

    Update

--- src/app/device-order/ais/device-order-ais-new-register/containers/device-order-ais-new-register-perso-sim-page/device-order-ais-new-register-perso-sim-page.component.ts
+++ src/app/device-order/ais/device-order-ais-new-register/containers/device-order-ais-new-register-perso-sim-page/device-order-ais-new-register-perso-sim-page.component.ts
@@ -9,7 +9,6 @@ import { environment } from 'src/environments/environment';
 import { WIZARD_DEVICE_ORDER_AIS } from '../../../../constants/wizard.constant';
 import {
   ROUTE_DEVICE_ORDER_AIS_NEW_REGISTER_BY_PATTERN_PAGE,
-  ROUTE_DEVICE_ORDER_AIS_NEW_REGISTER_EAPPLICATION_PAGE,
   ROUTE_DEVICE_ORDER_AIS_NEW_REGISTER_AGREEMENT_SIGN_PAGE,
   ROUTE_DEVICE_ORDER_AIS_NEW_REGISTER_FACE_CAPTURE_PAGE
 } from '../../constants/route-path.constant';
@@ -36,13 +35,13 @@ export class DeviceOrderAisNewRegisterPersoSimPageComponent implements OnInit, O
   persoSim: any;
 
   transaction: Transaction;
-  isNext: boolean;
   option: OptionPersoSim;
   persoSimConfig: PersoSimConfig;
   shoppingCart: ShoppingCart;
 
   koiskApiFn: any;
   readonly ERROR_PERSO = 'ไม่สามารถให้บริการได้ กรุณาติดต่อพนักงานเพื่อดำเนินการ ขออภัยในความไม่สะดวก';
+  readonly ERROR_PERSO_PC = 'ไม่สามารถ Perso Sim ได้';
 
   constructor(
     private router: Router,
@@ -54,55 +53,29 @@ export class DeviceOrderAisNewRegisterPersoSimPageComponent implements OnInit, O
     private persoSimService: PersoSimService,
     private shoppingCartService: ShoppingCartService,
   ) {
-    if (this.tokenService.getUser().channelType === ChannelType.SMART_ORDER) {
-      this.persoSimKoisk();
-    } else {
-      this.persoSimWebsocket();
-    }
-
+    this.option = { scan_sim: true, key_sim: false };
+    this.shoppingCart = this.shoppingCartService.getShoppingCartData();
+    this.transaction = this.transactionService.load();
   }
 
   ngOnInit() {
-    this.option = {
-      scan_sim: true,
-      key_sim: false
-    };
-    this.shoppingCart = this.shoppingCartService.getShoppingCartData();
-    this.transaction = this.transactionService.load();
     if (this.transaction.data.simCard.mobileNo) {
-      this.persoSimConfig = {
-        serviceGetPrivateKey: this.http.post('/api/customerportal/newRegister/getPrivateKeyCommand', {}).toPromise(),
-        serviceGetPersoDataCommand: (serialNo: string, indexNo: string)  => {
-          return this.http.get('/api/customerportal/newRegister/queryPersoData', {
-            params: {
-              indexNo: indexNo,
-              serialNo: serialNo,
-              mobileNo: this.transaction.data.simCard.mobileNo,
-              simService: 'Normal',
-              sourceSystem: 'MC-KIOSK'
-            }
-          }).toPromise();
-        },
-        serviceCreateOrderPersoSim: (refNo: string) => {
-          return this.http.get('/api/customerportal/newRegister/createPersoSim', {
-            params: {
-              refNo: refNo
-            }
-          }).toPromise();
-        },
-        serviceCheckOrderPersoSim: (refNo: string) => {
-          return this.http.get('/api/customerportal/newRegister/checkOrderStatus', {
-            params: {
-              refNo: refNo
-            }
-          }).toPromise();
+      this.setConfigPersoSim().then(() => {
+        if (this.tokenService.getUser().channelType === ChannelType.SMART_ORDER) {
+          this.persoSimKoisk();
+        } else {
+          this.persoSimWebsocket();
         }
-        };
+      });
+    } else {
+      this.router.navigate([ROUTE_DEVICE_ORDER_AIS_NEW_REGISTER_BY_PATTERN_PAGE]);
     }
   }
 
+
   persoSimKoisk() {
     this.title = 'กำลังเตรียมซิมใหม่';
+
     this.koiskApiFn = this.kioskApi();
     this.koiskApiFn.connect().then(() => {
       this.koiskApiFn.loadSim().then(() => {
@@ -122,6 +95,7 @@ export class DeviceOrderAisNewRegisterPersoSimPageComponent implements OnInit, O
               }
             });
           }
+
           if (persoSim.error) {
             console.error(persoSim.error);
             this.errorMessage = this.ERROR_PERSO;
@@ -130,14 +104,11 @@ export class DeviceOrderAisNewRegisterPersoSimPageComponent implements OnInit, O
           }
         });
       }).catch((err) => {
-        // โหลด sim ไม่ได้
         this.errorMessage = this.ERROR_PERSO;
         this.alertService.error(this.ERROR_PERSO);
         console.error(err);
       });
-    })
-    .catch((err) => {
-      //  ต่อ web socket ไม่ได้
+    }).catch((err) => {
       this.errorMessage = this.ERROR_PERSO;
       this.alertService.error(this.ERROR_PERSO);
       console.error(err);
@@ -162,6 +133,42 @@ export class DeviceOrderAisNewRegisterPersoSimPageComponent implements OnInit, O
       }
     });
  }
+
+
+ async setConfigPersoSim() {
+  return this.persoSimConfig = await {
+    serviceGetPrivateKey: () => {
+      return this.http.post('/api/customerportal/newRegister/getPrivateKeyCommand', {
+        params: { }
+      }).toPromise();
+    },
+    serviceGetPersoDataCommand: (serialNo: string, indexNo: string)  => {
+      return this.http.get('/api/customerportal/newRegister/queryPersoData', {
+        params: {
+          indexNo: indexNo,
+          serialNo: serialNo,
+          mobileNo: this.transaction.data.simCard.mobileNo,
+          simService: 'Normal',
+          sourceSystem: 'MC-KIOSK'
+        }
+      }).toPromise();
+    },
+    serviceCreateOrderPersoSim: (refNo: string) => {
+      return this.http.get('/api/customerportal/newRegister/createPersoSim', {
+        params: {
+          refNo: refNo
+        }
+      }).toPromise();
+    },
+    serviceCheckOrderPersoSim: (refNo: string) => {
+      return this.http.get('/api/customerportal/newRegister/checkOrderStatus', {
+        params: {
+          refNo: refNo
+        }
+      }).toPromise();
+    }
+    };
+}
 
  kioskApi() {
   if (!WebSocket) {
