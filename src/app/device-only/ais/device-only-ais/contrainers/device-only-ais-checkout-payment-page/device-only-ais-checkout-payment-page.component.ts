8c73588f1d3283fc20c63feebc722ac9f60aa1d5
passk505 <passk505@aparr.postbox.in.th>
2019-03-06 15:19:42 +0700

    เพิ่มหน้า _checkout_payment

--- src/app/device-only/ais/device-only-ais/contrainers/device-only-ais-checkout-payment-page/device-only-ais-checkout-payment-page.component.ts
+++ src/app/device-only/ais/device-only-ais/contrainers/device-only-ais-checkout-payment-page/device-only-ais-checkout-payment-page.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { Router } from '@angular/router';
+import { HomeService, Aggregate } from 'mychannel-shared-libs';
+import { ROUTE_DEVICE_ONLY_AIS_SUMMARY_PAGE, ROUTE_DEVICE_ONLY_AIS_QR_CODE_QUEUE_PAGE } from '../../constants/route-path.constant';
 
 @Component({
   selector: 'app-device-only-ais-checkout-payment-page',
   templateUrl: './device-only-ais-checkout-payment-page.component.html',
   styleUrls: ['./device-only-ais-checkout-payment-page.component.scss']
 })
-export class DeviceOnlyAisCheckoutPaymentPageComponent implements OnInit {
+export class DeviceOnlyAisCheckoutPaymentPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private homeService: HomeService,
+  ) { }
 
   ngOnInit() {
   }
 
+onBack() {
+  this.router.navigate([ROUTE_DEVICE_ONLY_AIS_SUMMARY_PAGE]);
+  
+  }
+
+  onNext() {
+    // QR code
+    // this.router.navigate([ROUTE_DEVICE_ORDER_AIS_NEW_REGISTER_QUEUE_PAGE]);
+    this.router.navigate([ROUTE_DEVICE_ONLY_AIS_QR_CODE_QUEUE_PAGE]);
+    // this.router.navigate([ROUTE_DEVICE_ONLY_AIS_QR_CODE_SUMMARY_PAGE]);
+    // รอหน้ามา
+  }
+
+  onHome() {
+    this.homeService.goToHome();
+  }
+
+  ngOnDestroy(): void {
+    // this.transactionService.update(this.transaction);
+  }
+
+  // summary(amount: number[]) {
+  //   return amount.reduce((prev, curr) => {
+  //     return prev + curr;
+  //   }, 0);
+  // }
 }
+
