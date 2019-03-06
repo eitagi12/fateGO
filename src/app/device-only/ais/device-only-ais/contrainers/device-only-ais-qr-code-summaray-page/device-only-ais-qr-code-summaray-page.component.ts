101469a658b33982d5945aabdce82cad1f2792b9
ice <niyana_ice@hotmail.com>
2019-03-06 13:32:43 +0700

    [ADD] creat device-only-ais-qr-code-summary-page

---
+++ src/app/device-only/ais/device-only-ais/contrainers/device-only-ais-qr-code-summaray-page/device-only-ais-qr-code-summaray-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-device-only-ais-qr-code-summaray-page',
+  templateUrl: './device-only-ais-qr-code-summaray-page.component.html',
+  styleUrls: ['./device-only-ais-qr-code-summaray-page.component.scss']
+})
+export class DeviceOnlyAisQrCodeSummarayPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
