5e52f98fe699f4e1677fce7d87faab07aa779d1c
Santi <santi@odds.team>
2019-03-06 14:25:19 +0700

    [ADD] router module testing

--- src/app/device-only/device-only.component.spec.ts
+++ src/app/device-only/device-only.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { DeviceOnlyComponent } from './device-only.component';
 
@@ -8,7 +9,12 @@ describe('DeviceOnlyComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DeviceOnlyComponent ]
+      declarations: [
+        DeviceOnlyComponent
+      ],
+      imports: [
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
