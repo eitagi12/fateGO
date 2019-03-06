ee70b4a5dac69e7bef5cffb6aa6604d150b09faf
Santi <santi@odds.team>
2019-03-06 14:25:34 +0700

    [ADD] custom elements schema

--- src/app/device-only/ais/device-only-ais/contrainers/device-only-ais-queue-page/device-only-ais-queue-page.component.spec.ts
+++ src/app/device-only/ais/device-only-ais/contrainers/device-only-ais-queue-page/device-only-ais-queue-page.component.spec.ts
@@ -1,3 +1,5 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeviceOnlyAisQueuePageComponent } from './device-only-ais-queue-page.component';
@@ -8,7 +10,10 @@ describe('DeviceOnlyAisQueuePageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DeviceOnlyAisQueuePageComponent ]
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      declarations: [
+        DeviceOnlyAisQueuePageComponent
+      ]
     })
     .compileComponents();
   }));
