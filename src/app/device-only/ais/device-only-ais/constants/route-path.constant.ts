dd6001c610a2b9edc9ea5904dc917bd8292c163d
tanyarat tanhin <tanyat96@wintl.postbox.in.th>
2019-03-07 11:43:23 +0700

    ADD ui qr generate

--- src/app/device-only/ais/device-only-ais/constants/route-path.constant.ts
+++ src/app/device-only/ais/device-only-ais/constants/route-path.constant.ts
@@ -6,3 +6,4 @@ export const ROUTE_DEVICE_ONLY_AIS_QR_CODE_QUEUE_PAGE = '/device-only/ais/qr-cod
 export const ROUTE_DEVICE_ONLY_AIS_SUMMARY_PAGE = '/device-only/ais/summary';
 export const ROUTE_DEVICE_ONLY_AIS_MOBILE_CARE_AVALIABLE_PAGE = '/device-only/ais/mobile-care-avaliable';
 export const ROUTE_DEVICE_ONLY_AIS_QR_CODE_SUMMARY_PAGE = '/device-only/ais/qr-code-summary';
+export const ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE = '/device-only/ais/qr-code-generate';
--- src/app/device-only/ais/device-only-ais/containers/device-only-ais-qr-code-summaray-page/device-only-ais-qr-code-summaray-page.component.ts
+++ src/app/device-only/ais/device-only-ais/containers/device-only-ais-qr-code-summaray-page/device-only-ais-qr-code-summaray-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE } from '../../constants/route-path.constant';
 @Component({
   selector: 'app-device-only-ais-qr-code-summaray-page',
   templateUrl: './device-only-ais-qr-code-summaray-page.component.html',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeviceOnlyAisQrCodeSummarayPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) {
+  }
 
   ngOnInit() {
   }
 
+  onNext() {
+    this.router.navigate([ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE]);
+  }
 }
