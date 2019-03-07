b48de40a3f4a71051f2015d8789bc65ba73dc131
ice <niyana_ice@hotmail.com>
2019-03-07 16:33:33 +0700

    [ADD]qr code summary

--- src/app/device-only/ais/device-only-ais/containers/device-only-ais-qr-code-summaray-page/device-only-ais-qr-code-summaray-page.component.ts
+++ src/app/device-only/ais/device-only-ais/containers/device-only-ais-qr-code-summaray-page/device-only-ais-qr-code-summaray-page.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE } from '../../constants/route-path.constant';
+import { ROUTE_DEVICE_ONLY_AIS_CHECKOUT_PAYMENT_PAGE, ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE } from '../../constants/route-path.constant';
+import { WIZARD_DEVICE_ONLY_MOBILE_CARE } from '../../constants/wizard.constant';
+
 @Component({
   selector: 'app-device-only-ais-qr-code-summaray-page',
   templateUrl: './device-only-ais-qr-code-summaray-page.component.html',
   styleUrls: ['./device-only-ais-qr-code-summaray-page.component.scss']
 })
 export class DeviceOnlyAisQrCodeSummarayPageComponent implements OnInit {
+  router: any;
+  homeService: any;
 
-  constructor(
-    private router: Router
-  ) {
-  }
+  constructor() { }
 
   ngOnInit() {
+  }
+
+  onBack() {
+    this.router.navigate([ROUTE_DEVICE_ONLY_AIS_CHECKOUT_PAYMENT_PAGE]);
   }
 
   onNext() {
     this.router.navigate([ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE]);
   }
+
+  onHome() {
+    this.homeService.goToHome();
+  }
+
 }
