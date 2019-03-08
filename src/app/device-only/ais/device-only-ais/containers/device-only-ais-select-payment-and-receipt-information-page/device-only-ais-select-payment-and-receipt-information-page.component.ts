580aac0f68fa53b497f4d47d4d60ac69ced729ba
Thitaporn <ammy.2410@gmail.com>
2019-03-08 15:55:57 +0700

    [edit]merge conflict

--- src/app/device-only/ais/device-only-ais/containers/device-only-ais-select-payment-and-receipt-information-page/device-only-ais-select-payment-and-receipt-information-page.component.ts
+++ src/app/device-only/ais/device-only-ais/containers/device-only-ais-select-payment-and-receipt-information-page/device-only-ais-select-payment-and-receipt-information-page.component.ts
@@ -1,7 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { WIZARD_DEVICE_ONLY_AIS } from '../../constants/wizard.constant';
-import { ShoppingCart, PaymentDetail, PaymentDetailQRCode, PaymentDetailBank, SelectPaymentDetail, PaymentDetailInstallment, PaymentDetailOption } from '../../../../../../../node_modules/mychannel-shared-libs';
-import { ReceiptInfo } from '../../components/receipt-information/receipt-information.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+export interface SelectPaymentDetail {
+  paymentType?: string; // 'qrcode' | 'credit' | 'debit';
+  qrCode?: PaymentDetailQRCode;
+  bank?: PaymentDetailBank;
+}
+export interface PaymentDetailBank {
+  abb: string;
+  name: string;
+  imageUrl: string;
+  promotion?: string;
+  installments: PaymentDetailInstallment[];
+  remark?: string;
+}
+export interface PaymentDetailInstallment {
+  installmentPercentage: number;
+  installmentMonth: number;
+}
+export interface PaymentDetail {
+  title?: string;
+  header?: string;
+  price?: string;
+  headerAdvancePay?: string;
+  priceAdvancePay?: string;
+  specialAmountPercent?: string;
+  specialAmountBaht?: string;
+  qrCodes?: PaymentDetailQRCode[];
+  banks?: PaymentDetailBank[];
+  installments?: PaymentDetailInstallment[];
+}
+export interface PaymentDetailQRCode {
+  id: number;
+  name: string;
+  imageUrl: string;
+  qrType: string;
+}
+export interface PaymentDetailOption {
+  isInstallment: boolean;
+  isEnable: boolean;
+}
 @Component({
   selector: 'app-device-only-ais-select-payment-and-receipt-information-page',
   templateUrl: './device-only-ais-select-payment-and-receipt-information-page.component.html',
@@ -10,30 +50,17 @@ import { ReceiptInfo } from '../../components/receipt-information/receipt-inform
 export class DeviceOnlyAisSelectPaymentAndReceiptInformationPageComponent implements OnInit {
 
   wizards = WIZARD_DEVICE_ONLY_AIS;
-  shoppingCart: ShoppingCart;
+  paymentForm: FormGroup;
   paymentDetail: PaymentDetail;
-  receiptInfo: ReceiptInfo;
   selectPaymentDetail: SelectPaymentDetail = {};
   paymentDetailOption: PaymentDetailOption;
-  isSuccess: boolean;
-  constructor() { }
+  constructor(
+   private fb: FormBuilder,
+  ) { }
 
   ngOnInit() {
-    this.shoppingCart = {
-      fullName: 'นาย ธีระยุทธ เจโตวิมุติพงศ์',
-      mobileNo: '0889540584',
-      campaignName: 'AIS Hot Deal',
-      commercialName: 'APPLE',
-      qty: 1,
-      price: 20780
-    };
-    this.receiptInfo = {
-      taxId: 'XXXXXXXXX9249',
-      branch: 'Telewiz',
-      buyer: 'นาย เทส เทส',
-      buyerAddress: 'พลหโยธิน 19 แขวงบางซื่อ เขตบางซื่อ กรุงเทพฯ',
-      telNo: ''
-    };
+    this.createForm();
+
     this.paymentDetail = {
       title: 'รูปแบบชำระเงิน',
       header: 'ค่าเครื่อง IPHONEX64 สี BLACK',
@@ -64,6 +91,12 @@ export class DeviceOnlyAisSelectPaymentAndReceiptInformationPageComponent implem
     };
   }
 
+  createForm() {
+    this.paymentForm = this.fb.group({
+      paymentType: [null, Validators.required]
+    });
+  }
+
   getQRCodeData(): PaymentDetailQRCode[] {
     return [
       {
@@ -81,171 +114,157 @@ export class DeviceOnlyAisSelectPaymentAndReceiptInformationPageComponent implem
     ];
   }
 
-getBankData(): PaymentDetailBank[] {
-  return [
-    {
-      abb: 'CITIREADY',
-      imageUrl: 'https://10.104.249.89/CPC-FE-WEB/api/contents/upload/CITIREADY_citibank02.jpg',
-      installments: [
-        {
-          installmentPercentage: 0,
-          installmentMonth: 3
-        },
-        {
-          installmentPercentage: 0,
-          installmentMonth: 6
-        },
-        {
-          installmentPercentage: 0,
-          installmentMonth: 12
-        },
-      ],
-      name: 'ซิตี้แบงก์ เรดดี้เครดิต',
-      remark: null,
-    },
-    {
-      abb: 'CITI',
-      imageUrl: 'https://10.104.249.89/CPC-FE-WEB/api/contents/upload/CITI_CITI.png',
-      installments: [
-        {
-          installmentPercentage: 0,
-          installmentMonth: 3
-        },
-        {
-          installmentPercentage: 0,
-          installmentMonth: 6
-        },
-        {
-          installmentPercentage: 0,
-          installmentMonth: 12
-        },
-      ],
-      name: 'ซิตี้แบงก์',
-      remark: null,
-    },
-    {
-      abb: 'SCB',
-      imageUrl: 'https://10.104.249.89/CPC-FE-WEB/api/contents/upload/SCB_SCB02.png',
-      installments: [
-        {
-          installmentPercentage: 0,
-          installmentMonth: 3
-        },
-        {
-          installmentPercentage: 0,
-          installmentMonth: 6
-        },
-        {
-          installmentPercentage: 0,
-          installmentMonth: 12
-        },
-      ],
-      name: 'ไทยพาณิชย์ จำกัด (มหาชน)',
-      remark: null,
-    },
-    {
-      abb: 'KBNK',
-      imageUrl: 'https://10.104.249.89/CPC-FE-WEB/api/contents/upload/KBNK_kBank02.png',
-      installments: [
-        {
-          installmentPercentage: 0,
-          installmentMonth: 3
-        },
-        {
-          installmentPercentage: 0,
-          installmentMonth: 6
-        },
-        {
-          installmentPercentage: 0,
-          installmentMonth: 12
-        },
-      ],
-      name: 'กสิกรไทย จำกัด (มหาชน)',
-      remark: null,
-    },
-    {
-      abb: 'NBNK',
-      imageUrl: 'https://10.104.249.89/CPC-FE-WEB/api/contents/upload/NBNK_NBNK.png',
-      installments: [
-        {
-          installmentPercentage: 0,
-          installmentMonth: 3
-        },
-        {
-          installmentPercentage: 0,
-          installmentMonth: 6
-        },
-        {
-          installmentPercentage: 0,
-          installmentMonth: 12
-        },
-      ],
-      name: 'ธนชาต จำกัด(มหาชน)',
-      remark: null,
-    },
-    {
-      abb: 'SCIB',
-      imageUrl: 'https://10.104.249.89/CPC-FE-WEB/api/contents/upload/SCIB_siamcitybank.jpg',
-      installments: [
-        {
-          installmentPercentage: 0,
-          installmentMonth: 3
-        },
-        {
-          installmentPercentage: 0,
-          installmentMonth: 6
-        },
-        {
-          installmentPercentage: 0,
-          installmentMonth: 12
-        },
-      ],
-      name: 'นครหลวงไทย  จำกัด (มหาชน)',
-      remark: null,
-    }
-  ];
-}
-onSelectQRCode(qrCode: PaymentDetailQRCode) {
-  console.log('onSelectQRCode', qrCode);
-  this.selectPaymentDetail.qrCode = qrCode;
 
-}
-onSelectBank(bank: PaymentDetailBank) {
-  console.log('onSelectBank', bank);
-  this.selectPaymentDetail.bank = bank;
-  console.log('bank.installments', bank.installments);
-  console.log('this.paymentDetail.installments', this.paymentDetail.installments);
+  getBankData(): PaymentDetailBank[] {
+    return [
+      {
+        abb: 'CITIREADY',
+        imageUrl: 'https://10.104.249.89/CPC-FE-WEB/api/contents/upload/CITIREADY_citibank02.jpg',
+        installments: [
+          {
+            installmentPercentage: 0,
+            installmentMonth: 3
+          },
+          {
+            installmentPercentage: 0,
+            installmentMonth: 6
+          },
+          {
+            installmentPercentage: 0,
+            installmentMonth: 12
+          },
+        ],
+        name: 'ซิตี้แบงก์ เรดดี้เครดิต',
+        remark: null,
+      },
+      {
+        abb: 'CITI',
+        imageUrl: 'https://10.104.249.89/CPC-FE-WEB/api/contents/upload/CITI_CITI.png',
+        installments: [
+          {
+            installmentPercentage: 0,
+            installmentMonth: 3
+          },
+          {
+            installmentPercentage: 0,
+            installmentMonth: 6
+          },
+          {
+            installmentPercentage: 0,
+            installmentMonth: 12
+          },
+        ],
+        name: 'ซิตี้แบงก์',
+        remark: null,
+      },
+      {
+        abb: 'SCB',
+        imageUrl: 'https://10.104.249.89/CPC-FE-WEB/api/contents/upload/SCB_SCB02.png',
+        installments: [
+          {
+            installmentPercentage: 0,
+            installmentMonth: 3
+          },
+          {
+            installmentPercentage: 0,
+            installmentMonth: 6
+          },
+          {
+            installmentPercentage: 0,
+            installmentMonth: 12
+          },
+        ],
+        name: 'ไทยพาณิชย์ จำกัด (มหาชน)',
+        remark: null,
+      },
+      {
+        abb: 'KBNK',
+        imageUrl: 'https://10.104.249.89/CPC-FE-WEB/api/contents/upload/KBNK_kBank02.png',
+        installments: [
+          {
+            installmentPercentage: 0,
+            installmentMonth: 3
+          },
+          {
+            installmentPercentage: 0,
+            installmentMonth: 6
+          },
+          {
+            installmentPercentage: 0,
+            installmentMonth: 12
+          },
+        ],
+        name: 'กสิกรไทย จำกัด (มหาชน)',
+        remark: null,
+      },
+      {
+        abb: 'NBNK',
+        imageUrl: 'https://10.104.249.89/CPC-FE-WEB/api/contents/upload/NBNK_NBNK.png',
+        installments: [
+          {
+            installmentPercentage: 0,
+            installmentMonth: 3
+          },
+          {
+            installmentPercentage: 0,
+            installmentMonth: 6
+          },
+          {
+            installmentPercentage: 0,
+            installmentMonth: 12
+          },
+        ],
+        name: 'ธนชาต จำกัด(มหาชน)',
+        remark: null,
+      },
+      {
+        abb: 'SCIB',
+        imageUrl: 'https://10.104.249.89/CPC-FE-WEB/api/contents/upload/SCIB_siamcitybank.jpg',
+        installments: [
+          {
+            installmentPercentage: 0,
+            installmentMonth: 3
+          },
+          {
+            installmentPercentage: 0,
+            installmentMonth: 6
+          },
+          {
+            installmentPercentage: 0,
+            installmentMonth: 12
+          },
+        ],
+        name: 'นครหลวงไทย  จำกัด (มหาชน)',
+        remark: null,
+      }
+    ];
+  }
+  onSelectQRCode(qrCode: PaymentDetailQRCode) {
+    console.log('onSelectQRCode', qrCode);
+    this.selectPaymentDetail.qrCode = qrCode;
 
-  this.paymentDetail.installments = bank.installments; // Object.assign({}, bank.installments);
-}
-onSelectInstallment(installment: PaymentDetailInstallment[]) {
-  console.log('onSelectInstallment', installment);
-  this.selectPaymentDetail.bank.installments = installment;
-}
-onSelectPaymentType(paymentType: string) {
-  console.log('onSelectPaymentType', paymentType);
-  this.selectPaymentDetail.paymentType = paymentType;
-}
-onSelectPaymentTypeAdvancePay(paymentType: string) {
-  console.log('onSelectPaymentType', paymentType);
-  this.selectPaymentDetail.paymentType = paymentType;
-}
-onCompleteBillingAddress(billingAddress) {
-  console.log('billingAddress', billingAddress);
-}
+  }
+  onSelectBank(bank: PaymentDetailBank) {
+    console.log('onSelectBank', bank);
+    this.selectPaymentDetail.bank = bank;
+    console.log('bank.installments', bank.installments);
+    console.log('this.paymentDetail.installments', this.paymentDetail.installments);
 
-onCompleteReceiptInfo(receiptInfo) {
-  console.log('billingAddress', receiptInfo);
-}
+    this.paymentDetail.installments = bank.installments; // Object.assign({}, bank.installments);
+  }
+  onSelectInstallment(installment: PaymentDetailInstallment[]) {
+    console.log('onSelectInstallment', installment);
+    this.selectPaymentDetail.bank.installments = installment;
+  }
+  onSelectPaymentType(paymentType: string) {
+    console.log('onSelectPaymentType', paymentType);
+    this.selectPaymentDetail.paymentType = paymentType;
+  }
+  onSelectPaymentTypeAdvancePay(paymentType: string) {
+    console.log('onSelectPaymentType', paymentType);
+    this.selectPaymentDetail.paymentType = paymentType;
+  }
 
-onErrorBillingAddress(error: boolean) {
-  this.isSuccess = error;
-  console.log(this.isSuccess);
-}
 
-onErrorReceiptInfo(error: boolean) {
-  this.isSuccess = error;
-  console.log(this.isSuccess);
 
-}
-}
+  }
