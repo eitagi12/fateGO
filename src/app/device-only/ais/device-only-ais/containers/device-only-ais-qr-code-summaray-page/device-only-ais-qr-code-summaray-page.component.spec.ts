0731092561b0cc272472cc60ce5007bd24a38099
passk505 <passk505@aparr.postbox.in.th>
2019-03-13 13:59:53 +0700

    edit route

--- src/app/device-only/ais/device-only-ais/containers/device-only-ais-qr-code-summaray-page/device-only-ais-qr-code-summaray-page.component.spec.ts
+++ src/app/device-only/ais/device-only-ais/containers/device-only-ais-qr-code-summaray-page/device-only-ais-qr-code-summaray-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { DeviceOnlyAisQrCodeSummarayPageComponent } from './device-only-ais-qr-code-summaray-page.component';
-import { ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE, ROUTE_DEVICE_ONLY_AIS_CHECKOUT_PAYMENT_PAGE } from 'src/app/device-only/ais/device-only-ais/constants/route-path.constant';
+import { ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE, ROUTE_DEVICE_ONLY_AIS_CHECKOUT_PAYMENT_QR_CODE_PAGE } from 'src/app/device-only/ais/device-only-ais/constants/route-path.constant';
 
 describe('DeviceOnlyAisQrCodeSummarayPageComponent', () => {
   let component: DeviceOnlyAisQrCodeSummarayPageComponent;
@@ -28,7 +28,7 @@ describe('DeviceOnlyAisQrCodeSummarayPageComponent', () => {
   describe('onBack', () => {
     it('should route navigate to ROUTE_DEVICE_ONLY_AIS_CHECKOUT_PAYMENT_PAGE', () => {
       component.onBack();
-      expect(router.navigate).toHaveBeenCalledWith([ROUTE_DEVICE_ONLY_AIS_CHECKOUT_PAYMENT_PAGE]);
+      expect(router.navigate).toHaveBeenCalledWith([ROUTE_DEVICE_ONLY_AIS_CHECKOUT_PAYMENT_QR_CODE_PAGE]);
     });
   });
 
