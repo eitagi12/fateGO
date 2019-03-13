0c40f8481d698ffb5a1e0a9644130ba394c53651
nakarin <nakarin.weerasak@gmail.com>
2019-03-13 10:46:41 +0700

    fix mock

--- src/app/order/order-new-register/containers/order-new-register-verify-document-page/order-new-register-verify-document-page.component.ts
+++ src/app/order/order-new-register/containers/order-new-register-verify-document-page/order-new-register-verify-document-page.component.ts
@@ -483,17 +483,25 @@ export class OrderNewRegisterVerifyDocumentPageComponent implements OnInit, OnDe
       eventName: null
     };
 
+    const defaultIfEmpty = (text: string): string => {
+      return (text || '').trim();
+    };
+
+    const convertStringToDate = (dateStr: string): string => {
+      return  Moment(dateStr, 'YYMMDD').add(543 , 'years').format('DD/MM/YYYY');
+    };
+
     const mapData = function mapDataFromAisWebConnect(data: any): any {
       return {
         idCardType: 'หนังสือเดินทาง',
-        expireDate: this.convertStringToDate(data.ExpireDate),
-        issuingCountry: this.defaultIfEmpty(data.IssuingCountry),
-        firstName: this.defaultIfEmpty(data.GivenName),
-        lastName: this.defaultIfEmpty(data.Surname),
-        nationality: this.defaultIfEmpty(data.Nationality),
-        birthdate: this.convertStringToDate(data.BirthDate),
-        gender: this.defaultIfEmpty(data.Sex) === '1' ? 'M' : 'F',
-        idCardNo: this.defaultIfEmpty(data.PassportNumber),
+        expireDate: convertStringToDate(data.ExpireDate),
+        issuingCountry: defaultIfEmpty(data.IssuingCountry),
+        firstName: defaultIfEmpty(data.GivenName),
+        lastName: defaultIfEmpty(data.Surname),
+        nationality: defaultIfEmpty(data.Nationality),
+        birthdate: convertStringToDate(data.BirthDate),
+        gender: defaultIfEmpty(data.Sex) === '1' ? 'M' : 'F',
+        idCardNo: defaultIfEmpty(data.PassportNumber),
         imageReadPassport: (data.imageReadPassport || '').replace(REGEX_DATA_IMAGE, ''),
       };
     };
@@ -569,13 +577,6 @@ export class OrderNewRegisterVerifyDocumentPageComponent implements OnInit, OnDe
         }
       });
   }
-
-  defaultIfEmpty(text: string): string {
-    return (text || '').trim();
-  }
-
-  convertStringToDate(dateStr: string): string {
-    return  Moment(dateStr, 'YYMMDD').add(543 , 'years').format('DD/MM/YYYY');
-  }
+  // mockFunc
 
 }
