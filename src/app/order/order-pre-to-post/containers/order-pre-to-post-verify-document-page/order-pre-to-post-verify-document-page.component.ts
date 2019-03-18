82633e4116e708013a4115978c301eb55ac4752d
Sirikan <sirikanpluem@gmail.com>
2019-03-18 10:41:13 +0700

    add data newca

--- src/app/order/order-pre-to-post/containers/order-pre-to-post-verify-document-page/order-pre-to-post-verify-document-page.component.ts
+++ src/app/order/order-pre-to-post/containers/order-pre-to-post-verify-document-page/order-pre-to-post-verify-document-page.component.ts
@@ -379,12 +379,25 @@ export class OrderPreToPostVerifyDocumentPageComponent implements OnInit, OnDest
         })
           .then((resp: any) => {
             const data = resp.data || {};
+            // readPassport NewCa จะไม่ได้ address
             return {
               caNumber: data.caNumber,
               mainMobile: data.mainMobile,
               billCycle: data.billCycle,
-              // zipCode: zipCode
+              homeNo: data.homeNo,
+              moo: data.moo,
+              mooBan: data.mooBan,
+              room: data.room,
+              floor: data.floor,
+              buildingName: data.buildingName,
+              soi: data.soi,
+              street: data.street,
+              tumbol: data.tumbol,
+              amphur: data.amphur,
+              province: data.province,
+              zipCode: data.zipCode
             };
+
           }).then((customer) => {
             this.transaction.data.customer = Object.assign(
               Object.assign({}, this.transaction.data.customer),
