25ed9118b3e92d441bfb3c13c03d846896ecd3a4
Sirikan <sirikanpluem@gmail.com>
2019-03-18 10:39:14 +0700

    add data newca

--- src/app/order/order-mnp/containers/order-mnp-verify-document-page/order-mnp-verify-document-page.component.ts
+++ src/app/order/order-mnp/containers/order-mnp-verify-document-page/order-mnp-verify-document-page.component.ts
@@ -319,7 +319,6 @@ export class OrderMnpVerifyDocumentPageComponent implements OnInit {
 
   onReadPassport() {
     this.readPassportSubscription = this.readPassportService.onReadPassport().subscribe((readPassport: ReadPassport) => {
-      console.log('readpassport', readPassport);
       if (readPassport.error) {
         this.alertService.error(this.ERR_MASSEAGE);
         return;
@@ -336,11 +335,23 @@ export class OrderMnpVerifyDocumentPageComponent implements OnInit {
         })
           .then((resp: any) => {
             const data = resp.data || {};
+            // readPassport NewCa จะไม่ได้ address
             return {
               caNumber: data.caNumber,
               mainMobile: data.mainMobile,
               billCycle: data.billCycle,
-              // zipCode: zipCode
+              homeNo: data.homeNo,
+              moo: data.moo,
+              mooBan: data.mooBan,
+              room: data.room,
+              floor: data.floor,
+              buildingName: data.buildingName,
+              soi: data.soi,
+              street: data.street,
+              tumbol: data.tumbol,
+              amphur: data.amphur,
+              province: data.province,
+              zipCode: data.zipCode
             };
           }).then((customer) => {
             this.transaction.data.customer = Object.assign(
