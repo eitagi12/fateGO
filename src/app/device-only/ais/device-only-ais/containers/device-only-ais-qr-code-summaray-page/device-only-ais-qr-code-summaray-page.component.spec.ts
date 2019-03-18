86bdbeb516ea201e811df257fe1d81e22304fbe4
Santi <santi@odds.team>
2019-03-18 17:52:02 +0700

    add more unit test for check route when click btn

--- src/app/device-only/ais/device-only-ais/containers/device-only-ais-qr-code-summaray-page/device-only-ais-qr-code-summaray-page.component.spec.ts
+++ src/app/device-only/ais/device-only-ais/containers/device-only-ais-qr-code-summaray-page/device-only-ais-qr-code-summaray-page.component.spec.ts
@@ -1,42 +1,86 @@
+import { CUSTOM_ELEMENTS_SCHEMA, Pipe, PipeTransform } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+
+import { async, ComponentFixture, TestBed, ComponentFixtureNoNgZone } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { DeviceOnlyAisQrCodeSummarayPageComponent } from './device-only-ais-qr-code-summaray-page.component';
-import { ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE, ROUTE_DEVICE_ONLY_AIS_CHECKOUT_PAYMENT_QR_CODE_PAGE } from 'src/app/device-only/ais/device-only-ais/constants/route-path.constant';
+import { CookiesStorageService } from 'ngx-store';
+import { JwtHelperService } from '@auth0/angular-jwt/src/jwthelper.service';
+import { By } from '@angular/platform-browser';
+import { ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE, ROUTE_DEVICE_ONLY_AIS_CHECKOUT_PAYMENT_QR_CODE_PAGE } from '../../constants/route-path.constant';
+
+@Pipe({name: 'mobileNo'})
+class MockMobileNoPipe implements PipeTransform {
+  transform(value: string): string {
+      return value;
+  }
+}
 
 describe('DeviceOnlyAisQrCodeSummarayPageComponent', () => {
   let component: DeviceOnlyAisQrCodeSummarayPageComponent;
-  let router;
-  let homeService;
+  let fixture: ComponentFixture<DeviceOnlyAisQrCodeSummarayPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      declarations: [
+        DeviceOnlyAisQrCodeSummarayPageComponent,
+        MockMobileNoPipe
+      ],
+      providers: [
+        CookiesStorageService,
+        {
+          provide: JwtHelperService,
+          useValue: {}
+        },
+        HttpClient,
+        HttpHandler
+      ]
+    })
+    .compileComponents();
+  }));
 
   beforeEach(() => {
-    router = {
-      navigate: jest.fn()
-    };
-
-    homeService = {
-      goToHome: jest.fn()
-    };
-
-    component = new DeviceOnlyAisQrCodeSummarayPageComponent(
-      router,
-      homeService
-    );
+    fixture = TestBed.createComponent(DeviceOnlyAisQrCodeSummarayPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should create component', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('onBack', () => {
-    it('should route navigate to ROUTE_DEVICE_ONLY_AIS_CHECKOUT_PAYMENT_PAGE', () => {
-      component.onBack();
-      expect(router.navigate).toHaveBeenCalledWith([ROUTE_DEVICE_ONLY_AIS_CHECKOUT_PAYMENT_QR_CODE_PAGE]);
+  it('should call method onNext when click next button', async(() => {
+    spyOn(component, 'onNext');
+    const buttonNext = fixture.debugElement.query(By.css('#button-next'));
+    buttonNext.triggerEventHandler('click', null);
+    fixture.whenStable().then(() => {
+      expect(component.onNext).toHaveBeenCalled();
     });
+  }));
+
+  it('should call method onBack when click next button', async(() => {
+    spyOn(component, 'onBack');
+    const buttonBack = fixture.debugElement.query(By.css('#button-back'));
+    buttonBack.triggerEventHandler('click', null);
+    fixture.whenStable().then(() => {
+      expect(component.onBack).toHaveBeenCalled();
+    });
+  }));
+
+  it('should navigate to ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE when execute method onNext', () => {
+    jest.spyOn(component.router, 'navigate').mockImplementation();
+    component.onNext();
+    expect(component.router.navigate).toHaveBeenCalledWith([ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE]);
   });
 
-  describe('onNext', () => {
-    it('should route to navigate to ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE', () => {
-      component.onNext();
-      expect(router.navigate).toHaveBeenCalledWith([ROUTE_DEVICE_ONLY_AIS_QR_CODE_GENERATE_PAGE]);
-    });
+  it('should navigate to ROUTE_DEVICE_ONLY_AIS_CHECKOUT_PAYMENT_QR_CODE_PAGE when execute method onBack', () => {
+    jest.spyOn(component.router, 'navigate').mockImplementation();
+    component.onBack();
+    expect(component.router.navigate).toHaveBeenCalledWith([ROUTE_DEVICE_ONLY_AIS_CHECKOUT_PAYMENT_QR_CODE_PAGE]);
   });
-
 });
