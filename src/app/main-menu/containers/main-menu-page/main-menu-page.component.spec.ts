beff43aadff03a4446c456fdbba95129282266e4
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-03-21 16:51:58 +0700

    fix test main-menu

--- src/app/main-menu/containers/main-menu-page/main-menu-page.component.spec.ts
+++ src/app/main-menu/containers/main-menu-page/main-menu-page.component.spec.ts
@@ -1,14 +1,49 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainMenuPageComponent } from './main-menu-page.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CookiesStorageService } from 'ngx-store';
+import { JwtModule, JwtHelperService } from '@auth0/angular-jwt';
+import { TokenService } from 'mychannel-shared-libs';
 
 describe('MainMenuPageComponent', () => {
   let component: MainMenuPageComponent;
   let fixture: ComponentFixture<MainMenuPageComponent>;
+  const mockTokenService: any = {
+    provide: TokenService,
+    useValue: {
+      getUser: () => {
+        return { locationCode: '' };
+      }
+    }
+  };
+  const mockJwtHelperService: any = {
+    provide: JwtHelperService,
+    useValue: {}
+  };
 
+  const mockConfigJwtModule: any = {
+    config: {
+      tokenGetter: () => {
+        return '';
+      }
+    }
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainMenuPageComponent ]
+      imports: [
+        RouterTestingModule,
+        JwtModule.forRoot(mockConfigJwtModule)
+      ],
+      declarations: [
+        MainMenuPageComponent
+      ],
+      providers: [
+        CookiesStorageService,
+
+        mockJwtHelperService,
+        mockTokenService
+      ]
     })
     .compileComponents();
   }));
