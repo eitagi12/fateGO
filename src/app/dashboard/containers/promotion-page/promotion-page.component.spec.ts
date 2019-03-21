4cadf5243d3c87b8b7e71f0083e92facafe1c57c
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-03-21 16:50:15 +0700

    fix test dashboard

--- src/app/dashboard/containers/promotion-page/promotion-page.component.spec.ts
+++ src/app/dashboard/containers/promotion-page/promotion-page.component.spec.ts
@@ -1,14 +1,59 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PromotionPageComponent } from './promotion-page.component';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, Injector } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ModalModule } from 'ngx-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { CookiesStorageService } from 'ngx-store';
+import { JwtHelperService } from '@auth0/angular-jwt/src/jwthelper.service';
+import { JwtModule } from '@auth0/angular-jwt';
+import { TokenService } from 'mychannel-shared-libs';
 
 describe('PromotionPageComponent', () => {
   let component: PromotionPageComponent;
   let fixture: ComponentFixture<PromotionPageComponent>;
+  const mockTokenService: any = {
+    provide: TokenService,
+    useValue: {
+      getUser: () => {
+        return { locationCode: '' };
+      }
+    }
+  };
+  const mockJwtHelperService: any = {
+    provide: JwtHelperService,
+    useValue: {}
+  };
+
+  const mockConfigJwtModule: any = {
+    config: {
+      tokenGetter: () => {
+        return '';
+      }
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PromotionPageComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ModalModule.forRoot(),
+        JwtModule.forRoot(mockConfigJwtModule)
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA,
+        NO_ERRORS_SCHEMA
+      ],
+      declarations: [
+        PromotionPageComponent
+      ],
+      providers: [
+        CookiesStorageService,
+        mockJwtHelperService,
+        mockTokenService
+      ]
     })
     .compileComponents();
   }));
