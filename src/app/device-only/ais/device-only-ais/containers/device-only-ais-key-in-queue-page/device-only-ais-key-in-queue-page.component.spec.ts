164384e0c8e8a675d4a18e1ab091bbb63f070371
oat <oat19454@gmail.com>
2019-03-21 15:13:06 +0700

    make form queue page

--- src/app/device-only/ais/device-only-ais/containers/device-only-ais-key-in-queue-page/device-only-ais-key-in-queue-page.component.spec.ts
+++ src/app/device-only/ais/device-only-ais/containers/device-only-ais-key-in-queue-page/device-only-ais-key-in-queue-page.component.spec.ts
@@ -1,23 +1,50 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { DeviceOnlyAisKeyInQueuePageComponent } from './device-only-ais-key-in-queue-page.component';
-import { ROUTE_DEVICE_ONLY_AIS_QUEUE_PAGE } from '../../constants/route-path.constant';
+import { CUSTOM_ELEMENTS_SCHEMA, Pipe, PipeTransform } from '@angular/core';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
+import { HomeService } from 'mychannel-shared-libs';
 
-describe('test device only ais queue page', () => {
+describe('DeviceOnlyAisKeyInQueuePageComponent', () => {
   let component: DeviceOnlyAisKeyInQueuePageComponent;
+  let fixture: ComponentFixture<DeviceOnlyAisKeyInQueuePageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      declarations: [
+        DeviceOnlyAisKeyInQueuePageComponent
+       ],
+      providers: [
+        {
+          provide: HomeService,
+          useValue: {
+            goToHome: jest.fn()
+          }
+        },
+        {
+          provide: TransactionService,
+          useValue: {
+            load: jest.fn()
+          }
+        }
+      ]
+    })
+    .compileComponents();
+  }));
+
   beforeEach(() => {
-    component = new DeviceOnlyAisKeyInQueuePageComponent(
-      {navigate: jest.fn()}
-    );
+    fixture = TestBed.createComponent(DeviceOnlyAisKeyInQueuePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  describe('test onNext', () => {
-    it('go to page queue', () => {
-      component.onNext();
-      expect(component.router.navigate).toHaveBeenCalledWith([ROUTE_DEVICE_ONLY_AIS_QUEUE_PAGE]);
-    });
-  });
-
 });
--- src/app/device-only/ais/device-only-ais/containers/device-only-ais-key-in-queue-page/device-only-ais-key-in-queue-page.component.ts
+++ src/app/device-only/ais/device-only-ais/containers/device-only-ais-key-in-queue-page/device-only-ais-key-in-queue-page.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ROUTE_DEVICE_ONLY_AIS_QUEUE_PAGE } from '../../constants/route-path.constant';
 import { Router } from '@angular/router';
 import { HomeService } from 'mychannel-shared-libs';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { validate } from '@babel/types';
+import { Transaction } from 'src/app/shared/models/transaction.model';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
 
 @Component({
   selector: 'app-device-only-ais-key-in-queue-page',
@@ -9,15 +13,34 @@ import { HomeService } from 'mychannel-shared-libs';
   styleUrls: ['./device-only-ais-key-in-queue-page.component.scss']
 })
 export class DeviceOnlyAisKeyInQueuePageComponent implements OnInit, OnDestroy {
+  public queueForm: FormGroup;
+  private transaction: Transaction;
 
-  constructor(private router: Router,
-              private homeService: HomeService) { }
+  constructor(
+    public router: Router,
+    private fb: FormBuilder,
+    private transactionService: TransactionService,
+    private homeService: HomeService) { }
 
   ngOnInit(): void {
-
+    this.transaction = this.transactionService.load();
+    this.createQueueForm();
   }
 
+  public createQueueForm(): void {
+    this.queueForm = this.fb.group({
+      queueNo: (['', Validators.compose([
+        Validators.required,
+        Validators.minLength(4),
+        Validators.pattern('^[A-Z0-9]+$')
+      ])])
+    });
+  }
+
+  get f(): any { return this.queueForm.controls; }
+
   onNext(): void {
+    this.transaction.data.queue = this.queueForm.value.queueNo;
     this.router.navigate([ROUTE_DEVICE_ONLY_AIS_QUEUE_PAGE]);
   }
 
