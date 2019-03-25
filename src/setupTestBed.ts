f525dbc1046ff819cfcc48d4558362405b4d119a
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-03-25 11:55:15 +0700

    [ADD] filter dup schemas

--- src/setupTestBed.ts
+++ src/setupTestBed.ts
@@ -1,18 +1,26 @@
-import { TestBed, async, TestModuleMetadata } from '@angular/core/testing';
+import { TestBed, async, TestModuleMetadata, getTestBed, ComponentFixture } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
-const resetTestingModule = TestBed.resetTestingModule,
-  preventAngularFromResetting = () => TestBed.resetTestingModule = () => TestBed;
-  const allowAngularToReset = () => TestBed.resetTestingModule = resetTestingModule;
+const resetTestingModule = TestBed.resetTestingModule;
+const preventAngularFromResetting = () => TestBed.resetTestingModule = () => TestBed;
+const allowAngularToReset = () => TestBed.resetTestingModule = resetTestingModule;
+const makeSchemas = (schemas: any[]) => {
+  let s = [];
+  if (schemas && schemas.length > 0) {
+    s = [
+      ...schemas,
+      CUSTOM_ELEMENTS_SCHEMA,
+      NO_ERRORS_SCHEMA
+    ];
+  }
+  return s.filter((elem, i, arr) => arr.indexOf(elem) === i);
+};
 
 global.setupTestBed = (moduleDef: TestModuleMetadata) => {
-  beforeAll(async(async() => {
+  beforeAll(async(async () => {
     resetTestingModule();
     preventAngularFromResetting();
-    moduleDef.schemas = [
-        CUSTOM_ELEMENTS_SCHEMA,
-        NO_ERRORS_SCHEMA
-    ],
+    moduleDef.schemas = makeSchemas(moduleDef.schemas);
     TestBed.configureTestingModule(moduleDef);
     await TestBed.compileComponents();
   }));
