3a6c87c54e0cf703d1fb6985094308c476e41605
kawinpop <kawinpop123@gmail.com>
2019-04-04 13:30:31 +0700

    Add Prepaid

--- src/app/device-order/ais/device-order-ais-existing-prepaid-hotdeal/containers/device-order-ais-existing-prepaid-hotdeal-eligible-mobile-page/device-order-ais-existing-prepaid-hotdeal-eligible-mobile-page.component.ts
+++ src/app/device-order/ais/device-order-ais-existing-prepaid-hotdeal/containers/device-order-ais-existing-prepaid-hotdeal-eligible-mobile-page/device-order-ais-existing-prepaid-hotdeal-eligible-mobile-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
-import { HomeService, ShoppingCart, EligibleMobile } from 'mychannel-shared-libs';
+import { HomeService, ShoppingCart, EligibleMobile, REGEX_MOBILE, PageLoadingService } from 'mychannel-shared-libs';
 import {
   ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_CUSTOMER_INFO_PAGE,
   ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_VALIDATE_CUSTOMER_ID_CARD_PAGE
@@ -10,7 +10,8 @@ import { ShoppingCartService } from 'src/app/device-order/services/shopping-cart
 import { Transaction } from 'src/app/shared/models/transaction.model';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
 import { HttpClient } from '@angular/common/http';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { resolve } from 'dns';
 
 export interface BillingAccount {
   billingName: string;
@@ -29,12 +30,13 @@ export interface BillingAccount {
   templateUrl: './device-order-ais-existing-prepaid-hotdeal-eligible-mobile-page.component.html',
   styleUrls: ['./device-order-ais-existing-prepaid-hotdeal-eligible-mobile-page.component.scss']
 })
-export class DeviceOrderAisExistingPrepaidHotdealEligibleMobilePageComponent implements OnInit {
+export class DeviceOrderAisExistingPrepaidHotdealEligibleMobilePageComponent implements OnInit, OnDestroy {
 
   wizards: string[] = WIZARD_DEVICE_ORDER_AIS;
   shoppingCart: ShoppingCart;
   eligibleMobiles: Array<EligibleMobile>;
   selectMobileNo: EligibleMobile;
+  addMobileNo: string;
   transaction: Transaction;
   billingAccountList: Array<BillingAccount>;
   billingNetExtremeList: Array<BillingAccount>;
@@ -48,7 +50,8 @@ export class DeviceOrderAisExistingPrepaidHotdealEligibleMobilePageComponent imp
     private http: HttpClient,
     private shoppingCartService: ShoppingCartService,
     private transactionService: TransactionService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private pageLoadingService: PageLoadingService,
   ) {
     this.transaction = this.transactionService.load();
     this.shoppingCart = Object.assign(this.shoppingCartService.getShoppingCartData(), {
@@ -58,7 +61,6 @@ export class DeviceOrderAisExistingPrepaidHotdealEligibleMobilePageComponent imp
   }
 
   ngOnInit(): void {
-
     if (this.transaction.data.customer) {
       this.idCardNo = this.transaction.data.customer.idCardNo;
       this.getMobileList();
@@ -69,12 +71,15 @@ export class DeviceOrderAisExistingPrepaidHotdealEligibleMobilePageComponent imp
 
   createForm(): void {
     this.eligibleAddMobile = this.fb.group({
-      mobileAdd: new FormControl()
+      mobileAdd: ['', [Validators.pattern(REGEX_MOBILE)]]
     });
 
     this.eligibleAddMobile.valueChanges.subscribe((value) => {
-      console.log('value', value);
-
+      if (this.eligibleAddMobile.valid ) {
+        this.addMobileNo = value.mobileAdd;
+      } else {
+        this.addMobileNo = '';
+      }
     });
   }
 
@@ -96,13 +101,105 @@ export class DeviceOrderAisExistingPrepaidHotdealEligibleMobilePageComponent imp
     this.eligibleMobiles = mobiles;
   }
 
+  verifyMobileNo(mobileNo: string): Promise<any> {
+
+    const verifyMobile: any = {
+      mobileNo: mobileNo,
+      isRePrepaidIdent: false
+    };
+    // tslint:disable-next-line:no-shadowed-variable
+    return new Promise((resolve, reject) => {
+      this.http.get(`/api/customerportal/asset/${mobileNo}/profile`).toPromise()
+      .then((resp: any) => {
+        if (!resp) {
+          return reject('ไม่สามารถทำรายการได้ เนื่องจากหมายเลขนี้ยังไม่เปิดใช้บริการในระบบเติมเงิน');
+        }
+        const data = resp.data;
+        const status = data.mobileStatus;
+        if (data.chargeType !== 'Pre-paid') {
+          return reject('ไม่สามารถทำรายการได้ เนื่องจากเป็นหมายเลขระบบรายเดือน');
+        }
+        if (status === '000' || status === '378' || status === 'Active' || status === 'Suspended') {
+          return data;
+        } else {
+          return reject('ไม่สามารถทำรายการได้ เนื่องจากหมายเลขนี้ยังไม่เปิดใช้บริการในระบบเติมเงิน');
+        }
+      })
+      .then((req: any) => {
+        return this.http.get(`/api/customerportal/newRegister/verifyPrepaidIdent?idCard=${this.idCardNo}&mobileNo=${mobileNo}`).toPromise()
+        .then((respPrepaid: any) => {
+          verifyMobile.isRePrepaidIdent = !!(respPrepaid.data.success);
+          return respPrepaid;
+        });
+      })
+      .then((respPrivilege: any) => {
+
+      })
+      .catch((err) => {
+        return reject(err);
+      });
+    });
+  }
+
+  checkPrivilage(mobiles: any[], ussdCode: string): Promise<any[]> {
+    if (!mobiles) {
+        return Promise.resolve([]);
+    }
+    const promise = [];
+    mobiles.forEach(mobile => {
+        // tslint:disable-next-line:no-shadowed-variable
+        const service = new Promise((resolve, reject) => {
+          this.http.post(`/api/salesportal/privilege/check-privilege`, {
+            msisdn: mobile.mobileNo,
+            shortCode: ussdCode
+          }).toPromise()
+          .then((resp: any) => {
+            if (resp.data.status === '20000') {
+                resolve(mobile);
+            } else {
+                resolve(null);
+            }
+          }).catch((error: any) => {
+            console.log('Check privilage ', error);
+            const data = this.handleResponseError(error);
+              if (data.description === 'DUPLICATE' || data.description === 'SEND_MSG_AGAIN') {
+                  // this.checkDeviceTransaction(mobile, ussdCode)
+                  //           .then(resolve).catch(() => resolve(null));
+              } else {
+                resolve(null);
+              }
+            });
+        });
+        promise.push(service);
+    });
+
+    return Promise.all(promise).then((values: any[]) => values.filter(value => value));
+}
+
+handleResponseError(error: any): any {
+  try {
+      const body = JSON.parse(error._body);
+      const _error = body.errors;
+      const data = _error.substring(_error.indexOf('{'), _error.indexOf('}') + 1);
+      return JSON.parse(data);
+  } catch (e) {
+      return error;
+  }
+}
+
   onComplete(eligibleMobile: EligibleMobile): void {
     this.selectMobileNo = eligibleMobile;
   }
 
   onNext(): void {
-    this.transaction.data.simCard = { mobileNo: this.selectMobileNo.mobileNo , persoSim: false };
-    this.router.navigate([ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_CUSTOMER_INFO_PAGE]);
+    this.pageLoadingService.openLoading();
+    if (this.addMobileNo) {
+      this.verifyMobileNo(this.addMobileNo).then((res: any) => {
+        this.pageLoadingService.closeLoading();
+      });
+    }
+    // this.transaction.data.simCard = { mobileNo: this.selectMobileNo.mobileNo , persoSim: false };
+    // this.router.navigate([ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_CUSTOMER_INFO_PAGE]);
   }
 
   onBack(): void {
@@ -113,7 +210,6 @@ export class DeviceOrderAisExistingPrepaidHotdealEligibleMobilePageComponent imp
     this.homeService.goToHome();
   }
 
-  // tslint:disable-next-line: use-life-cycle-interface
   ngOnDestroy(): void {
     this.transactionService.update(this.transaction);
   }
