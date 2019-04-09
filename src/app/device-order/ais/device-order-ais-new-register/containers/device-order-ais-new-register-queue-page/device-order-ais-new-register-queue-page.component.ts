ad3596475fc84bc2648a1cebcf7bc87bbcab8b27
prapat wongman <prapao49@corp-ais900>
2019-04-09 16:45:27 +0700

    [edit] data create order

--- src/app/device-order/ais/device-order-ais-new-register/containers/device-order-ais-new-register-queue-page/device-order-ais-new-register-queue-page.component.ts
+++ src/app/device-order/ais/device-order-ais-new-register/containers/device-order-ais-new-register-queue-page/device-order-ais-new-register-queue-page.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import {
   HomeService, PageLoadingService, REGEX_MOBILE, TokenService
 } from 'mychannel-shared-libs';
-import { Transaction } from 'src/app/shared/models/transaction.model';
+import { Transaction, TransactionData, MainPackage, Payment } from 'src/app/shared/models/transaction.model';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
 import { ROUTE_DEVICE_ORDER_AIS_NEW_REGISTER_AGGREGATE_PAGE, ROUTE_DEVICE_ORDER_AIS_NEW_REGISTER_RESULT_PAGE } from 'src/app/device-order/ais/device-order-ais-new-register/constants/route-path.constant';
 import { HttpClient } from '@angular/common/http';
@@ -56,12 +56,16 @@ export class DeviceOrderAisNewRegisterQueuePageComponent implements OnInit, OnDe
     const productStock = this.priceOption.productStock;
     const trade = this.priceOption.trade;
     const data = this.transaction.data;
+    const customerGroup = this.priceOption.customerGroup;
 
     const discount = trade.discount;
     const customer = data.customer;
     const simCard = data.simCard;
     const order = data.order;
     const queue = data.queue;
+    const payment = data.payment;
+    const advancePay = data.advancePayment;
+
     return {
       userId: user.username,
       locationSource: user.locationCode,
@@ -99,25 +103,25 @@ export class DeviceOrderAisNewRegisterQueuePageComponent implements OnInit, OnDe
         tumbon: customer.tumbol
       },
       cusMobileNoOrder: simCard.mobileNo || '',
-      // returnCode: simCard.privilegeCode || '4GEYYY',
+      returnCode: simCard.privilegeCode || '4GEYYY',
       // cashBackFlg: cashBackFlg,
-      // matAirTime: advancePay && advancePay.matAirtime || '',
+      matAirTime: trade.matAirtime || '',
       // matCodeFreeGoods: matCodeFreeGoods,
-      // paymentRemark: remark ? this.remarkService.getOrderRemark(remark, this.airTimeBank) : '',
-      // installmentTerm: installmentMounth,
-      // installmentRate: installmentPercentage,
-      // mobileAisFlg: 'Y',
-      // paymentMethod: paymentMethod,
-      // bankCode: bankCode,
-      // tradeFreeGoodsId: freeGoodsId || '',
+      paymentRemark: this.getOrderRemark(trade, data),
+      installmentTerm: payment.paymentBank.paymentMethod.month,
+      installmentRate: payment.paymentBank.paymentMethod.percentage,
+      mobileAisFlg: 'Y',
+      paymentMethod: this.getPaymentMethod(trade, advancePay),
+      bankCode: this.getBankCode(trade, payment, advancePay),
+      tradeFreeGoodsId: trade.freeGoods[0].tradeFreegoodsId || '',
       // matairtimeId: '',
-      // tradeDiscountId: discountId || '',
-      // tradeAirtimeId: airtimeId || '',
+      tradeDiscountId: trade.discount.tradeDiscountId || '',
+      tradeAirtimeId: trade.advancePay.tradeAirtimeId || '',
       // focCode: '',
-      // bankAbbr: bankCode,
+      bankAbbr: this.getBankCode(trade, payment, advancePay),
       // preBookingNo: preBookingNo,
       // depositAmt: depositAmt,
-      // convertToNetwotkType: customerGroup.code === 'MC002' ? '3G POSTPAID' : undefined
+      convertToNetwotkType: customerGroup.code === 'MC002' ? '3G POSTPAID' : undefined
     };
   }
 
@@ -152,4 +156,134 @@ export class DeviceOrderAisNewRegisterQueuePageComponent implements OnInit, OnDe
     this.transactionService.update(this.transaction);
   }
 
+  getPaymentMethod(trade: any, advancePay: Payment): string {
+    let paymentMethod;
+    if (trade && trade.payments && trade.payments.length > 0) {
+      paymentMethod = trade.payments[0].method;
+
+      if (trade.advancePay.installmentFlag === 'Y') {
+        return paymentMethod;
+      }
+
+      if (trade.amount > 0) {
+        if (advancePay.paymentType !== 'CREDIT') {
+          paymentMethod = paymentMethod + '|CA';
+        }
+      }
+
+    }
+    return paymentMethod;
+  }
+
+  getBankCode(trade: any, payment: Payment, advancePay: Payment): string {
+
+    let bankCode: string = payment.paymentBank.abb;
+
+    if (trade.advancePay.installmentFlag === 'Y') { // ผ่อนรวม
+      return bankCode;
+    }
+
+    if (trade.amount > 0) {
+      if (advancePay.paymentType === 'CREDIT') {
+        bankCode = payment.paymentBank.abb + '|' + advancePay.paymentBank.abb;
+      } else {
+        bankCode = payment.paymentBank.abb;
+      }
+    }
+    return bankCode;
+  }
+  getOrderRemark(trade: any, data: TransactionData): string {
+    const NEW_LINE = '\n';
+
+    const remark = this.getRemarkCampaignName(data.mainPackage) + NEW_LINE
+      + this.getRemarkAdvancePay(trade, data.advancePayment) + NEW_LINE
+      + this.getRemarkTradeAndInstallment(trade, data.payment) + NEW_LINE
+      + this.getRemarkOtherInfomation(data) + NEW_LINE;
+
+    return remark;
+  }
+  getRemarkCampaignName(mainPackage: MainPackage): string {
+    const promotionName = '[PM]';
+    return promotionName + ' ' + mainPackage.customAttributes.shortNameThai;
+  }
+  getRemarkAdvancePay(trade: any, advancePay: Payment): string {
+    let airTime = '';
+    if (trade.advancePay.installmentFlag !== 'Y' && +trade.advancePay.amount !== 0) {
+      airTime += '[AT]';
+      if (advancePay.paymentType !== 'CREDIT') {
+        if (advancePay.paymentType === 'QR_CODE') {
+          if (advancePay.paymentQrCodeType === 'THAI_QR') {
+            airTime += '[PB]'; // Thai QRcode
+          } else {
+            airTime += '[RL]'; // Rabbit line pay
+          }
+        } else {
+          airTime += '[CA]';
+        }
+
+        return airTime;
+      } else {
+        airTime += '[AT]';
+        airTime += '[CC]' + ',' + ' ';
+        airTime += '[B]' + advancePay.paymentBank.abb;
+        return airTime;
+      }
+    } else {
+      return airTime;
+    }
+  }
+  getRemarkTradeAndInstallment(trade: any, payment: Payment): string {
+
+    let tradeAndInstallment = '';
+    if (+trade.advancePay.amount !== 0) {
+      if (trade.advancePay.installmentFlag === 'Y') {
+        tradeAndInstallment = '[AD]';
+      } else {
+        tradeAndInstallment = '[DV]';
+        if (payment.paymentType !== 'CREDIT') {
+          if (payment.paymentType === 'QR_CODE') {
+            if (payment.paymentQrCodeType === 'THAI_QR') {
+              tradeAndInstallment += '[PB]' + ',' + ' '; // Thai QRcode
+            } else {
+              tradeAndInstallment += '[RL]' + ',' + ' '; // Rabbit line pay
+            }
+          }
+        }
+      }
+    } else {
+      tradeAndInstallment = '[AD]';
+      if (payment.paymentType !== 'CREDIT') {
+        if (payment.paymentType === 'QR_CODE') {
+          if (payment.paymentQrCodeType === 'THAI_QR') {
+            tradeAndInstallment += '[PB]' + ',' + ' '; // Thai QRcode
+          } else {
+            tradeAndInstallment += '[RL]' + ',' + ' '; // Rabbit line pay
+          }
+        }
+        tradeAndInstallment += '[CA]' + ',' + ' ';
+      } else {
+        tradeAndInstallment += '[CC]' + ',' + ' ';
+        tradeAndInstallment += '[B]' + ',' + ' ';
+        if (payment.paymentBank.installmentMonth > 0) {
+          tradeAndInstallment += '[I]' + payment.paymentBank.percentage + '%' + ' ' + payment.paymentBank.month + 'เดือน' + ',' + ' ';
+        }
+      }
+    }
+    tradeAndInstallment += '[T]' + trade.tradeNo;
+    return tradeAndInstallment;
+  }
+  getRemarkOtherInfomation(data: TransactionData): string {
+    let otherInformation = '';
+    otherInformation += '[SP]' + ' ' + 0 + ',' + ' ';
+    otherInformation += '[SD]' + ' ' + 0 + ',' + ' ';
+    // otherInformation += '[D]' + ' ' + this.getDiscountAmount(remark.discount) + ',' + ' ';
+    // otherInformation += '[RC]' + ' ' + privilegeCode + ',' + ' ';
+    // otherInformation += '[OT]' + ' ' + this.getCustomerType(remark.customerGroupCode) + ',' + ' ';
+    // otherInformation += '[PC]' + ' ' + remark.mainPackageCode + ',' + ' ';
+    // otherInformation += '[MCC]' + ' ' + remark.mobileCare + ',' + ' ';
+    // otherInformation += '[MC]' + ' ' + remark.mobileCareName + ',' + ' ';
+    // otherInformation += '[PN]' + ' ' + remark.privilegeDesc + ',' + ' ';
+    // otherInformation += '[Q]' + ' ' + remark.queueNumber;
+    return otherInformation;
+  }
 }
