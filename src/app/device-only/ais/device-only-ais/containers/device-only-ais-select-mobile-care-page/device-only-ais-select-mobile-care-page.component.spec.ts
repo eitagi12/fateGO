8347ac957d741c2e604b5ec2d0ec398607f41911
toom pornpavit <pornpavit@odds.team>
2019-04-09 17:00:44 +0700

    [Add] unit test mobile-care

--- src/app/device-only/ais/device-only-ais/containers/device-only-ais-select-mobile-care-page/device-only-ais-select-mobile-care-page.component.spec.ts
+++ src/app/device-only/ais/device-only-ais/containers/device-only-ais-select-mobile-care-page/device-only-ais-select-mobile-care-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CookiesStorageService, LocalStorageService } from 'ngx-store';
@@ -7,49 +7,116 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { JwtHelperService } from '@auth0/angular-jwt/src/jwthelper.service';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
-import { TokenService } from 'mychannel-shared-libs';
+import { TokenService, HomeService } from 'mychannel-shared-libs';
+import { By } from '@angular/platform-browser';
+import { ROUTE_DEVICE_ONLY_AIS_SELECT_PAYMENT_AND_RECEIPT_INFORMATION_PAGE, ROUTE_DEVICE_ONLY_AIS_SUMMARY_PAGE } from '../../constants/route-path.constant';
+import { Router } from '@angular/router';
 
 describe('DeviceOnlyAisSelectMobileCarePageComponent', () => {
   let component: DeviceOnlyAisSelectMobileCarePageComponent;
   let fixture: ComponentFixture<DeviceOnlyAisSelectMobileCarePageComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [ RouterTestingModule ],
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-      declarations: [ DeviceOnlyAisSelectMobileCarePageComponent ],
-      providers: [
-        CookiesStorageService,
-        {
-          provide: JwtHelperService,
-          useValue: {}
-        },
-        HttpClient,
-        HttpHandler,
-        {
-          provide: TransactionService,
-          useValue: {
-            load: jest.fn()
-          }
-        },
-        LocalStorageService,
-        {
-          provide: TokenService,
-          useValue: {
-            getUser: jest.fn()
-          }
+  let nextButton: DebugElement;
+  let router: Router;
+  let homeService: HomeService;
+  setupTestBed({
+    imports: [ RouterTestingModule.withRoutes([]) ],
+    declarations: [ DeviceOnlyAisSelectMobileCarePageComponent ],
+    providers: [
+      CookiesStorageService,
+      {
+        provide: JwtHelperService,
+        useValue: {}
+      },
+      HttpClient,
+      HttpHandler,
+      {
+        provide: TransactionService,
+        useValue: {
+          load: jest.fn(() => {
+            return {
+              data: {}
+            };
+          }),
+          update: jest.fn()
         }
-      ]
-    }).compileComponents();
-  }));
+      },
+      LocalStorageService,
+      {
+        provide: TokenService,
+        useValue: {
+          getUser: jest.fn()
+        }
+      },
+      HomeService
+    ]
+  });
 
   beforeEach(() => {
+    router = TestBed.get(Router);
+    homeService = TestBed.get(HomeService);
     fixture = TestBed.createComponent(DeviceOnlyAisSelectMobileCarePageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    nextButton = fixture.debugElement.query(By.css('button#button-next'));
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('templates', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should be disable next button when component initial', () => {
+      fixture.detectChanges();
+      expect(nextButton.nativeElement.disabled).toBeTruthy();
+    });
+
+    it('should be enable next button when choose mobile care package', () => {
+      component.onCompleted({id: 1, title: 'Mobile Care Mock'});
+      fixture.detectChanges();
+      expect(nextButton.nativeElement.disabled).toBeFalsy();
+      expect(component.transaction.data.mobileCarePackage).toBeTruthy();
+    });
+  });
+
+  describe('methods', () => {
+    it('should be navigate to "/device-only/ais/select-payment" when call onBack', () => {
+      const navigateSpy = spyOn(router, 'navigate');
+      component.onBack();
+      expect(navigateSpy).toHaveBeenCalledWith([ROUTE_DEVICE_ONLY_AIS_SELECT_PAYMENT_AND_RECEIPT_INFORMATION_PAGE]);
+    });
+
+    it('should be navigate to "/device-only/ais/summary" when call onNext', () => {
+      const navigateSpy = spyOn(router, 'navigate');
+      component.onNext();
+      expect(navigateSpy).toHaveBeenCalledWith([ROUTE_DEVICE_ONLY_AIS_SUMMARY_PAGE]);
+    });
+
+    it('should be go to home when call onHome', () => {
+      const goHomeSpy = spyOn(homeService, 'goToHome');
+      component.onHome();
+      expect(goHomeSpy).toHaveBeenCalled();
+    });
+
+    it('should be update mobileCarePackage in transaction object when call onCompleted', () => {
+      component.onCompleted({id: 1, title: 'Mobile Care Mock'});
+
+      expect(component.transaction.data.mobileCarePackage['id']).toBe(1);
+      expect(component.transaction.data.mobileCarePackage['title']).toBe('Mobile Care Mock');
+    });
+
+    it('should be go to home when call onHome', () => {
+      component.onCompleted({id: 1, title: 'Mobile Care Mock'});
+
+      component.ngOnDestroy();
+
+      expect(component.transactionService.update).toHaveBeenCalledWith({
+        data: {
+          mobileCarePackage: {
+            id: 1,
+            title: 'Mobile Care Mock'
+          }
+        }
+      });
+    });
   });
 });
