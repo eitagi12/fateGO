56c69348b00b1581258ca128165dc01c6bf45a8f
kawinpop <kawinpop123@gmail.com>
2019-04-09 16:02:30 +0700

    Add order prepaid

--- src/app/device-order/ais/device-order-ais-existing-prepaid-hotdeal/containers/device-order-ais-existing-prepaid-hotdeal-aggregate-page/device-order-ais-existing-prepaid-hotdeal-aggregate-page.component.ts
+++ src/app/device-order/ais/device-order-ais-existing-prepaid-hotdeal/containers/device-order-ais-existing-prepaid-hotdeal-aggregate-page/device-order-ais-existing-prepaid-hotdeal-aggregate-page.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { HomeService } from 'mychannel-shared-libs';
+import { HomeService, Aggregate } from 'mychannel-shared-libs';
 import {
   ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_SUMMARY_PAGE,
   ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_QUEUE_PAGE
 } from '../../constants/route-path.constant';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
+import { PriceOptionService } from 'src/app/shared/services/price-option.service';
+import { Transaction } from 'src/app/shared/models/transaction.model';
+import { PriceOption } from 'src/app/shared/models/price-option.model';
 
 @Component({
   selector: 'app-device-order-ais-existing-prepaid-hotdeal-aggregate-page',
@@ -13,12 +17,34 @@ import {
 })
 export class DeviceOrderAisExistingPrepaidHotdealAggregatePageComponent implements OnInit {
 
+  transaction: Transaction;
+  aggregate: Aggregate;
+  priceOption: PriceOption;
+
   constructor(
     private router: Router,
     private homeService: HomeService,
-  ) { }
+    private transactionService: TransactionService,
+    private priceOptionService: PriceOptionService
+  ) {
+    this.transaction = this.transactionService.load();
+    this.priceOption = this.priceOptionService.load();
+   }
 
   ngOnInit(): void {
+  }
+
+  getThumbnail(): string {
+    const product = (this.priceOption.productDetail.products || []).find((p: any) => {
+      return p.colorName === this.priceOption.productStock.color;
+    });
+    return product && product.images ? product.images.thumbnail : '';
+  }
+
+  summary(amount: number[]): number {
+    return amount.reduce((prev, curr) => {
+      return prev + curr;
+    }, 0);
   }
 
   onNext(): void {
