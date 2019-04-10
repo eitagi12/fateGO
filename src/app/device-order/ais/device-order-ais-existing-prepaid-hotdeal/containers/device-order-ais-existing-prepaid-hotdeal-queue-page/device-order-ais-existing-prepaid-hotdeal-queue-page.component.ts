4da2fc3b8de5120e8a7518a6b58378fabddc7c0a
kawinpop <kawinpop123@gmail.com>
2019-04-10 11:32:46 +0700

    create order prepaid

--- src/app/device-order/ais/device-order-ais-existing-prepaid-hotdeal/containers/device-order-ais-existing-prepaid-hotdeal-queue-page/device-order-ais-existing-prepaid-hotdeal-queue-page.component.ts
+++ src/app/device-order/ais/device-order-ais-existing-prepaid-hotdeal/containers/device-order-ais-existing-prepaid-hotdeal-queue-page/device-order-ais-existing-prepaid-hotdeal-queue-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
-import { HomeService, TokenService, PageLoadingService, REGEX_MOBILE } from 'mychannel-shared-libs';
+import { HomeService, TokenService, PageLoadingService, REGEX_MOBILE, User } from 'mychannel-shared-libs';
 import { ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_AGGREGATE_PAGE, ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_RESULT_PAGE } from '../../constants/route-path.constant';
-import { Transaction } from 'src/app/shared/models/transaction.model';
+import { Transaction, Customer, Payment, Prebooking, TransactionData, MainPackage } from 'src/app/shared/models/transaction.model';
 import { PriceOption } from 'src/app/shared/models/price-option.model';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
@@ -18,7 +18,11 @@ export class DeviceOrderAisExistingPrepaidHotdealQueuePageComponent implements O
 
   transaction: Transaction;
   priceOption: PriceOption;
+  mobileFrom: FormGroup;
   queueFrom: FormGroup;
+  queue: string;
+  user: User;
+  mobileNo: string;
 
   constructor(
     private fb: FormBuilder,
@@ -44,8 +48,240 @@ export class DeviceOrderAisExistingPrepaidHotdealQueuePageComponent implements O
     });
   }
 
+  onSendSMSQueue(mobileNo: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+        return this.http.post('/api/salesportal/device-order/transaction/get-queue-qmatic', {
+          mobileNo: mobileNo
+        }).toPromise()
+          .then((respQueue: any) => {
+            const data = respQueue.data && respQueue.data.result ? respQueue.data.result : {};
+            resolve(data.queueNo);
+          });
+    });
+  }
+
+  getRequestDeviceSellOrder(): any {
+    const user = this.tokenService.getUser();
+    const productStock = this.priceOption.productStock;
+
+    const trade = this.priceOption.trade;
+    const data = this.transaction.data;
+    const customerGroup = this.priceOption.customerGroup;
+
+    const discount = trade.discount;
+    const customer = data.customer;
+    const simCard = data.simCard;
+    const order = data.order;
+    const queue = data.queue;
+    const payment = data.payment;
+    const advancePay = data.advancePayment;
+    const paymentBank = payment.paymentBank;
+
+    return {
+      userId: user.username,
+      locationSource: user.locationCode,
+      locationReceipt: user.locationCode,
+      soCompany: productStock.company,
+      productType: productStock.productType,
+      productSubType: productStock.productSubType,
+      brand: productStock.brand,
+      model: productStock.model,
+      color: productStock.color,
+      matCode: '',
+      priceIncAmt: Number((+trade.normalPrice || 0)),
+      tradeNo: trade.tradeNo || '',
+      ussdCode: trade.ussdCode || '',
+      priceDiscountAmt: Number((+discount.amount || 0)),
+      // grandTotalAmt: this.getGrandTotalAmt(normalPrice, advancePay, discount, depositAmt, useDeposit),
+      soId: order.soId,
+      queueNo: queue.queueNo || '',
+      cusNameOrder: `${customer.titleName || ''} ${customer.firstName || ''} ${customer.lastName || ''}`.trim(),
+      taxCardId: customer.idCardNo || '',
+      customerAddress: {
+        addrNo: customer.homeNo,
+        amphur: customer.amphur,
+        buildingName: customer.buildingName,
+        country: 'ประเทศไทย',
+        floor: customer.floor,
+        moo: customer.moo,
+        mooban: customer.mooBan,
+        postCode: customer.zipCode,
+        province: customer.province.replace(/มหานคร$/, ''),
+        room: customer.room,
+        soi: customer.soi,
+        streetName: customer.street,
+        tumbon: customer.tumbol
+      },
+      cusMobileNoOrder: simCard.mobileNo || '',
+      returnCode: simCard.privilegeCode || '4GEYYY',
+      // cashBackFlg: cashBackFlg,
+      matAirTime: trade.matAirtime || '',
+      // matCodeFreeGoods: matCodeFreeGoods,
+      paymentRemark: this.getOrderRemark(trade, data),
+      installmentTerm: paymentBank && paymentBank.paymentMethod.month ? paymentBank.paymentMethod.month : 0,
+      installmentRate: paymentBank && paymentBank.paymentMethod.percentage ? paymentBank.paymentMethod.percentage : 0,
+      mobileAisFlg: 'Y',
+      paymentMethod: this.getPaymentMethod(trade, advancePay),
+      bankCode: this.getBankCode(trade, payment, advancePay),
+      tradeFreeGoodsId: trade.freeGoods[0] && trade.freeGoods[0].tradeFreegoodsId ? trade.freeGoods[0].tradeFreegoodsId : '',
+      tradeDiscountId: trade.discount.tradeDiscountId || '',
+      tradeAirtimeId: trade.advancePay.tradeAirtimeId || '',
+      bankAbbr: this.getBankCode(trade, payment, advancePay),
+      convertToNetwotkType: customerGroup.code === 'MC002' ? '3G POSTPAID' : undefined
+    };
+  }
+
+  getPaymentMethod(trade: any, advancePay: Payment): string {
+    let paymentMethod;
+    if (trade && trade.payments && trade.payments.length > 0) {
+      paymentMethod = trade.payments[0].method;
+
+      if (trade.advancePay.installmentFlag === 'Y') {
+        return paymentMethod;
+      }
+
+      if (trade.amount > 0) {
+        if (advancePay.paymentType !== 'CREDIT') {
+          paymentMethod = paymentMethod + '|CA';
+        }
+      }
+
+    }
+    return paymentMethod;
+  }
+
+  getBankCode(trade: any, payment: Payment, advancePay: Payment): string {
+
+    let bankCode: string = payment.paymentBank.abb;
+
+    if (trade.advancePay.installmentFlag === 'Y') { // ผ่อนรวม
+      return bankCode;
+    }
+
+    if (trade.amount > 0) {
+      if (advancePay.paymentType === 'CREDIT') {
+        bankCode = payment.paymentBank.abb + '|' + advancePay.paymentBank.abb;
+      } else {
+        bankCode = payment.paymentBank.abb;
+      }
+    }
+    return bankCode;
+  }
+  getOrderRemark(trade: any, data: TransactionData): string {
+    const NEW_LINE = '\n';
+
+    const remark = this.getRemarkCampaignName(data.mainPackage) + NEW_LINE
+      + this.getRemarkAdvancePay(trade, data.advancePayment) + NEW_LINE
+      + this.getRemarkTradeAndInstallment(trade, data.payment) + NEW_LINE
+      + this.getRemarkOtherInfomation(data) + NEW_LINE;
+
+    return remark;
+  }
+  getRemarkCampaignName(mainPackage: MainPackage): string {
+    const promotionName = '[PM]';
+    return promotionName + ' ' + mainPackage.shortNameThai;
+  }
+  getRemarkAdvancePay(trade: any, advancePay: Payment): string {
+    let airTime = '';
+    if (trade.advancePay.installmentFlag !== 'Y' && +trade.advancePay.amount !== 0) {
+      airTime += '[AT]';
+      if (advancePay.paymentType !== 'CREDIT') {
+        if (advancePay.paymentType === 'QR_CODE') {
+          if (advancePay.paymentQrCodeType === 'THAI_QR') {
+            airTime += '[PB]'; // Thai QRcode
+          } else {
+            airTime += '[RL]'; // Rabbit line pay
+          }
+        } else {
+          airTime += '[CA]';
+        }
+
+        return airTime;
+      } else {
+        airTime += '[AT]';
+        airTime += '[CC]' + ',' + ' ';
+        airTime += '[B]' + advancePay.paymentBank.abb;
+        return airTime;
+      }
+    } else {
+      return airTime;
+    }
+  }
+  getRemarkTradeAndInstallment(trade: any, payment: Payment): string {
+
+    let tradeAndInstallment = '';
+    if (+trade.advancePay.amount !== 0) {
+      if (trade.advancePay.installmentFlag === 'Y') {
+        tradeAndInstallment = '[AD]';
+      } else {
+        tradeAndInstallment = '[DV]';
+        if (payment.paymentType !== 'CREDIT') {
+          if (payment.paymentType === 'QR_CODE') {
+            if (payment.paymentQrCodeType === 'THAI_QR') {
+              tradeAndInstallment += '[PB]' + ',' + ' '; // Thai QRcode
+            } else {
+              tradeAndInstallment += '[RL]' + ',' + ' '; // Rabbit line pay
+            }
+          }
+        }
+      }
+    } else {
+      tradeAndInstallment = '[AD]';
+      if (payment.paymentType !== 'CREDIT') {
+        if (payment.paymentType === 'QR_CODE') {
+          if (payment.paymentQrCodeType === 'THAI_QR') {
+            tradeAndInstallment += '[PB]' + ',' + ' '; // Thai QRcode
+          } else {
+            tradeAndInstallment += '[RL]' + ',' + ' '; // Rabbit line pay
+          }
+        }
+        tradeAndInstallment += '[CA]' + ',' + ' ';
+      } else {
+        tradeAndInstallment += '[CC]' + ',' + ' ';
+        tradeAndInstallment += '[B]' + ',' + ' ';
+        if (payment.paymentBank.installmentMonth > 0) {
+          tradeAndInstallment += '[I]' + payment.paymentBank.percentage + '%' + ' ' + payment.paymentBank.month + 'เดือน' + ',' + ' ';
+        }
+      }
+    }
+    tradeAndInstallment += '[T]' + trade.tradeNo;
+    return tradeAndInstallment;
+  }
+  getRemarkOtherInfomation(data: TransactionData): string {
+    let otherInformation = '';
+    otherInformation += '[SP]' + ' ' + 0 + ',' + ' ';
+    otherInformation += '[SD]' + ' ' + 0 + ',' + ' ';
+    // otherInformation += '[D]' + ' ' + this.getDiscountAmount(remark.discount) + ',' + ' ';
+    // otherInformation += '[RC]' + ' ' + privilegeCode + ',' + ' ';
+    // otherInformation += '[OT]' + ' ' + this.getCustomerType(remark.customerGroupCode) + ',' + ' ';
+    // otherInformation += '[PC]' + ' ' + remark.mainPackageCode + ',' + ' ';
+    // otherInformation += '[MCC]' + ' ' + remark.mobileCare + ',' + ' ';
+    // otherInformation += '[MC]' + ' ' + remark.mobileCareName + ',' + ' ';
+    // otherInformation += '[PN]' + ' ' + remark.privilegeDesc + ',' + ' ';
+    // otherInformation += '[Q]' + ' ' + remark.queueNumber;
+    return otherInformation;
+  }
+
   onNext(): void {
-    this.router.navigate([ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_RESULT_PAGE]);
+    this.pageLoadingService.openLoading();
+    this.http.post('/api/salesportal/device-sell/order', this.getRequestDeviceSellOrder()).toPromise()
+    .then(() => {
+      return this.http.post('/api/salesportal/device-order/transaction/get-queue-qmatic', {
+        mobileNo: this.queueFrom.value.mobileNo
+      }).toPromise()
+        .then((respQueue: any) => {
+          const data = respQueue.data && respQueue.data.result ? respQueue.data.result : {};
+          return data.queueNo;
+        });
+    })
+    .then((queueNo: string) => {
+      this.transaction.data.queue = {
+        queueNo: queueNo
+      };
+      // update transaction
+      this.router.navigate([ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_RESULT_PAGE]);
+    })
+    .then(() => this.pageLoadingService.closeLoading());
   }
 
   onBack(): void {
--- src/app/device-order/ais/device-order-ais-existing-prepaid-hotdeal/containers/device-order-ais-existing-prepaid-hotdeal-validate-customer-id-card-page/device-order-ais-existing-prepaid-hotdeal-validate-customer-id-card-page.component.ts
+++ src/app/device-order/ais/device-order-ais-existing-prepaid-hotdeal/containers/device-order-ais-existing-prepaid-hotdeal-validate-customer-id-card-page/device-order-ais-existing-prepaid-hotdeal-validate-customer-id-card-page.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { HomeService, ValidateCustomerIdCardComponent, ReadCardProfile, PageLoadingService, TokenService, AlertService, Utils, KioskControls, ChannelType, ChargeType } from 'mychannel-shared-libs';
+import { HomeService, ValidateCustomerIdCardComponent, ReadCardProfile, PageLoadingService, TokenService, AlertService, Utils, KioskControls, ChannelType, ChargeType, User } from 'mychannel-shared-libs';
 import { ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_ELIGIBLE_MOBILE_PAGE } from '../../constants/route-path.constant';
 import { Transaction, TransactionType, TransactionAction } from 'src/app/shared/models/transaction.model';
 import { PriceOption } from 'src/app/shared/models/price-option.model';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
 import { PriceOptionService } from 'src/app/shared/services/price-option.service';
 import { HttpClient } from '@angular/common/http';
+import { SharedTransactionService } from 'src/app/shared/services/shared-transaction.service';
 
 @Component({
   selector: 'app-device-order-ais-existing-prepaid-hotdeal-validate-customer-id-card-page',
@@ -22,6 +23,7 @@ export class DeviceOrderAisExistingPrepaidHotdealValidateCustomerIdCardPageCompo
   zipcode: string;
   readCardValid: boolean;
   priceOption: PriceOption;
+  user: User;
 
   @ViewChild(ValidateCustomerIdCardComponent)
   validateCustomerIdcard: ValidateCustomerIdCardComponent;
@@ -35,7 +37,8 @@ export class DeviceOrderAisExistingPrepaidHotdealValidateCustomerIdCardPageCompo
     private tokenService: TokenService,
     private utils: Utils,
     private alertService: AlertService,
-    private priceOptionService: PriceOptionService
+    private priceOptionService: PriceOptionService,
+    private sharedTransactionService: SharedTransactionService,
   ) {
     this.homeService.callback = () => {
       if (this.validateCustomerIdcard.koiskApiFn) {
@@ -43,6 +46,7 @@ export class DeviceOrderAisExistingPrepaidHotdealValidateCustomerIdCardPageCompo
       }
       window.location.href = '';
     };
+    this.user = this.tokenService.getUser();
     this.kioskApi = this.tokenService.getUser().channelType === ChannelType.SMART_ORDER;
     this.priceOption = this.priceOptionService.load();
   }
@@ -73,9 +77,60 @@ export class DeviceOrderAisExistingPrepaidHotdealValidateCustomerIdCardPageCompo
   onNext(): void {
     this.pageLoadingService.openLoading();
 
-    this.getZipCode(this.profile.province, this.profile.amphur, this.profile.tumbol)
+    // this.getZipCode(this.profile.province, this.profile.amphur, this.profile.tumbol)
+    //   .then((zipCode: string) => {
+    //     return this.http.get('/api/customerportal/validate-customer-prepaid-hotdeal', {
+    //       params: {
+    //         identity: this.profile.idCardNo
+    //       }
+    //     }).toPromise()
+    //       .then((resp: any) => {
+    //         const data = resp.data || {};
+    //         return {
+    //           caNumber: data.caNumber,
+    //           mainMobile: data.mainMobile,
+    //           billCycle: data.billCycle,
+    //           zipCode: zipCode
+    //         };
+    //       }).catch(() => {
+    //         return {
+    //           zipCode: zipCode
+    //         };
+    //       });
+    //   })
+    //   .then((customer: any) => { // load bill cycle
+    //     this.transaction.data.customer = Object.assign(this.profile, customer);
+
+    //     return this.http.get(`/api/customerportal/newRegister/${this.profile.idCardNo}/queryBillingAccount`).toPromise()
+    //       .then((resp: any) => {
+    //         const data = resp.data || {};
+    //         return this.http.post('/api/customerportal/verify/billingNetExtreme', {
+    //           businessType: '1',
+    //           listBillingAccount: data.billingAccountList
+    //         }).toPromise()
+    //           .then((respBillingNetExtreme: any) => {
+    //             return {
+    //               billCycles: data.billingAccountList,
+    //               billCyclesNetExtreme: respBillingNetExtreme.data
+    //             };
+    //           })
+    //           .catch(() => {
+    //             return {
+    //               billCycles: data.billingAccountList
+    //             };
+    //           });
+    //       });
+    //   })
+    //   .then((billingInformation: any) => {
+    //     this.transaction.data.billingInformation = billingInformation;
+    //     if (this.checkBusinessLogic()) {
+    //       this.router.navigate([ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_ELIGIBLE_MOBILE_PAGE]);
+    //     }
+    //   });
+
+      this.getZipCode(this.profile.province, this.profile.amphur, this.profile.tumbol)
       .then((zipCode: string) => {
-        return this.http.get('/api/customerportal/validate-customer-new-register', {
+        return this.http.get('/api/customerportal/validate-customer-prepaid-hotdealr', {
           params: {
             identity: this.profile.idCardNo
           }
@@ -89,40 +144,79 @@ export class DeviceOrderAisExistingPrepaidHotdealValidateCustomerIdCardPageCompo
               zipCode: zipCode
             };
           }).catch(() => {
-            return {
-              zipCode: zipCode
-            };
+            return { zipCode: zipCode };
           });
       })
-      .then((customer: any) => { // load bill cycle
+      .then((customer: any) => {
+        // load bill cycle
         this.transaction.data.customer = Object.assign(this.profile, customer);
-
         return this.http.get(`/api/customerportal/newRegister/${this.profile.idCardNo}/queryBillingAccount`).toPromise()
           .then((resp: any) => {
             const data = resp.data || {};
-            return this.http.post('/api/customerportal/verify/billingNetExtreme', {
-              businessType: '1',
-              listBillingAccount: data.billingAccountList
-            }).toPromise()
-              .then((respBillingNetExtreme: any) => {
-                return {
-                  billCycles: data.billingAccountList,
-                  billCyclesNetExtreme: respBillingNetExtreme.data
-                };
-              })
-              .catch(() => {
-                return {
-                  billCycles: data.billingAccountList
-                };
-              });
+            return {
+              billCycles: data.billingAccountList
+            };
           });
-      })
-      .then((billingInformation: any) => {
+      }).then((billingInformation: any) => {
         this.transaction.data.billingInformation = billingInformation;
-        if (this.checkBusinessLogic()) {
-          this.router.navigate([ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_ELIGIBLE_MOBILE_PAGE]);
-        }
-      });
+
+        return this.conditionIdentityValid()
+          .then(() => {
+            return this.http.post(
+              '/api/salesportal/add-device-selling-cart',
+              this.getRequestAddDeviceSellingCart()
+            ).toPromise()
+              .then((resp: any) => resp.data.soId);
+          })
+          .then((soId: string) => {
+            this.transaction.data.order = { soId: soId };
+
+            return this.sharedTransactionService.createSharedTransaction(this.transaction, this.priceOption);
+          })
+          .then(() => this.router.navigate([ROUTE_DEVICE_ORDER_AIS_PREPAID_HOTDEAL_ELIGIBLE_MOBILE_PAGE]));
+
+      }).then(() => this.pageLoadingService.closeLoading());
+  }
+
+  conditionIdentityValid(): Promise<string> {
+    return new Promise((resovle, reject) => {
+
+      const birthdate = this.transaction.data.customer.birthdate; // '19/03/2560';
+      const expireDate = this.transaction.data.customer.expireDate;
+      const idCardType = this.transaction.data.customer.idCardType;
+
+      if (this.utils.isLowerAge17Year(birthdate)) {
+        return reject(`ไม่สามารถทำรายการได้ เนื่องจากอายุของผู้ใช้บริการต่ำกว่า 17 ปี`);
+      }
+      if (this.utils.isIdCardExpiredDate(expireDate)) {
+        return reject(`ไม่สามารถทำรายการได้ เนื่องจาก ${idCardType} หมดอายุ`);
+      }
+      resovle(null);
+    });
+  }
+
+  getRequestAddDeviceSellingCart(): any {
+    const productStock = this.priceOption.productStock;
+    const productDetail = this.priceOption.productDetail;
+    const customer = this.transaction.data.customer;
+    return {
+      soCompany: productStock.company || 'AWN',
+      locationSource: this.user.locationCode,
+      locationReceipt: this.user.locationCode,
+      productType: productDetail.productType || 'DEVICE',
+      productSubType: productDetail.productSubType || 'HANDSET',
+      brand: productStock.brand,
+      model: productDetail.model,
+      color: productStock.color,
+      priceIncAmt: '',
+      priceDiscountAmt: '',
+      grandTotalAmt: '',
+      userId: this.user.username,
+      cusNameOrder: `${customer.firstName || ''} ${customer.lastName || ''}`.trim(),
+      preBookingNo: '',
+      depositAmt: '',
+      reserveNo: ''
+    };
   }
 
   checkBusinessLogic(): boolean {
