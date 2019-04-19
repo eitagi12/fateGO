c594bb8dbfab64df35cec9b88a34468847cd78d4
Bart <kijja85@gmail.com>
2019-04-19 20:27:29 +0700

    [FIX] Case

--- src/app/device-order/ais/device-order-ais-existing-best-buy/containers/device-order-ais-existing-best-buy-validate-customer-repi-page/device-order-ais-existing-best-buy-validate-customer-repi-page.component.ts
+++ src/app/device-order/ais/device-order-ais-existing-best-buy/containers/device-order-ais-existing-best-buy-validate-customer-repi-page/device-order-ais-existing-best-buy-validate-customer-repi-page.component.ts
@@ -93,22 +93,7 @@ export class DeviceOrderAisExistingBestBuyValidateCustomerRepiPageComponent impl
             this.transaction.data.customer.repi = repi;
           }
           this.transaction.data.billingInformation = {};
-          const addressCustomer = this.transaction.data.customer;
-          this.transaction.data.billingInformation.billDeliveryAddress = {
-            homeNo: addressCustomer.homeNo,
-            moo: addressCustomer.moo,
-            mooBan: addressCustomer.mooBan,
-            room: addressCustomer.room,
-            floor: addressCustomer.floor,
-            buildingName: addressCustomer.buildingName,
-            soi: addressCustomer.soi,
-            street: addressCustomer.street,
-            province: addressCustomer.province,
-            amphur: addressCustomer.amphur,
-            tumbol: addressCustomer.tumbol,
-            zipCode: addressCustomer.zipCode
-          };
-
+          this.transaction.data.billingInformation.billDeliveryAddress = this.transaction.data.customer;
           this.pageLoadingService.closeLoading();
           if (customerInfo.caNumber) {
             this.router.navigate([ROUTE_DEVICE_ORDER_AIS_BEST_BUY_CUSTOMER_INFO_PAGE]);
