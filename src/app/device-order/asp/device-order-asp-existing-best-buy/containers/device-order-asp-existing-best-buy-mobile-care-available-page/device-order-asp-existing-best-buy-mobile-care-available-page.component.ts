99ce53020c96e5af47d42e6a446c12f9a0519502
Bart <kijja85@gmail.com>
2019-04-22 12:23:48 +0700

    [FIX] not change mobile Care

--- src/app/device-order/asp/device-order-asp-existing-best-buy/containers/device-order-asp-existing-best-buy-mobile-care-available-page/device-order-asp-existing-best-buy-mobile-care-available-page.component.ts
+++ src/app/device-order/asp/device-order-asp-existing-best-buy/containers/device-order-asp-existing-best-buy-mobile-care-available-page/device-order-asp-existing-best-buy-mobile-care-available-page.component.ts
@@ -21,6 +21,7 @@ export class DeviceOrderAspExistingBestBuyMobileCareAvailablePageComponent imple
 
   wizards: any = this.tokenService.isTelewizUser() ? WIZARD_DEVICE_ORDER_ASP : WIZARD_DEVICE_ORDER_AIS;
   active: number = this.tokenService.isTelewizUser() ? 4 : 3;
+  reason: any = 'ยืนยันใช้ Mobile Care กับเครื่องเดิม';
 
   identityValid: boolean = false;
   transaction: Transaction;
@@ -80,7 +81,7 @@ export class DeviceOrderAspExistingBestBuyMobileCareAvailablePageComponent imple
       if (value.changeMobileCare === 'Yes') {
         this.changeMobileCare = true;
       } else {
-        this.transaction.data.mobileCarePackage = null;
+        this.transaction.data.mobileCarePackage = this.reason;
         this.changeMobileCare = false;
       }
       this.identityValid = true;
