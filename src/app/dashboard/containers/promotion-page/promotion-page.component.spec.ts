30b06dfae6fd39fdebe14aa9ba61e4ad6fc5a103
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-04-23 11:31:32 +0700

    setup jest

--- src/app/dashboard/containers/promotion-page/promotion-page.component.spec.ts
+++ src/app/dashboard/containers/promotion-page/promotion-page.component.spec.ts
@@ -1,17 +1,36 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PromotionPageComponent } from './promotion-page.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ModalModule } from 'ngx-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { TokenService } from 'mychannel-shared-libs';
 
 describe('PromotionPageComponent', () => {
   let component: PromotionPageComponent;
   let fixture: ComponentFixture<PromotionPageComponent>;
+  const mockTokenService: any = {
+    provide: TokenService,
+    useValue: {
+      getUser: () => {
+        return { locationCode: '' };
+      }
+    }
+  };
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PromotionPageComponent ]
-    })
-    .compileComponents();
-  }));
+  setupTestBed({
+    imports: [
+      RouterTestingModule,
+      HttpClientModule,
+      ModalModule.forRoot()
+    ],
+    schemas: [
+    ],
+    declarations: [
+      PromotionPageComponent
+    ],
+    providers: [mockTokenService
+    ]
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PromotionPageComponent);
---
+++ src/jest.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    "preset": "jest-preset-angular",
+    "setupFilesAfterEnv": ["<rootDir>/src/setupJest.ts"],
+    "transform": {
+      "^.+\\.(ts|html)$": "<rootDir>/node_modules/jest-preset-angular/preprocessor.js",
+      "^.+\\.js$": "babel-jest"
+    },
+    "rootDir": ".",
+    "globals": {
+      "ts-jest": {
+        "tsConfigFile": "<rootDir>/src/app/tsconfig.spec.json"
+      },
+      "__TRANSFORM_HTML__": true
+    },
+    "testMatch": [
+      "<rootDir>/**/*.spec.ts"
+    ],
+    "moduleFileExtensions": [
+      "ts",
+      "js",
+      "html",
+      "json"
+    ],
+    "mapCoverage": true,
+    "coveragePathIgnorePatterns": [
+      "/node_modules/",
+      "/modules/*.*/"
+    ],
+    "moduleNameMapper": {
+      "^@ngrx/(?!db)(.*)": "<rootDir>/../../modules/$1",
+      "^@example-app/(.*)": "<rootDir>/src/app/$1"
+    },
+    "transformIgnorePatterns": [
+      "node_modules/(?!mychannel-shared-libs|@angular/core|rxjs|@auth0/angular-jwt)"
+    ],
+    "modulePathIgnorePatterns": [
+      "dist"
+    ],
+    "testMatch": [
+      "<rootDir>/src/app/dashboard/**/*.spec.ts"
+    ],
+    "setupTestFrameworkScriptFile": "<rootDir>/src/setupJest.ts",
+    "coverageReporters": [
+      "text",
+      "cobertura",
+      "html"
+    ]
+  };
