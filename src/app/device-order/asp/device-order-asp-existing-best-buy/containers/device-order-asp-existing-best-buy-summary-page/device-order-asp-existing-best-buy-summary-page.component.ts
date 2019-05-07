1bac8b381704dc7ad73eb84f2bfefcede1af6a05
Bart <kijja85@gmail.com>
2019-05-07 15:29:44 +0700

    [FIX] Issue Back

--- src/app/device-order/asp/device-order-asp-existing-best-buy/containers/device-order-asp-existing-best-buy-summary-page/device-order-asp-existing-best-buy-summary-page.component.ts
+++ src/app/device-order/asp/device-order-asp-existing-best-buy/containers/device-order-asp-existing-best-buy-summary-page/device-order-asp-existing-best-buy-summary-page.component.ts
@@ -103,7 +103,7 @@ export class DeviceOrderAspExistingBestBuySummaryPageComponent implements OnInit
     }
     const mobileCarePackage: any = this.transaction.data.mobileCarePackage;
 
-    if (changeMobileCareFlag &&  mobileCarePackage !== 'ยืนยันใช้ Mobile Care กับเครื่องเดิม') {
+    if ((!this.transaction.data.existingMobileCare) || (changeMobileCareFlag && mobileCarePackage !== 'ยืนยันใช้ Mobile Care กับเครื่องเดิม')) {
       this.router.navigate([ROUTE_DEVICE_ORDER_ASP_BEST_BUY_MOBILE_CARE_PAGE]);
     } else {
       this.router.navigate([ROUTE_DEVICE_ORDER_ASP_BEST_BUY_MOBILE_CARE_AVAILABLE_PAGE]);
