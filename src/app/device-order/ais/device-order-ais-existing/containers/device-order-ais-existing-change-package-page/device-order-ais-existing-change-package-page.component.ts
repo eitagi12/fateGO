346a0c01984bdf2522a49707857f605fa8ab9faf
meths418 <meths418@saraf.postbox.in.th>
2019-05-08 15:33:27 +0700

    add check privilege and requestUsePrivilege to repi device convert and mc shoping card to device existing

--- src/app/device-order/ais/device-order-ais-existing/containers/device-order-ais-existing-change-package-page/device-order-ais-existing-change-package-page.component.ts
+++ src/app/device-order/ais/device-order-ais-existing/containers/device-order-ais-existing-change-package-page/device-order-ais-existing-change-package-page.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ROUTE_DEVICE_ORDER_AIS_EXISTING_ELIGIBLE_MOBILE_PAGE, ROUTE_DEVICE_ORDER_AIS_EXISTING_MOBILE_DETAIL_PAGE } from '../../constants/route-path.constant';
-import { HomeService } from 'mychannel-shared-libs';
+import { HomeService, ShoppingCart } from 'mychannel-shared-libs';
 import { WIZARD_DEVICE_ORDER_AIS } from 'src/app/device-order/constants/wizard.constant';
 import { Transaction } from 'src/app/shared/models/transaction.model';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
 import { PriceOptionService } from 'src/app/shared/services/price-option.service';
 import { PriceOption } from 'src/app/shared/models/price-option.model';
+import { ShoppingCartService } from 'src/app/device-order/services/shopping-cart.service';
 
 @Component({
   selector: 'app-device-order-ais-existing-change-package-page',
@@ -18,17 +19,21 @@ export class DeviceOrderAisExistingChangePackagePageComponent implements OnInit 
   wizards: string[] = WIZARD_DEVICE_ORDER_AIS;
   transaction: Transaction;
   priceOption: PriceOption;
+  shoppingCart: ShoppingCart;
+
   constructor(
     private router: Router,
     private homeService: HomeService,
     private transactionService: TransactionService,
-    private priceOptionService: PriceOptionService
+    private priceOptionService: PriceOptionService,
+    private shoppingCartService: ShoppingCartService
   ) {
     this.transaction = this.transactionService.load();
     this.priceOption = this.priceOptionService.load();
   }
 
   ngOnInit(): void {
+    this.shoppingCart = this.shoppingCartService.getShoppingCartData();
   }
 
   onBack(): void {
