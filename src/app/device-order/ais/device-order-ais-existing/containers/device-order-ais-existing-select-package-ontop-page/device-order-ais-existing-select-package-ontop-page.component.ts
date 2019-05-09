a02f13a21d9f1258fa3b9859e2209a7ae1e010f0
sirikan <sirika49@aware.postbox.in.th>
2019-05-09 09:43:54 +0700

    select package ontop

---
+++ src/app/device-order/ais/device-order-ais-existing/containers/device-order-ais-existing-select-package-ontop-page/device-order-ais-existing-select-package-ontop-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-device-order-ais-existing-select-package-ontop-page',
+  templateUrl: './device-order-ais-existing-select-package-ontop-page.component.html',
+  styleUrls: ['./device-order-ais-existing-select-package-ontop-page.component.scss']
+})
+export class DeviceOrderAisExistingSelectPackageOntopPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
