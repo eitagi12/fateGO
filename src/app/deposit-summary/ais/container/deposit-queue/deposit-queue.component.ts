727719e855c98fc27a8efbc98d6d8d8adc166dd5
kessirin <kessirin296@gmail.com>
2019-05-21 16:51:22 +0700

    update queue page for deposit flow

--- src/app/deposit-summary/ais/container/deposit-queue/deposit-queue.component.ts
+++ src/app/deposit-summary/ais/container/deposit-queue/deposit-queue.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
-import { PageLoadingService, AlertService } from 'mychannel-shared-libs';
+import { PageLoadingService, AlertService, User, TokenService } from 'mychannel-shared-libs';
 import { DEPOSIT_RESULT_PAGE, DEPOSIT_PAYMENT_SUMMARY_PAGE} from '../../../constants/route-path.constant';
 import { Transaction } from 'src/app/shared/models/transaction.model';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
@@ -11,6 +11,8 @@ import { CreateDeviceOrderService } from '../../../services/create-device-order.
 import { PriceOption } from 'src/app/shared/models/price-option.model';
 import { PriceOptionService } from 'src/app/shared/services/price-option.service';
 import { WIZARD_RESERVE_WITH_DEPOSIT } from '../../../constants/wizard.constant';
+import { SharedTransactionService } from 'src/app/shared/services/shared-transaction.service';
+import { QueuePageService } from 'src/app/device-order/services/queue-page.service';
 
 @Component({
   selector: 'app-deposit-queue',
@@ -21,26 +23,69 @@ export class DepositQueueComponent implements OnInit, OnDestroy {
 
   transaction: Transaction;
   priceOption: PriceOption;
+  mobileFrom: FormGroup;
   queueFrom: FormGroup;
   queue: string;
   wizards: any = WIZARD_RESERVE_WITH_DEPOSIT;
+  user: User;
+  mobileNo: string;
+  queueType: string;
+  inputType: string;
+  errorQueue: boolean = false;
+  skipQueue: boolean = false;
+
   constructor(
     private router: Router,
     private fb: FormBuilder,
+    private http: HttpClient,
     private transactionService: TransactionService,
     private createOrderService: CreateDeviceOrderService,
     private alertService: AlertService,
-    private priceOptionService: PriceOptionService
+    private priceOptionService: PriceOptionService,
+    private pageLoadingService: PageLoadingService,
+    private tokenService: TokenService,
+    private sharedTransactionService: SharedTransactionService,
+    private queuePageService: QueuePageService
   ) {
     this.transaction = this.transactionService.load();
     this.priceOption = this.priceOptionService.load();
+    this.user = this.tokenService.getUser();
   }
 
   ngOnInit(): void {
+    this.setQueueType();
     this.createForm();
   }
 
+  setQueueType(): void {
+    this.queuePageService.checkQueueLocation().then((queueType) => {
+      this.queueType = queueType;
+    }).then(() => this.createForm());
+  }
+
+  checkInput(event: any, type: string): void {
+    this.inputType = type;
+    if (type === 'mobileNo') {
+      this.queueFrom.reset();
+    } else {
+      this.mobileFrom.reset();
+    }
+  }
+
   createForm(): void {
+    this.mobileFrom = this.fb.group({
+      'mobileNo': ['', Validators.compose([Validators.required, Validators.pattern(/^0[6-9]{1}[0-9]{8}/)])],
+    });
+
+    if (this.transaction.data.simCard.mobileNo && this.queueType === 'SMART_SHOP') {
+      this.mobileFrom.patchValue({ mobileNo: this.transaction.data.simCard.mobileNo });
+      this.mobileNo = this.transaction.data.simCard.mobileNo;
+    }
+
+    this.mobileFrom.valueChanges.subscribe((value) => {
+      this.mobileNo = value.mobileNo;
+    });
+
     this.queueFrom = this.fb.group({
       'queue': ['', Validators.compose([Validators.required, Validators.pattern(/([A-Y]{1}[0-9]{3})/)])],
     });
@@ -48,6 +93,22 @@ export class DepositQueueComponent implements OnInit, OnDestroy {
     this.queueFrom.valueChanges.subscribe((value) => {
       this.queue = value.queue;
     });
+  }
+
+  onSkip(): void {
+    this.http.get('/api/salesportal/device-sell/gen-queue', { params: { locationCode: this.user.locationCode } }).toPromise()
+      .then((resp: any) => {
+        const queueNo = resp.data.queue;
+        this.skipQueue = true;
+        this.transaction.data.queue = { queueNo: queueNo };
+        this.queuePageService.createDeviceSellingOrder(this.transaction, this.priceOption).then(() => {
+          return this.sharedTransactionService.updateSharedTransaction(this.transaction, this.priceOption).then(() => {
+            this.pageLoadingService.closeLoading();
+            // chang path
+            this.router.navigate([DEPOSIT_RESULT_PAGE]);
+          });
+        });
+      });
   }
 
   onNext(): void {
@@ -72,4 +133,52 @@ export class DepositQueueComponent implements OnInit, OnDestroy {
   onBack(): void {
     this.router.navigate([DEPOSIT_PAYMENT_SUMMARY_PAGE]);
   }
+
+  isLocationPhuket(): boolean {
+    return this.user.locationCode === '1213' && this.tokenService.isAisUser();
+  }
+
+  onSendSMSQueue(mobileNo: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if (this.isLocationPhuket()) {
+        return this.http.post('/api/salesportal/device-order/transaction/get-queue-qmatic', {
+          mobileNo: mobileNo
+        }).toPromise()
+          .then((respQueue: any) => {
+            const data = respQueue.data && respQueue.data.result ? respQueue.data.result : {};
+            resolve(data.queueNo);
+          }).catch((error) => {
+            reject(null);
+          });
+      } else {
+        return this.http.post('/api/salesportal/device-order/transaction/auto-gen-queue', {
+          mobileNo: mobileNo
+        }).toPromise()
+          .then((response: any) => {
+            if (response && response.data && response.data.data && response.data.data.queueNo) {
+              resolve(response.data.data.queueNo);
+            } else {
+              reject(null);
+            }
+          }).catch((error) => {
+            reject(null);
+          });
+      }
+    });
+  }
+
+  checkValid(): boolean {
+    if (this.queueType === 'AUTO_GEN_Q') {
+      return this.mobileFrom.invalid && this.queueFrom.invalid;
+    } else if (this.queueType === 'SMART_SHOP') {
+      return this.mobileFrom.invalid;
+    } else {
+      return this.queueFrom.invalid;
+    }
+  }
+
+  checkSkip(): boolean {
+    return this.mobileFrom.value['mobileNo'] ? true : false;
+  }
+
 }
