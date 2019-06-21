346b6e7f0c82bf9d8ee35621b457a5cfa3aabcd1
Atinan <atinan.rakkeadtipao@gmail.com>
2019-06-21 10:24:46 +0700

    edit route back smart digital

--- src/app/device-only/services/home-button.service.ts
+++ src/app/device-only/services/home-button.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Transaction } from 'src/app/shared/models/transaction.model';
-import { HomeService, AlertService } from 'mychannel-shared-libs';
+import { HomeService, AlertService, User, TokenService } from 'mychannel-shared-libs';
 import { CreateOrderService } from './create-order.service';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
 import { PriceOptionService } from 'src/app/shared/services/price-option.service';
@@ -10,27 +10,39 @@ import { PriceOptionService } from 'src/app/shared/services/price-option.service
 export class HomeButtonService {
 
   transaction: Transaction;
+  user: User;
 
   constructor(
     private homeService: HomeService,
     private alertService: AlertService,
     private createOrderService: CreateOrderService,
     private transactionService: TransactionService,
-    private priceOptionService: PriceOptionService
-  ) { }
+    private priceOptionService: PriceOptionService,
+    private tokenService: TokenService,
+  ) {
+    this.user = this.tokenService.getUser();
+  }
 
   initEventButtonHome(): void {
     this.homeService.callback = () => {
       this.alertService.question('ต้องการยกเลิกรายการขายหรือไม่ การยกเลิก ระบบจะคืนสินค้าเข้าสต๊อคสาขาทันที', 'ตกลง', 'ยกเลิก')
         .then((response: any) => {
+          console.log(response);
           if (response.value === true) {
             this.createOrderService.cancelOrder(this.transaction).then((isSuccess: any) => {
+              console.log('isSuccess', isSuccess);
+              console.log('localtioncode', this.user.locationCode);
               this.transactionService.remove();
               this.priceOptionService.remove();
-              window.location.href = '/';
+              if (this.user.locationCode === '1213') {
+                window.location.href = '/smart-digital/main-menu';
+              } else {
+                window.location.href = '/';
+              }
             });
           }
-        }).catch(() => {
+        }).catch((err: any) => {
+          console.log('err', err);
           this.transactionService.remove();
           this.priceOptionService.remove();
           window.location.href = '/';
