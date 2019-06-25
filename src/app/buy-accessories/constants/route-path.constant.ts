3d387526d7b6fdb2c37925a27a8f047273fdd7ef
Panyapron Wiratchadit <supermax2555@gmail.com>
2019-06-25 15:23:01 +0700

    add icon accessories

---
+++ src/app/buy-accessories/constants/route-path.constant.ts
@@ -0,0 +1,3 @@
+export const ROUTE_BUY_ACCESSORIES_BRAND_PAGE = '/buy-accessories/brand';
+export const ROUTE_BUY_ACCESSORIE_PRODUCT_PAGE = '/buy-accessories/product';
+export const ROUTE_BUY_ACCESSORIE_CAMPAIGN_PAGE = '/buy-accessories/campaign';
