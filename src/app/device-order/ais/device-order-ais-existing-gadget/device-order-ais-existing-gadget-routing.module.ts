296df2c49909892b7977eaec5df0947f37da8404
gun <witawassgunn@gmail.com>
2019-06-26 12:33:09 +0700

    create component validate customer page (gadget)

--- src/app/device-order/ais/device-order-ais-existing-gadget/device-order-ais-existing-gadget-routing.module.ts
+++ src/app/device-order/ais/device-order-ais-existing-gadget/device-order-ais-existing-gadget-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DeviceOrderAisExistingGadgetValidateCustomerPageComponent } from './containers/device-order-ais-existing-gadget-validate-customer-page/device-order-ais-existing-gadget-validate-customer-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'validate-customer', pathMatch: 'full' },
+  { path: 'validate-customer', component: DeviceOrderAisExistingGadgetValidateCustomerPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/device-order/ais/device-order-ais-existing-gadget/device-order-ais-existing-gadget.module.ts
+++ src/app/device-order/ais/device-order-ais-existing-gadget/device-order-ais-existing-gadget.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DeviceOrderAisExistingGadgetRoutingModule } from './device-order-ais-existing-gadget-routing.module';
+import { DeviceOrderAisExistingGadgetValidateCustomerPageComponent } from './containers/device-order-ais-existing-gadget-validate-customer-page/device-order-ais-existing-gadget-validate-customer-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TabsModule } from 'ngx-bootstrap';
+import { MyChannelSharedLibsModule } from 'mychannel-shared-libs';
 
 @NgModule({
   imports: [
     CommonModule,
-    DeviceOrderAisExistingGadgetRoutingModule
+    DeviceOrderAisExistingGadgetRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MyChannelSharedLibsModule,
+    TabsModule.forRoot()
   ],
   declarations: [
-
+    DeviceOrderAisExistingGadgetValidateCustomerPageComponent
   ]
 })
 export class DeviceOrderAisExistingGadgetModule { }
