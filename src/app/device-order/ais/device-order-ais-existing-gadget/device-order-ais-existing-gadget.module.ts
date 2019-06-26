c4b3c5cdd0e214dbafa3b4e3a459495eeda97d18
matsi masamin <matsim49@aware.postbox.in.th>
2019-06-26 11:46:26 +0700

    ลบไฟล์ที่ create ผิดที่ออก

--- src/app/device-order/ais/device-order-ais-existing-gadget/device-order-ais-existing-gadget.module.ts
+++ src/app/device-order/ais/device-order-ais-existing-gadget/device-order-ais-existing-gadget.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DeviceOrderAisExistingGadgetRoutingModule } from './device-order-ais-existing-gadget-routing.module';
-import { DeviceOrderAisExistingGadgetValidateCustomerPageComponent } from './device-order-ais-existing-gadget-validate-customer-page/device-order-ais-existing-gadget-validate-customer-page.component';
 
 @NgModule({
   imports: [
@@ -10,7 +9,7 @@ import { DeviceOrderAisExistingGadgetValidateCustomerPageComponent } from './dev
     DeviceOrderAisExistingGadgetRoutingModule
   ],
   declarations: [
-    DeviceOrderAisExistingGadgetValidateCustomerPageComponent
+
   ]
 })
 export class DeviceOrderAisExistingGadgetModule { }
