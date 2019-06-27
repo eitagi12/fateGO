d7fbc593a918bfbe3671d35a929c6fc99cc8f783
gun <witawassgunn@gmail.com>
2019-06-27 18:40:27 +0700

    edit path validate customer id card

--- src/app/device-order/ais/device-order-ais-existing-gadget/constants/route-path.constant.ts
+++ src/app/device-order/ais/device-order-ais-existing-gadget/constants/route-path.constant.ts
@@ -1,6 +1,6 @@
 export const ROUTE_DEVICE_ORDER_AIS_GADGET_VALIDATE_CUSTOMER_PAGE = '/device-order/ais/existing-gadget/validate-customer';
 export const ROUTE_DEVICE_ORDER_AIS_GADGET_VALIDATE_CUSTOMER_PI_PAGE = '/device-order/ais/existing-gadget/validate-customer-pi';
-export const ROUTE_DEVICE_ORDER_AIS_GADGET_VALIDATE_CUSTOMER_ID_CARD_PAGE = '/device-order/ais/existing-gadgetvalidate-customer-id-card';
+export const ROUTE_DEVICE_ORDER_AIS_GADGET_VALIDATE_CUSTOMER_ID_CARD_PAGE = '/device-order/ais/existing-gadget/validate-customer-id-card';
 export const ROUTE_DEVICE_ORDER_AIS_GADGET_VALIDATE_CUSTOMER_ID_CARD_PI_PAGE = '/device-order/ais/existing-gadget/validate-customer-id-card-pi';
 export const ROUTE_DEVICE_ORDER_AIS_GADGET_CUSTOMER_INFO_PAGE = '/device-order/ais/existing-gadget/customer-info';
 export const ROUTE_DEVICE_ORDER_AIS_GADGET_MOBILE_DETAIL_PAGE = '/device-order/ais/existing-gadget/mobile-detail';
