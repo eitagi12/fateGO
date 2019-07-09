757813feb536de97bb13bed7865d928dba1abc1c
oat <oat19454@gmail.com>
2019-07-09 11:51:20 +0700

    edit cancel order

--- src/app/device-only/services/home-button.service.ts
+++ src/app/device-only/services/home-button.service.ts
@@ -4,6 +4,8 @@ import { HomeService, AlertService } from 'mychannel-shared-libs';
 import { CreateOrderService } from './create-order.service';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
 import { PriceOptionService } from 'src/app/shared/services/price-option.service';
+import { Router } from '@angular/router';
+import { async } from '@angular/core/testing';
 @Injectable({
   providedIn: 'root'
 })
@@ -16,7 +18,8 @@ export class HomeButtonService {
     private alertService: AlertService,
     private createOrderService: CreateOrderService,
     private transactionService: TransactionService,
-    private priceOptionService: PriceOptionService
+    private priceOptionService: PriceOptionService,
+    private router: Router
   ) {
     this.transaction = this.transactionService.load();
   }
@@ -24,17 +27,17 @@ export class HomeButtonService {
   initEventButtonHome(): void {
     this.homeService.callback = () => {
       this.alertService.question('ต้องการยกเลิกรายการขายหรือไม่ การยกเลิก ระบบจะคืนสินค้าเข้าสต๊อคสาขาทันที', 'ตกลง', 'ยกเลิก')
-        .then((response: any) => {
+        .then(async(response: any) => {
           if (response.value === true) {
-            this.createOrderService.cancelOrder(this.transaction).then((isSuccess: any) => {
-              this.transactionService.remove();
-              this.priceOptionService.remove();
+            await this.createOrderService.cancelOrder(this.transaction).then(async(isSuccess: any) => {
+              await this.transactionService.remove();
+              await this.priceOptionService.remove();
               window.location.href = '/';
             });
           }
-        }).catch((err: any) => {
-          this.transactionService.remove();
-          this.priceOptionService.remove();
+        }).catch(async(err: any) => {
+          await this.transactionService.remove();
+          await this.priceOptionService.remove();
           window.location.href = '/';
         });
     };
