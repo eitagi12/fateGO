7b180b7ef68662f4b75fa0cc052ac615d3b6fb25
Panyapron Wiratchadit <supermax2555@gmail.com>
2019-07-15 19:41:21 +0700

    mock vas package

--- src/app/vas-package/containers/vas-package-select-vas-package-page/vas-package-select-vas-package-page.component.ts
+++ src/app/vas-package/containers/vas-package-select-vas-package-page/vas-package-select-vas-package-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, ElementRef, ViewChild, OnChanges } from '@angular/core';
 import { Router } from '@angular/router';
-import { HomeService } from 'mychannel-shared-libs';
 import { ROUTE_VAS_PACKAGE_LOGIN_WITH_PIN_PAGE, ROUTE_VAS_PACKAGE_MENU_VAS_ROM_PAGE } from '../../constants/route-path.constant';
 import { PackageProductsService } from '../../services/package-products.service';
+import { HomeService, BannerSlider, SalesService, User} from 'mychannel-shared-libs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 declare var $: any;
 
@@ -17,11 +18,18 @@ export class VasPackageSelectVasPackagePageComponent implements OnInit, OnChange
   campaignSlider: ElementRef;
 
   private $owl: any;
+   mobileForm: FormGroup;
+   mobileNo: string;
+   bannerSliders: BannerSlider[];
+   priceOptionDetailService: Promise<any>;
+   salesService: SalesService;
+   user: User;
 
   constructor(
     private router: Router,
     private homeService: HomeService,
-    private packageProductsService: PackageProductsService
+    private packageProductsService: PackageProductsService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
@@ -59,6 +67,7 @@ export class VasPackageSelectVasPackagePageComponent implements OnInit, OnChange
       }
     });
     this.campaignSlider.nativeElement.style.display = 'block';
+    this.createForm();
   }
 
   onBack(): void {
@@ -80,5 +89,15 @@ export class VasPackageSelectVasPackagePageComponent implements OnInit, OnChange
   getPackageProducts(): void {
     const userId = 'bMfAlzjKaZSSKY3s6c3farMxbUaEsFnIIAgbjsXKA3cOhnKfvawKb60MITINd04Os73YJBQ5aWypkxFk';
     this.packageProductsService.getPackageProducts(userId);
+  }
+
+  createForm(): void {
+    this.mobileForm = this.formBuilder.group({
+      'mobileNo': ['', Validators.compose([Validators.required, Validators.pattern(/^0[6-9]{1}[0-9]{8}/)])],
+    });
+
+    this.mobileForm.valueChanges.subscribe((value) => {
+      this.mobileNo = value.mobileNo;
+    });
   }
 }
