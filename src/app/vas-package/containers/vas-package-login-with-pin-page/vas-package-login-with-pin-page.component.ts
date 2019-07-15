bb09a33ab98aaffe027496ab7c8b2b4e27dc80ce
oat <oat19454@gmail.com>
2019-07-15 16:20:36 +0700

    edit set data transaction

--- src/app/vas-package/containers/vas-package-login-with-pin-page/vas-package-login-with-pin-page.component.ts
+++ src/app/vas-package/containers/vas-package-login-with-pin-page/vas-package-login-with-pin-page.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { HomeService, TokenService, AlertService } from 'mychannel-shared-libs';
-import { ROUTE_VAS_PACKAGE_OTP_PAGE, ROUTE_VAS_PACKAGE_SELECT_PACKAGE_PAGE } from 'src/app/vas-package/constants/route-path.constant';
+import { ROUTE_VAS_PACKAGE_OTP_PAGE, ROUTE_VAS_PACKAGE_SELECT_PACKAGE_PAGE, ROUTE_VAS_PACKAGE_CURRENT_BALANCE_PAGE } from 'src/app/vas-package/constants/route-path.constant';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import * as moment from 'moment';
 import { HttpClient } from '@angular/common/http';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
 
@@ -16,6 +15,7 @@ export class VasPackageLoginWithPinPageComponent implements OnInit, OnDestroy {
   loginForm: FormGroup;
   transaction: any;
   mobileNo: string = '';
+  isRom: boolean;
 
   constructor(
     private router: Router,
@@ -39,6 +39,7 @@ export class VasPackageLoginWithPinPageComponent implements OnInit, OnDestroy {
       .then((res: any) => {
         if (res.data.length > 0) {
           this.mobileNo = res.data.mobileNo;
+          this.isRom = true;
         }
       });
   }
@@ -56,13 +57,9 @@ export class VasPackageLoginWithPinPageComponent implements OnInit, OnDestroy {
     this.router.navigate([ROUTE_VAS_PACKAGE_SELECT_PACKAGE_PAGE]);
   }
 
-  public genTransactionId(): any {
-    return moment().format('YYYYMMDDHHmmssSSS');
-  }
-
   onNext(): void {
     const data = {
-      transactionid: this.genTransactionId(),
+      transactionid: this.transaction.transactionid,
       mobile_no_agent: this.loginForm.value.mobileNo,
       // tslint:disable-next-line: max-line-length
       device_id: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJST00gTW9iaWxlIGFwaSIsImF1ZCI6Imh 0dHBzOi8vbXlyb20uYWlzLmNvLnRoL0FQSS9W MS9zaWdpbiIsInN1YiI6IjA0Ni1iNTc2Mjc4ZC1j MTY4LTQ5YjMtOWYxZi1jODVhYTc4YjgwYzAiL CJtc2lzZG4iOiIwNjIyNDM0MjA4IiwiYWdlbnRpZ CI6IjYyMzgxNDciLCJpYXQiOjE1Mzc0MzE3NjAsI mV4cCI6MTUzNzQzMjY2MH0.kY85wPWDSxy1ll rpejMRJrtKC_PE6F_7fuTMg5y-ZS0'
@@ -70,8 +67,11 @@ export class VasPackageLoginWithPinPageComponent implements OnInit, OnDestroy {
     this.http.post(`/api/customerportal/rom/get-profile`, data).toPromise()
       .then((res: any) => {
         if (res.data.status === 'success') {
+          this.transaction.data.customer = {
+            agentId: res.data.agentId,
+          };
           const customerData: any = {
-            transactionid: this.genTransactionId(),
+            transactionid: this.transaction.transactionid,
             deviceos: 'Android',
             deviceversion: '5.1.1',
             mobile_no_agent: this.loginForm.value.mobileNo,
@@ -82,7 +82,13 @@ export class VasPackageLoginWithPinPageComponent implements OnInit, OnDestroy {
           return this.http.post(`/api/customerportal/rom/sign-in`, customerData).toPromise()
             .then((status: any) => {
               if (status.data.status === 'success') {
-                this.transaction.data.mobileNo = this.loginForm.value.mobileNo;
+                this.transaction.data.customer = {
+                  mobileNo: this.loginForm.value.mobileNo,
+                  pin: this.loginForm.value.pin
+                };
+                if (this.isRom) {
+                  this.router.navigate([ROUTE_VAS_PACKAGE_CURRENT_BALANCE_PAGE]);
+                }
                 this.router.navigate([ROUTE_VAS_PACKAGE_OTP_PAGE]);
               } else {
                 this.alertService.error(status.data.message);
