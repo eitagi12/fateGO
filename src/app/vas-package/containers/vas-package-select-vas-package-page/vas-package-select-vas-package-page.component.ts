4bf0819519aa53d18fe293424265cc9de0188c8e
Panyapron Wiratchadit <supermax2555@gmail.com>
2019-07-17 15:49:58 +0700

    packageBestSallerBanner

--- src/app/vas-package/containers/vas-package-select-vas-package-page/vas-package-select-vas-package-page.component.ts
+++ src/app/vas-package/containers/vas-package-select-vas-package-page/vas-package-select-vas-package-page.component.ts
@@ -2,38 +2,26 @@ import { Component, OnInit, ElementRef, ViewChild, OnChanges } from '@angular/co
 import { Router } from '@angular/router';
 import { ROUTE_VAS_PACKAGE_LOGIN_WITH_PIN_PAGE, ROUTE_VAS_PACKAGE_MENU_VAS_ROM_PAGE } from '../../constants/route-path.constant';
 import { PackageProductsService } from '../../services/package-products.service';
-import { HomeService, BannerSlider, SalesService, User} from 'mychannel-shared-libs';
+import { HomeService, BannerSlider, SalesService, User } from 'mychannel-shared-libs';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-
-declare var $: any;
+import { filter } from 'minimatch';
 
 @Component({
   selector: 'app-vas-package-select-vas-package-page',
   templateUrl: './vas-package-select-vas-package-page.component.html',
   styleUrls: ['./vas-package-select-vas-package-page.component.scss']
 })
-export class VasPackageSelectVasPackagePageComponent implements OnInit, OnChanges {
+export class VasPackageSelectVasPackagePageComponent implements OnInit {
 
-  @ViewChild('slider')
-  slider: ElementRef;
-
-  private $owl: any;
-   mobileForm: FormGroup;
-   mobileNo: string;
-   bannerSliders: BannerSlider[];
-   priceOptionDetailService: Promise<any>;
-   salesService: SalesService;
-   user: User;
-   data: Object;
-
-   policies: any [] = [
-    {id: 0, name: 'policy001'},
-    {id: 2, name: 'policy002'},
-    {id: 3, name: 'policy003'},
-    {id: 4, name: 'policy004'},
-    {id: 5, name: 'policy005'}
-  ];
+  mobileForm: FormGroup;
+  mobileNo: string;
+  bannerSliders: BannerSlider[];
+  priceOptionDetailService: Promise<any>;
+  salesService: SalesService;
+  user: User;
+  shelves: any;
+  packageBestSaller: Array<any> = [];
 
   constructor(
     private router: Router,
@@ -44,42 +32,9 @@ export class VasPackageSelectVasPackagePageComponent implements OnInit, OnChange
   ) { }
 
   ngOnInit(): void {
-    this.createSlider();
     // this.getPackageProducts();
     this.callService();
     this.createForm();
-  }
-
-  ngOnChanges(): void {
-    this.slider.nativeElement.style.display = 'none';
-    setTimeout(() => {
-      this.$owl.trigger('destroy.owl.carousel')
-        .removeClass('owl-loaded owl-hidden')
-        .find('.owl-stage:empty, .owl-item:empty')
-        .remove();
-      this.createSlider();
-    }, 50);
-  }
-
-  private createSlider(): void {
-    this.$owl = $(this.slider.nativeElement).owlCarousel({
-      dots: true,
-      responsive: {
-        0: {
-          items: 2
-        },
-        768: {
-          items: 3
-        },
-        1024: {
-          items: 3
-        },
-        1440: {
-          items: 4
-        }
-      }
-    });
-    this.slider.nativeElement.style.display = 'block';
   }
 
   onBack(): void {
@@ -115,10 +70,31 @@ export class VasPackageSelectVasPackagePageComponent implements OnInit, OnChange
 
   callService(): any {
     const userId = 'bMfAlzjKaZSSKY3s6c3farMxbUaEsFnIIAgbjsXKA3cOhnKfvawKb60MITINd04Os73YJBQ5aWypkxFk';
-    this.http.post('/api/salesportal/promotion-vas', {userId}).toPromise()
-    .then((response) => {
-      console.log('res', response);
-      console.log('titel', response);
-    });
+    this.http.post('/api/salesportal/promotion-vas', { userId }).toPromise()
+      .then((response: any) => {
+        // console.log('best_seller_priority', .items[0].customAttributes.best_seller_priority);
+        response.data.data.map((data: any) => {
+          data.subShelves.map((subShelves: any) => {
+            subShelves.items.map((item: any) => {
+              if (+item.customAttributes.best_seller_priority > 0) {
+                console.log('item ', item);
+                item.mainTitle = data.title;
+                if (data.title === 'เน็ตและโทร') {
+                  item.icon = 'assets/images/icon/Phone_net.png';
+                } else if (data.title === 'เน้นคุย') {
+                  item.icon = 'assets/images/icon/Call.png';
+                } else {
+                  item.icon = 'assets/images/icon/Net.png';
+                }
+                this.packageBestSaller.push(item);
+              }
+
+            }).sort((a: any, b: any) => (+a.customAttributes.best_seller_priority) - (b.customAttributes.best_seller_priority));
+          });
+        });
+      }).then(() => {
+        console.log('res', this.packageBestSaller);
+      });
+
   }
 }
