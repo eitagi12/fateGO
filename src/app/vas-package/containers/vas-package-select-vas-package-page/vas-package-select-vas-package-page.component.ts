4f2c1faeff737ba36ebf9a75b901f465fad937fc
Bart <kijja85@gmail.com>
2019-07-18 18:57:06 +0700

    [filter prioity]

--- src/app/vas-package/containers/vas-package-select-vas-package-page/vas-package-select-vas-package-page.component.ts
+++ src/app/vas-package/containers/vas-package-select-vas-package-page/vas-package-select-vas-package-page.component.ts
@@ -254,7 +254,8 @@ export class VasPackageSelectVasPackagePageComponent implements OnInit, OnDestro
           data.subShelves.map((subShelves: any) => {
             const listPackage = this.filterRomPackage(subShelves.items);
             listPackage.map((item: any) => {
-              item.idCategory = data.id; // data.priority;
+              item.idCategory = data.priority;
+              // console.log(item.idCategory);
               if (+item.customAttributes.best_seller_priority > 0) {
                 // console.log('item ', item);
                 item.mainTitle = data.title;
@@ -324,6 +325,7 @@ export class VasPackageSelectVasPackagePageComponent implements OnInit, OnDestro
       });
       newTabs[0].active = true;
     }
+    console.log(newTabs);
     return newTabs;
   }
 
