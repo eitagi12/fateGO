db5ab6a56a3a421a6910591c380930123f76fd2a
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2019-07-19 21:04:58 +0700

    try to fix  check balance

--- src/app/vas-package/containers/vas-package-current-balance-page/vas-package-current-balance-page.component.ts
+++ src/app/vas-package/containers/vas-package-current-balance-page/vas-package-current-balance-page.component.ts
@@ -90,6 +90,7 @@ export class VasPackageCurrentBalancePageComponent implements OnInit, OnDestroy 
             transactionIdRom: res.data.transactionid
           };
           this.balance = res.data.balance ? res.data.balance : '';
+          this.romAgentForm.controls.amount.setValue(this.balance);
           this.alertService.error('balane : ' + this.balance);
           this.checkBalanceRomWithPricePackage(this.balance);
           this.pageLoadingService.closeLoading();
