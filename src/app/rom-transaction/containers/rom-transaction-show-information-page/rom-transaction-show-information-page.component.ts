611d013c4143aee461730c58a2136760bf328b38
kawinpop <kawinpop123@gmail.com>
2019-07-23 13:56:49 +0700

    Edit param

--- src/app/rom-transaction/containers/rom-transaction-show-information-page/rom-transaction-show-information-page.component.ts
+++ src/app/rom-transaction/containers/rom-transaction-show-information-page/rom-transaction-show-information-page.component.ts
@@ -6,7 +6,7 @@ import { TransactionService } from 'src/app/shared/services/transaction.service'
 import {
   ROUTE_ROM_TRANSACTION_RESULT_PAGE,
   ROUTE_ROM_TRANSACTION_LIST_MOBILE_PAGE
- } from 'src/app/rom-transaction/constants/route-path.constant';
+} from 'src/app/rom-transaction/constants/route-path.constant';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 @Component({
@@ -34,26 +34,58 @@ export class RomTransactionShowInformationPageComponent implements OnInit, OnDes
   }
 
   ngOnInit(): void {
-   this.createForm();
+    this.createForm();
   }
 
   validatePIN(): void {
     if (this.pinForm.valid) {
-      const pinNo = this.pinForm.controls.pin.value;
-      const refNo = this.pinForm.controls.ref.value;
-      this.pageLoadingService.openLoading();
-      this.http.post(('/api/customerportal/rom/aisrom-ussdadapter'), {
-        from: this.romTransaction.romTransaction.romNo,
-        content: this.romTransaction.romTransaction.romNo,
-        romPinCode: pinNo,
-        lastCustomerMobile: this.romTransaction.romTransaction.cusMobileNo,
-        romRefNo: refNo
-      }).toPromise()
-      .then((respone: any) => {
-        this.pageLoadingService.closeLoading();
-        this.transaction.data.romTransaction.pin = pinNo;
-        this.transaction.data.romTransaction.refNo = refNo;
-        this.onNext();
+      this.alertService.notify({
+        type: 'question',
+        showConfirmButton: true,
+        confirmButtonText: 'ยืนยัน',
+        cancelButtonText: 'ยกเลิก',
+        showCancelButton: true,
+        reverseButtons: true,
+        allowEscapeKey: false,
+        html: `
+        <div class="container">
+          <h5>ยกเลิกเติมเงิน</h5>
+          <div class="text-left">
+          <small>เบอร์มือถือ</small>
+          <h4 class="text-green">${ this.mobileNoPipe.transform(this.romTransaction.romTransaction.cusMobileNo)}</h4>
+          <hr>
+          <small>ประเภทรายการ</small>
+          <h4 class="text-green">${
+          this.romTransaction.romTransaction.transactionType === 'VAS'
+            ? 'โปรเสริมออนไลน์'
+            : 'เติมเงินออนไลน์'
+          }</h4>
+          <hr>
+          <small>จำนวนเงิน</small>
+          <h4 class="text-green">${this.romTransaction.romTransaction.price} บาท</h4>
+          </div>
+        </div>
+        `
+      }).then((confirm) => {
+        if (confirm.value) {
+          const pinNo = this.pinForm.controls.pin.value;
+          const refNo = this.pinForm.controls.ref.value;
+          this.pageLoadingService.openLoading();
+          this.http.post(('/api/customerportal/rom/aisrom-ussdadapter'), {
+            ssid: this.romTransaction.romTransaction.ssid,
+            from: this.romTransaction.romTransaction.romNo,
+            content: this.romTransaction.romTransaction.romNo,
+            romPinCode: pinNo,
+            lastCustomerMobile: this.romTransaction.romTransaction.cusMobileNo,
+            romRefNo: refNo
+          }).toPromise()
+            .then((respone: any) => {
+              this.pageLoadingService.closeLoading();
+              this.transaction.data.romTransaction.pin = pinNo;
+              this.transaction.data.romTransaction.refNo = refNo;
+              this.onNext();
+            });
+        }
       });
     }
   }
@@ -62,7 +94,7 @@ export class RomTransactionShowInformationPageComponent implements OnInit, OnDes
     this.pinForm = this.fb.group({
       'pin': ['', Validators.compose([Validators.required, Validators.pattern(/^[0-9]{4}/)])],
       'ref': ['', Validators.compose([Validators.required, Validators.pattern(/^[0-9]{9}/)])]
-  });
+    });
   }
 
   onBack(): void {
@@ -70,38 +102,7 @@ export class RomTransactionShowInformationPageComponent implements OnInit, OnDes
   }
 
   onNext(): void {
-    this.alertService.notify({
-      type: 'question',
-      showConfirmButton: true,
-      confirmButtonText: 'ยืนยัน',
-      cancelButtonText: 'ยกเลิก',
-      showCancelButton: true,
-      reverseButtons: true,
-      allowEscapeKey: false,
-      html: `
-      <div class="container">
-        <h5>ยกเลิกเติมเงิน</h5>
-        <div class="text-left">
-        <small>เบอร์มือถือ</small>
-        <h4 class="text-green">${ this.mobileNoPipe.transform(this.romTransaction.romTransaction.cusMobileNo) }</h4>
-        <hr>
-        <small>ประเภทรายการ</small>
-        <h4 class="text-green">${
-          this.romTransaction.romTransaction.transactionType === 'VAS'
-          ? 'โปรเสริมออนไลน์'
-          : 'เติมเงินออนไลน์'
-        }</h4>
-        <hr>
-        <small>จำนวนเงิน</small>
-        <h4 class="text-green">${this.romTransaction.romTransaction.price} บาท</h4>
-        </div>
-      </div>
-      `
-    }).then((confirm) => {
-      if (confirm.value) {
-        this.router.navigate([ROUTE_ROM_TRANSACTION_RESULT_PAGE]);
-      }
-    });
+    this.router.navigate([ROUTE_ROM_TRANSACTION_RESULT_PAGE]);
   }
 
   onHome(): void {
