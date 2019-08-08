e24f79765454fbba52ce61a08fede8fdd1dcc583
matsi masamin <matsim49@aware.postbox.in.th>
2019-08-08 13:09:56 +0700

    [change] productType and productSubType

--- src/app/buy-gadget/constants/gadget.constant.ts
+++ src/app/buy-gadget/constants/gadget.constant.ts
@@ -1,3 +1,3 @@
-export const PRODUCT_TYPE = 'DEVICE';
-export const PRODUCT_SUB_TYPE = 'HANDSET';
+export const PRODUCT_TYPE = 'GADGET/IOT';
+export const PRODUCT_SUB_TYPE = 'N/A';
 export const SUB_STOCK_DESTINATION = 'BRN';
