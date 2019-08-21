96ec482431ce483fb84f7513b492acd5744da9f1
tanawat wongsawan <fasttanawat12@gmail.com>
2019-08-21 10:04:44 +0700

    fix qr code gen and qr code queue

--- src/app/device-order/ais/device-order-ais-device/containers/device-order-ais-device-qr-code-generator-page/device-order-ais-device-qr-code-generator-page.component.ts
+++ src/app/device-order/ais/device-order-ais-device/containers/device-order-ais-device-qr-code-generator-page/device-order-ais-device-qr-code-generator-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { ROUTE_DEVICE_AIS_DEVICE_QR_CODE_SUMMARY_PAGE, ROUTE_DEVICE_AIS_DEVICE_QR_CODE_QUEUE_PAGE } from 'src/app/device-order/ais/device-order-ais-device/constants/route-path.constant';
 import * as moment from 'moment';
@@ -15,7 +15,7 @@ import { QrCodePageService } from 'src/app/device-order/services/qr-code-page.se
   templateUrl: './device-order-ais-device-qr-code-generator-page.component.html',
   styleUrls: ['./device-order-ais-device-qr-code-generator-page.component.scss']
 })
-export class DeviceOrderAisDeviceQrCodeGeneratorPageComponent implements OnInit {
+export class DeviceOrderAisDeviceQrCodeGeneratorPageComponent implements OnInit, OnDestroy {
   transaction: Transaction;
   priceOption: PriceOption;
 
@@ -40,6 +40,17 @@ export class DeviceOrderAisDeviceQrCodeGeneratorPageComponent implements OnInit 
 
   ngOnInit(): void {
     this.onGenerateQRCode();
+  }
+
+  ngOnDestroy(): void {
+    this.transactionService.update(this.transaction);
+
+    if (this.timeCounterSubscription) {
+      this.timeCounterSubscription.unsubscribe();
+    }
+    if (this.checkResponseMpaySubscription) {
+      this.checkResponseMpaySubscription.unsubscribe();
+    }
   }
 
   onGenerateQRCode(): void {
@@ -110,7 +121,33 @@ export class DeviceOrderAisDeviceQrCodeGeneratorPageComponent implements OnInit 
 
         this.checkResponseMpaySubscription = this.qrCodePageService.checkPaymentResponseMpayStatus(orderId)
           .subscribe(obs => {
-            this.transaction.data.mpayPayment = obs;
+            const mpay = {
+              locationCode: obs.locationCode,
+              qrType: obs.qrType,
+              startDtm: obs.startDtm,
+              status: obs.status,
+              tranDtm: obs.status
+            };
+            this.transaction.data.mpayPayment = Object.assign(this.transaction.data.mpayPayment, mpay);
+            if (this.priceOption.productStock.company === 'AWN') {
+              this.transaction.data.mpayPayment = Object.assign(this.transaction.data.mpayPayment, obs);
+            } else {
+              const status = this.getStatusPay();
+              this.transaction.data.mpayPayment.orderId = obs.orderId;
+              if (status === 'DEVICE') {
+                this.transaction.data.mpayPayment.mpayStatus.orderIdDevice = obs.orderId;
+                this.transaction.data.mpayPayment.mpayStatus.statusDevice = 'SUCCESS';
+                this.transaction.data.mpayPayment.tranId = obs.tranId;
+                this.transaction.data.mpayPayment.amount = obs.amount;
+              } else if (status === 'AIRTIME') {
+                this.transaction.data.mpayPayment.mpayStatus.orderIdAirTime = obs.orderId;
+                this.transaction.data.mpayPayment.mpayStatus.statusAirTime = 'SUCCESS';
+                this.transaction.data.mpayPayment.qrAirtimeTransId = obs.tranId;
+                this.transaction.data.mpayPayment.qrAirtimeAmt = obs.amount;
+              } else {
+                this.transaction.data.mpayPayment = Object.assign(this.transaction.data.mpayPayment, obs);
+              }
+            }
             this.onNext();
           });
 
@@ -137,6 +174,20 @@ export class DeviceOrderAisDeviceQrCodeGeneratorPageComponent implements OnInit 
                 });
             });
       });
+  }
+
+  getStatusPay(): string {
+    const mpayPayment = this.transaction.data.mpayPayment;
+    const tread = this.priceOption.trade;
+    if (tread.advancePay.installmentFlag === 'Y') {
+      return 'DEVICE&AIRTIME';
+    } else {
+      if (mpayPayment.mpayStatus && mpayPayment.mpayStatus.statusDevice && mpayPayment.mpayStatus.statusDevice === 'WAITING') {
+        return 'DEVICE';
+      } else {
+        return 'AIRTIME';
+      }
+    }
   }
 
   onNext(): void {
