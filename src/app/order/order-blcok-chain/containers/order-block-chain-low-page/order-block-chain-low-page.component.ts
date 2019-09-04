494fc274550d9ead01d0f209fd2713cb9e2610c2
sirikan <sirika49@aware.postbox.in.th>
2019-09-04 13:25:07 +0700

    add wizards

--- src/app/order/order-blcok-chain/containers/order-block-chain-low-page/order-block-chain-low-page.component.ts
+++ src/app/order/order-blcok-chain/containers/order-block-chain-low-page/order-block-chain-low-page.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { HomeService, TokenService, AlertService } from 'mychannel-shared-libs';
 import { ROUTE_ORDER_BLOCK_CHAIN_ELIGIBLE_MOBILE_PAGE, ROUTE_ORDER_BLOCK_CHAIN_AGREEMENT_SIGN_PAGE } from 'src/app/order/order-blcok-chain/constants/route-path.constant';
-
+import { WIZARD_ORDER_BLOCK_CHAIN } from 'src/app/order/constants/wizard.constant';
 @Component({
   selector: 'app-order-block-chain-low-page',
   templateUrl: './order-block-chain-low-page.component.html',
   styleUrls: ['./order-block-chain-low-page.component.scss']
 })
 export class OrderBlockChainLowPageComponent implements OnInit {
-
+  wizards: string[] = WIZARD_ORDER_BLOCK_CHAIN;
   constructor(
     private router: Router,
     private homeService: HomeService,
