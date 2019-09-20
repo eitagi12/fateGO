d3d900abebcc77669ec03a92ff3c0f24f10dd0af
Dethsakda Meekrailat <dethm130@corp-ais900>
2019-09-20 14:32:11 +0700

    update route path

--- src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-qr-code-queue-page/new-register-mnp-qr-code-queue-page.component.ts
+++ src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-qr-code-queue-page/new-register-mnp-qr-code-queue-page.component.ts
@@ -1,15 +1,120 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Transaction } from 'src/app/shared/models/transaction.model';
+import { PriceOption } from 'src/app/shared/models/price-option.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
+import { PriceOptionService } from 'src/app/shared/services/price-option.service';
+import { PageLoadingService, REGEX_MOBILE } from 'mychannel-shared-libs';
+import { QueuePageService } from 'src/app/device-order/services/queue-page.service';
+import { SummaryPageService } from 'src/app/device-order/services/summary-page.service';
+import { SharedTransactionService } from 'src/app/shared/services/shared-transaction.service';
+import { ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_RESULT_PAGE } from '../../constants/route-path.constant';
 
 @Component({
   selector: 'app-new-register-mnp-qr-code-queue-page',
   templateUrl: './new-register-mnp-qr-code-queue-page.component.html',
   styleUrls: ['./new-register-mnp-qr-code-queue-page.component.scss']
 })
-export class NewRegisterMnpQrCodeQueuePageComponent implements OnInit {
+export class NewRegisterMnpQrCodeQueuePageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  transaction: Transaction;
+  priceOption: PriceOption;
+  queueFrom: FormGroup;
 
-    ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private transactionService: TransactionService,
+    private priceOptionService: PriceOptionService,
+    private pageLoadingService: PageLoadingService,
+    private queuePageService: QueuePageService,
+    public summaryPageService: SummaryPageService,
+    private sharedTransactionService: SharedTransactionService
+  ) {
+    // this.transaction = this.transactionService.load();
+    // this.priceOption = this.priceOptionService.load();
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  onNext(): void {
+    this.pageLoadingService.openLoading();
+    this.queuePageService.getQueueQmatic(this.queueFrom.value.mobileNo)
+      .then((resp: any) => {
+        const data = resp.data && resp.data.result ? resp.data.result : {};
+        return data.queueNo;
+      })
+      .then((queueNo: string) => {
+        this.transaction.data.queue = {
+          queueNo: queueNo
+        };
+        return this.queuePageService.createDeviceSellingOrder(this.transaction, this.priceOption)
+          .then(() => {
+            return this.sharedTransactionService.updateSharedTransaction(this.transaction, this.priceOption);
+          });
+      })
+      .then(() => {
+        this.router.navigate([ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_RESULT_PAGE]);
+      })
+      .then(() => this.pageLoadingService.closeLoading());
+  }
+
+  getPaymentBalance(): number {
+    const trade = this.priceOption.trade;
+    const payment: any = this.transaction.data.payment || {};
+    const advancePayment: any = this.transaction.data.advancePayment || {};
+    let summary = 0;
+    const advancePay = trade.advancePay || {};
+    if (trade.advancePay.installmentFlag === 'Y') {
+      return this.summary([+trade.promotionPrice, +advancePay.amount]);
+    }
+
+    if (payment.paymentType === 'QR_CODE') {
+      summary += +trade.promotionPrice;
+    }
+    if (advancePayment.paymentType === 'QR_CODE') {
+      summary += +advancePay.amount;
+    }
+    return summary;
+  }
+
+  getOutStandingBalance(): number {
+    const trade = this.priceOption.trade;
+    const payment: any = this.transaction.data.payment || {};
+    const advancePayment: any = this.transaction.data.advancePayment || {};
+    let summary = 0;
+    const advancePay = trade.advancePay || {};
+
+    if (trade.advancePay.installmentFlag === 'Y') {
+      return this.summary([+trade.promotionPrice, +advancePay.amount]);
+    }
+
+    if (payment.paymentType !== 'QR_CODE') {
+      summary += +trade.promotionPrice;
+    }
+    if (advancePayment.paymentType !== 'QR_CODE') {
+      summary += +advancePay.amount;
+    }
+    return summary;
+  }
+
+  summary(amount: number[]): number {
+    return amount.reduce((prev, curr) => {
+      return prev + curr;
+    }, 0);
+  }
+
+  createForm(): void {
+    this.queueFrom = this.fb.group({
+      'mobileNo': ['', Validators.compose([Validators.required, Validators.pattern(REGEX_MOBILE)])],
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.transactionService.update(this.transaction);
   }
 
 }
