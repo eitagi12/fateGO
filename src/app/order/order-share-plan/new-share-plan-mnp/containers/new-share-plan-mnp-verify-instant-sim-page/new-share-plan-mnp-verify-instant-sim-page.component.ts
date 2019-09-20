5efb5fd581a48819114d75b6fc3899b4bf67f075
pornnapa sritubtim <pornns49@aware.postbox.in.th>
2019-09-20 11:56:27 +0700

    [ADD] verify-instant-sim-page

--- src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-verify-instant-sim-page/new-share-plan-mnp-verify-instant-sim-page.component.ts
+++ src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-verify-instant-sim-page/new-share-plan-mnp-verify-instant-sim-page.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { ROUTE_NEW_SHARE_PLAN_MNP_SELECT_NUMBER_PAGE, ROUTE_NEW_SHARE_PLAN_MNP_SELECT_PACKAGE_PAGE } from '../../constants/route-path.constant';
+import { WIZARD_ORDER_NEW_REGISTER } from 'src/app/order/constants/wizard.constant';
+import { Transaction } from 'src/app/shared/models/transaction.model';
+import { SimSerial, SimSerialComponent, HomeService, PageLoadingService, AlertService } from 'mychannel-shared-libs';
+import { Subscription } from 'rxjs';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
+import { TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-new-share-plan-mnp-verify-instant-sim-page',
   templateUrl: './new-share-plan-mnp-verify-instant-sim-page.component.html',
   styleUrls: ['./new-share-plan-mnp-verify-instant-sim-page.component.scss']
 })
-export class NewSharePlanMnpVerifyInstantSimPageComponent implements OnInit {
+export class NewSharePlanMnpVerifyInstantSimPageComponent implements OnInit, OnDestroy {
+
+  wizards: string[] = WIZARD_ORDER_NEW_REGISTER;
+  transaction: Transaction;
+  simSerial: SimSerial;
+  simSerialValid: boolean;
+  translationSubscribe: Subscription;
+
+  @ViewChild('mcSimSerial')
+  mcSimSerial: SimSerialComponent;
 
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private homeService: HomeService,
+    private transactionService: TransactionService,
+    private translationService: TranslateService,
+    private pageLoadingService: PageLoadingService,
+    private http: HttpClient,
+    private alertService: AlertService
+  ) {
+    this.transaction = this.transactionService.load();
+    this.translationSubscribe = this.translationService.onLangChange.subscribe(lang => {
+      this.mcSimSerial.focusInput();
+    });
+   }
 
   ngOnInit(): void {
+    delete this.transaction.data.simCard;
   }
 
   onBack(): void {
@@ -22,6 +50,42 @@ export class NewSharePlanMnpVerifyInstantSimPageComponent implements OnInit {
 
   onNext(): void {
     this.router.navigate([ROUTE_NEW_SHARE_PLAN_MNP_SELECT_PACKAGE_PAGE]);
+  }
+
+  onHome(): void {
+    this.homeService.goToHome();
+  }
+
+  onCheckSimSerial(serial: string): void {
+    this.pageLoadingService.openLoading();
+    this.http.get(`/api/customerportal/validate-verify-instant-sim?serialNo=${serial}`).toPromise()
+      .then((resp: any) => {
+        const simSerial = resp.data || [];
+        this.simSerialValid = true;
+        this.simSerial = {
+          mobileNo: simSerial.mobileNo,
+          simSerial: serial
+        };
+        this.transaction.data.simCard = {
+          mobileNo: this.simSerial.mobileNo,
+          simSerial: this.simSerial.simSerial,
+          persoSim: false
+        };
+        this.pageLoadingService.closeLoading();
+      }).catch((resp: any) => {
+        this.simSerialValid = false;
+        this.simSerial = undefined;
+        const error = resp.error || [];
+        this.pageLoadingService.closeLoading();
+        this.alertService.notify({
+          type: 'error',
+          html: this.translationService.instant(error.resultDescription.replace(/<br>/, ' '))
+        });
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.transactionService.update(this.transaction);
   }
 
 }
