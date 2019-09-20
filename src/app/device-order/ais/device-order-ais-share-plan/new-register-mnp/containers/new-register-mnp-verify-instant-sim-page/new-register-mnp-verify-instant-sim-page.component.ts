123a38e617709536fd50de6b1019c1453508031b
Dethsakda Meekrailat <dethm130@corp-ais900>
2019-09-20 10:17:22 +0700

    add logic function

--- src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-verify-instant-sim-page/new-register-mnp-verify-instant-sim-page.component.ts
+++ src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-verify-instant-sim-page/new-register-mnp-verify-instant-sim-page.component.ts
@@ -1,15 +1,110 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Transaction } from 'src/app/shared/models/transaction.model';
+import { SimSerial, ShoppingCart, SimSerialComponent, HomeService, PageLoadingService, AlertService } from 'mychannel-shared-libs';
+import { Subscription } from 'rxjs';
+import { WIZARD_DEVICE_ORDER_AIS } from 'src/app/device-order/constants/wizard.constant';
+import { Router } from '@angular/router';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
+import { HttpClient } from '@angular/common/http';
+import { ShoppingCartService } from 'src/app/device-order/services/shopping-cart.service';
+import { TranslateService } from '@ngx-translate/core';
+import { environment } from 'src/environments/environment';
+import { ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_SELECT_NUMBER_PAGE, ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_SELECT_PACKAGE_PAGE } from '../../constants/route-path.constant';
 
 @Component({
   selector: 'app-new-register-mnp-verify-instant-sim-page',
   templateUrl: './new-register-mnp-verify-instant-sim-page.component.html',
   styleUrls: ['./new-register-mnp-verify-instant-sim-page.component.scss']
 })
-export class NewRegisterMnpVerifyInstantSimPageComponent implements OnInit {
+export class NewRegisterMnpVerifyInstantSimPageComponent implements OnInit, OnDestroy {
+  wizards: string[] = WIZARD_DEVICE_ORDER_AIS;
 
-  constructor() { }
+  transaction: Transaction;
+  simSerial: SimSerial;
+  simSerialValid: boolean;
+  shoppingCart: ShoppingCart;
+  mcSimSerial: SimSerialComponent;
+  translationSubscribe: Subscription;
+
+  constructor(
+    private router: Router,
+    private homeService: HomeService,
+    private transactionService: TransactionService,
+    private pageLoadingService: PageLoadingService,
+    private alertService: AlertService,
+    private http: HttpClient,
+    private shoppingCartService: ShoppingCartService,
+    private translationService: TranslateService
+  ) {
+    this.transaction = this.transactionService.load();
+   }
 
   ngOnInit(): void {
+    delete this.transaction.data.simCard;
+
+    this.shoppingCart = Object.assign(this.shoppingCartService.getShoppingCartData(), {
+      mobileNo: ''
+    });
+  }
+
+  onCheckSimSerial(serial: string): void {
+    let validateVerifyInstantSim;
+    if (environment.name === 'LOCAL' && serial === '9999999999999') {
+      validateVerifyInstantSim = Promise.resolve({
+        data: {
+          mobileNo: '0999999999'
+        }
+      });
+    } else {
+      validateVerifyInstantSim = this.http.get(`/api/customerportal/validate-verify-instant-sim?serialNo=${serial}`).toPromise();
+    }
+
+    this.pageLoadingService.openLoading();
+    validateVerifyInstantSim.then((resp: any) => {
+      const simSerial = resp.data || {};
+      this.simSerialValid = true;
+      this.simSerial = {
+        mobileNo: simSerial.mobileNo,
+        simSerial: serial
+      };
+      this.transaction.data.simCard = {
+        mobileNo: this.simSerial.mobileNo,
+        simSerial: this.simSerial.simSerial,
+        persoSim: false
+      };
+      this.shoppingCart = Object.assign(this.shoppingCart, {
+        mobileNo: simSerial.mobileNo
+      });
+      this.pageLoadingService.closeLoading();
+    }).catch((resp: any) => {
+      this.simSerialValid = false;
+      this.simSerial = undefined;
+      const error = resp.error || [];
+      this.pageLoadingService.closeLoading();
+      this.alertService.notify({
+        type: 'error',
+        html: this.translationService.instant(error.resultDescription.replace(/<br>/, ' '))
+      });
+    });
+  }
+
+  onBack(): void {
+    this.router.navigate([ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_SELECT_NUMBER_PAGE]);
+  }
+
+  onNext(): void {
+    this.router.navigate([ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_SELECT_PACKAGE_PAGE]);
+  }
+
+  onHome(): void {
+    this.homeService.goToHome();
+  }
+
+  ngOnDestroy(): void {
+    if (this.translationSubscribe) {
+      this.translationSubscribe.unsubscribe();
+    }
+    this.transactionService.update(this.transaction);
   }
 
 }
