ecc0427b04218212737e54602e77f787f0f5b7e8
Petcharat Sriyos <petcs529@corp-ais900>
2019-09-20 14:09:05 +0700

    add face confirm page and select number page

--- src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-face-confirm-page/new-share-plan-mnp-face-confirm-page.component.ts
+++ src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-face-confirm-page/new-share-plan-mnp-face-confirm-page.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ROUTE_NEW_SHARE_PLAN_MNP_FACE_COMPARE_PAGE, ROUTE_NEW_SHARE_PLAN_MNP_SELECT_NUMBER_PAGE } from '../../constants/route-path.constant';
+import { WIZARD_ORDER_NEW_REGISTER } from 'src/app/order/constants/wizard.constant';
+import { environment } from 'src/environments/environment';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PageLoadingService, AlertService, HomeService } from 'mychannel-shared-libs';
+import { TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-new-share-plan-mnp-face-confirm-page',
@@ -9,11 +15,32 @@ import { ROUTE_NEW_SHARE_PLAN_MNP_FACE_COMPARE_PAGE, ROUTE_NEW_SHARE_PLAN_MNP_SE
 })
 export class NewSharePlanMnpFaceConfirmPageComponent implements OnInit {
 
+  wizards: string[] = WIZARD_ORDER_NEW_REGISTER;
+  confirmForm: FormGroup;
+
   constructor(
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private pageLoadingService: PageLoadingService,
+    private alertService: AlertService,
+    private homeService: HomeService,
+    private translation: TranslateService
   ) { }
 
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    let username;
+    if (environment.name === 'LOCAL' || environment.name === 'PVT') {
+      username = 'netnapht';
+    }
+    this.confirmForm = this.fb.group({
+      username: [username, [Validators.required]],
+      password: ['', [Validators.required]],
+    });
   }
 
   onBack(): void {
@@ -21,7 +48,27 @@ export class NewSharePlanMnpFaceConfirmPageComponent implements OnInit {
   }
 
   onNext(): void {
-    this.router.navigate([ROUTE_NEW_SHARE_PLAN_MNP_SELECT_NUMBER_PAGE]);
+    this.pageLoadingService.openLoading();
+    const username = this.confirmForm.value.username;
+    this.http.get('/api/customerportal/checkEmployeeCode', {
+      params: {
+        username: username
+      }
+    }).toPromise()
+      .then((resp: any) => {
+        if (resp && resp.data) {
+          this.router.navigate([ROUTE_NEW_SHARE_PLAN_MNP_SELECT_NUMBER_PAGE]);
+        } else {
+          return this.alertService.error(this.translation.instant('ชื่อ/รหัสผ่าน ไม่ถูกต้อง กรุณาระบุใหม่อีกครั้ง'));
+        }
+      })
+      .then(() => {
+        this.pageLoadingService.closeLoading();
+      });
+  }
+
+  onHome(): void {
+    this.homeService.goToHome();
   }
 
 }
--- src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-select-number-page/new-share-plan-mnp-select-number-page.component.ts
+++ src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-select-number-page/new-share-plan-mnp-select-number-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ROUTE_NEW_SHARE_PLAN_MNP_FACE_CONFIRM_PAGE, ROUTE_NEW_SHARE_PLAN_MNP_VERIFY_INSTANT_SIM_PAGE } from '../../constants/route-path.constant';
+import { ROUTE_NEW_SHARE_PLAN_MNP_FACE_CONFIRM_PAGE, ROUTE_NEW_SHARE_PLAN_MNP_VERIFY_INSTANT_SIM_PAGE, ROUTE_NEW_SHARE_PLAN_MNP_VERIFY_BY_PATTERN_PAGE } from '../../constants/route-path.constant';
+import { WIZARD_ORDER_NEW_REGISTER } from 'src/app/order/constants/wizard.constant';
+import { HomeService } from 'mychannel-shared-libs';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
+import { Transaction } from 'src/app/shared/models/transaction.model';
 
 @Component({
   selector: 'app-new-share-plan-mnp-select-number-page',
@@ -9,11 +13,26 @@ import { ROUTE_NEW_SHARE_PLAN_MNP_FACE_CONFIRM_PAGE, ROUTE_NEW_SHARE_PLAN_MNP_VE
 })
 export class NewSharePlanMnpSelectNumberPageComponent implements OnInit {
 
+  wizards: string[] = WIZARD_ORDER_NEW_REGISTER;
+  transaction: Transaction;
+
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private homeService: HomeService,
+    private transactionService: TransactionService
+  ) {
+    this.transaction = this.transactionService.load();
+  }
 
   ngOnInit(): void {
+  }
+
+  onVerifyInstantSim(): void {
+    this.router.navigate([ROUTE_NEW_SHARE_PLAN_MNP_VERIFY_INSTANT_SIM_PAGE]);
+  }
+
+  onByPattern(): void {
+    this.router.navigate([ROUTE_NEW_SHARE_PLAN_MNP_VERIFY_BY_PATTERN_PAGE]);
   }
 
   onBack(): void {
@@ -22,6 +41,10 @@ export class NewSharePlanMnpSelectNumberPageComponent implements OnInit {
 
   onNext(): void {
     this.router.navigate([ROUTE_NEW_SHARE_PLAN_MNP_VERIFY_INSTANT_SIM_PAGE]);
+  }
+
+  onHome(): void {
+    this.homeService.goToHome();
   }
 
 }
