3f4f8936b6aaaa7b730577da2c3eb75ebe22277e
petcs529 <petcs529@vanpl.postbox.in.th>
2019-09-23 13:18:15 +0700

    ebilling page share plan

--- src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-ebilling-page/new-share-plan-mnp-ebilling-page.component.ts
+++ src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-ebilling-page/new-share-plan-mnp-ebilling-page.component.ts
@@ -1,26 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { ROUTE_NEW_SHARE_PLAN_MNP_CUSTOMER_INFO_PAGE } from '../../constants/route-path.constant';
+import { WIZARD_ORDER_NEW_REGISTER } from 'src/app/order/constants/wizard.constant';
+import { Transaction } from 'src/app/shared/models/transaction.model';
+import { HomeService, Ebilling } from 'mychannel-shared-libs';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
+import { HttpClient } from '@angular/common/http';
+import { ROUTE_ORDER_NEW_REGISTER_CONFIRM_USER_INFORMATION_PAGE } from 'src/app/order/order-new-register/constants/route-path.constant';
 @Component({
   selector: 'app-new-share-plan-mnp-ebilling-page',
   templateUrl: './new-share-plan-mnp-ebilling-page.component.html',
   styleUrls: ['./new-share-plan-mnp-ebilling-page.component.scss']
 })
-export class NewSharePlanMnpEbillingPageComponent implements OnInit {
+export class NewSharePlanMnpEbillingPageComponent implements OnInit, OnDestroy {
+  wizards: string[] = WIZARD_ORDER_NEW_REGISTER;
+  transaction: Transaction;
+  billCycleValid: boolean;
+  billCycle: any;
+  billCycles: Ebilling[];
 
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private homeService: HomeService,
+    private http: HttpClient,
+    private transactionService: TransactionService
+  ) {
+    this.transaction = this.transactionService.load();
+
+    if (!this.transaction.data.billingInformation) {
+      this.transaction.data.billingInformation = {};
+    }
+  }
 
   ngOnInit(): void {
+    this.http.get('/api/customerportal/newRegister/queryBillCycle', {
+      params: {
+        coProject: 'N'
+      }
+    }).toPromise().then((resp: any) => {
+      const data = resp.data || {};
+      this.billCycles = data.billCycles || [];
+      if (!this.transaction.data.billingInformation.billCycle) {
+        this.setBillingDefault(data.billCycles || []);
+      } else {
+        this.billCycle = this.transaction.data.billingInformation.billCycle;
+      }
+    });
+  }
+
+  setBillingDefault(ebilling: Ebilling[]): void {
+    for (const ebill of ebilling) {
+      if (ebill.bill === this.transaction.data.customer.billCycle) {
+        this.billCycle = ebill;
+      }
+    }
+  }
+
+  onCompleted(billCycle: any): void {
+    this.billCycle = billCycle;
+  }
+
+  onError(valid: boolean): void {
+    this.billCycleValid = valid;
   }
 
   onBack(): void {
-    this.router.navigate([ROUTE_NEW_SHARE_PLAN_MNP_CUSTOMER_INFO_PAGE]);
+    this.router.navigate([ROUTE_ORDER_NEW_REGISTER_CONFIRM_USER_INFORMATION_PAGE]);
   }
 
   onNext(): void {
-    this.router.navigate([ROUTE_NEW_SHARE_PLAN_MNP_CUSTOMER_INFO_PAGE]);
+    this.transaction.data.billingInformation.billCycle = this.billCycle;
+    this.router.navigate([ROUTE_ORDER_NEW_REGISTER_CONFIRM_USER_INFORMATION_PAGE]);
   }
 
+  onHome(): void {
+    this.homeService.goToHome();
+  }
+
+  ngOnDestroy(): void {
+    this.transactionService.update(this.transaction);
+  }
 }
