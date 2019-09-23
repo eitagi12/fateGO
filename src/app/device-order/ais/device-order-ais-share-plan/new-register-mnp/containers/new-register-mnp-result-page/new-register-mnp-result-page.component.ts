612f752676f1c2bffd87f8a5cd5196f11fc44757
Danai Tuwakam <danat529@vanpl.postbox.in.th>
2019-09-23 10:28:22 +0700

    create result page

--- src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-result-page/new-register-mnp-result-page.component.ts
+++ src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-result-page/new-register-mnp-result-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
+import { Transaction } from 'src/app/shared/models/transaction.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-new-register-mnp-result-page',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-register-mnp-result-page.component.scss']
 })
 export class NewRegisterMnpResultPageComponent implements OnInit {
+  transaction: Transaction;
+  isSuccess: boolean;
+  mobileNo: string;
+  mainSimSerail: string;
+  memberMobileNo: string;
+  memberSimSerail: string;
 
-  constructor() { }
+  constructor(
+    private transactionService: TransactionService
+  ) {
+    this.transaction = this.transactionService.load();
+  }
 
-    ngOnInit(): void {
+  ngOnInit(): void {
+    this.isSuccess = true;
+    this.mobileNo = this.transaction.data.simCard.mobileNo;
+    this.mainSimSerail = this.transaction.data.simCard.simSerial;
+    this.memberMobileNo = '0910045268';
+    this.memberSimSerail = this.transaction.data.simCard.simSerial;
+  }
+
+  onMainMenu(): void {
+    if (environment.name === 'LOCAL') {
+      window.location.href = '/main-menu';
+    } else {
+      window.location.href = '/smart-digital/main-menu';
+    }
   }
 
 }
