51c2b10535ca0f3455158974864ca510d3f8b133
Atinan Rakkeadtipao <atinar49@corp-ais900>
2019-09-25 15:02:36 +0700

    config back asp to sff

--- src/app/device-only/services/home-button.service.ts
+++ src/app/device-only/services/home-button.service.ts
@@ -1,25 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Transaction } from 'src/app/shared/models/transaction.model';
-import { HomeService, AlertService } from 'mychannel-shared-libs';
+import { HomeService, AlertService, TokenService } from 'mychannel-shared-libs';
 import { CreateOrderService } from './create-order.service';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
 import { PriceOptionService } from 'src/app/shared/services/price-option.service';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class HomeButtonService {
 
   transaction: Transaction;
+  user: any;
 
   constructor(
     private homeService: HomeService,
     private alertService: AlertService,
     private createOrderService: CreateOrderService,
     private transactionService: TransactionService,
-    private priceOptionService: PriceOptionService
+    private priceOptionService: PriceOptionService,
+    private tokenService: TokenService,
   ) {
     this.transaction = this.transactionService.load();
-   }
+    this.user = this.tokenService.getUser();
+  }
 
   initEventButtonHome(): void {
     this.homeService.callback = () => {
@@ -27,15 +31,25 @@ export class HomeButtonService {
         .then((response: any) => {
           if (response.value === true) {
             this.createOrderService.cancelOrder(this.transaction).then((isSuccess: any) => {
-              this.transactionService.remove();
-              this.priceOptionService.remove();
-              window.location.href = '/';
+              // this.transactionService.remove();
+              // this.priceOptionService.remove();
+              if (this.user.userType === 'ASP') {
+                console.log('asp');
+              } else {
+                // window.location.href = '/';
+                console.log('ssza');
+
+              }
             });
           }
         }).catch(() => {
           this.transactionService.remove();
           this.priceOptionService.remove();
-          window.location.href = '/';
+          if (this.user.userType === 'ASP') {
+            window.location.href = environment.AUTH_URL;
+          } else {
+            window.location.href = '/';
+          }
         });
     };
   }
