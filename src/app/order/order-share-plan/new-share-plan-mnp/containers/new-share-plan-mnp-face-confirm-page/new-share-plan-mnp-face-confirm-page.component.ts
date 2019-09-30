ce7bfe21b83d8da4a0ba6c4a4b96dd626f437d7d
pornnapa sritubtim <pornns49@aware.postbox.in.th>
2019-09-30 14:59:01 +0700

    [EDIT] UI face confirm page

--- src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-face-confirm-page/new-share-plan-mnp-face-confirm-page.component.ts
+++ src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-face-confirm-page/new-share-plan-mnp-face-confirm-page.component.ts
@@ -2,11 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ROUTE_NEW_SHARE_PLAN_MNP_FACE_COMPARE_PAGE, ROUTE_NEW_SHARE_PLAN_MNP_SELECT_NUMBER_PAGE } from '../../constants/route-path.constant';
 import { WIZARD_ORDER_NEW_REGISTER } from 'src/app/order/constants/wizard.constant';
-import { environment } from 'src/environments/environment';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { PageLoadingService, AlertService, HomeService } from 'mychannel-shared-libs';
-import { TranslateService } from '@ngx-translate/core';
-import { HttpClient } from '@angular/common/http';
+import { HomeService } from 'mychannel-shared-libs';
 
 @Component({
   selector: 'app-new-share-plan-mnp-face-confirm-page',
@@ -21,31 +18,17 @@ export class NewSharePlanMnpFaceConfirmPageComponent implements OnInit {
   constructor(
     private router: Router,
     private fb: FormBuilder,
-    private http: HttpClient,
-    private pageLoadingService: PageLoadingService,
-    private alertService: AlertService,
-    private homeService: HomeService,
-    private translation: TranslateService
+    private homeService: HomeService
   ) { }
 
   ngOnInit(): void {
     this.createForm();
   }
 
-  private createForm(): void {
-    const username = this.getUsername();
+  createForm(): void {
     this.confirmForm = this.fb.group({
-      username: [username, [Validators.required]],
-      password: ['', [Validators.required]],
+      code: ['******', Validators.required]
     });
-  }
-
-  private getUsername(): string {
-    let username;
-    if (environment.name === 'LOCAL' || environment.name === 'PVT') {
-      username = 'netnapht';
-    }
-    return username;
   }
 
   onBack(): void {
@@ -53,23 +36,7 @@ export class NewSharePlanMnpFaceConfirmPageComponent implements OnInit {
   }
 
   onNext(): void {
-    this.pageLoadingService.openLoading();
-    const username = this.confirmForm.value.username;
-    this.http.get('/api/customerportal/checkEmployeeCode', {
-      params: {
-        username: username
-      }
-    }).toPromise()
-      .then((resp: any) => {
-        if (resp && resp.data) {
-          this.router.navigate([ROUTE_NEW_SHARE_PLAN_MNP_SELECT_NUMBER_PAGE]);
-        } else {
-          return this.alertService.error(this.translation.instant('ชื่อ/รหัสผ่าน ไม่ถูกต้อง กรุณาระบุใหม่อีกครั้ง'));
-        }
-      })
-      .then(() => {
-        this.pageLoadingService.closeLoading();
-      });
+    this.router.navigate([ROUTE_NEW_SHARE_PLAN_MNP_SELECT_NUMBER_PAGE]);
   }
 
   onHome(): void {
