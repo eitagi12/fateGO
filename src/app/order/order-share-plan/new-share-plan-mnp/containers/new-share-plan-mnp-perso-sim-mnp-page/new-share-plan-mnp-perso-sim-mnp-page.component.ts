38baf3699e3f86352081c4de6566d93f001866be
Bordin Ploypradab <bordip49@aware.postbox.in.th>
2019-09-30 14:22:49 +0700

    ui_preso_sim_mnp

--- src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-perso-sim-mnp-page/new-share-plan-mnp-perso-sim-mnp-page.component.ts
+++ src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-perso-sim-mnp-page/new-share-plan-mnp-perso-sim-mnp-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WIZARD_ORDER_NEW_REGISTER } from 'src/app/order/constants/wizard.constant';
 
 @Component({
   selector: 'app-new-share-plan-mnp-perso-sim-mnp-page',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-share-plan-mnp-perso-sim-mnp-page.component.scss']
 })
 export class NewSharePlanMnpPersoSimMnpPageComponent implements OnInit {
-
+  wizards: string[] = WIZARD_ORDER_NEW_REGISTER;
+  title: string ='กรุณาเสียบซิมการ์ด';
   constructor() { }
 
   ngOnInit(): void {
