f2ff493994e1c85984f4651e312a04f036fe6f3f
Dethsakda Meekrailat <dethm130@corp-ais900>
2019-10-01 13:59:16 +0700

    add network type page

--- src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-network-type-page/new-register-mnp-network-type-page.component.ts
+++ src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-network-type-page/new-register-mnp-network-type-page.component.ts
@@ -2,13 +2,18 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Transaction, TransactionType } from 'src/app/shared/models/transaction.model';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { HomeService, AlertService, PageLoadingService, REGEX_MOBILE, ShoppingCart } from 'mychannel-shared-libs';
+import { HomeService, AlertService, PageLoadingService, REGEX_MOBILE, ShoppingCart, PromotionShelve, BillingSystemType } from 'mychannel-shared-libs';
 import { HttpClient } from '@angular/common/http';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
 import { WIZARD_DEVICE_ORDER_AIS } from 'src/app/device-order/constants/wizard.constant';
 import * as moment from 'moment';
 import { environment } from 'src/environments/environment';
 import { ShoppingCartService } from 'src/app/device-order/services/shopping-cart.service';
+import { PromotionShelveService } from 'src/app/device-order/services/promotion-shelve.service';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+import { PriceOption } from 'src/app/shared/models/price-option.model';
+import { PriceOptionService } from 'src/app/shared/services/price-option.service';
 
 @Component({
   selector: 'app-new-register-mnp-network-type-page',
@@ -20,8 +25,11 @@ export class NewRegisterMnpNetworkTypePageComponent implements OnInit, OnDestroy
   wizards: string[] = WIZARD_DEVICE_ORDER_AIS;
 
   transaction: Transaction;
+  priceOption: PriceOption;
   mnpForm: FormGroup;
   shoppingCart: ShoppingCart;
+  promotionShelves: PromotionShelve[];
+  translateSubscription: Subscription;
 
   constructor(
     private router: Router,
@@ -30,18 +38,28 @@ export class NewRegisterMnpNetworkTypePageComponent implements OnInit, OnDestroy
     private http: HttpClient,
     private alertService: AlertService,
     private pageLoadingService: PageLoadingService,
+    private priceOptionService: PriceOptionService,
     private transactionService: TransactionService,
-    private shoppingCartService: ShoppingCartService
+    private shoppingCartService: ShoppingCartService,
+    private promotionShelveService: PromotionShelveService,
+    private translateService: TranslateService
   ) {
+    this.priceOption = this.priceOptionService.load();
     this.homeService.callback = () => {
       window.location.href = '/smart-digital/main-menu';
     };
+    this.translateSubscription = this.translateService.onLangChange.subscribe((lang: any) => {
+      if (this.promotionShelves) {
+        this.checkTranslateLang(lang);
+      }
+    });
   }
 
   ngOnInit(): void {
     this.shoppingCart = this.shoppingCartService.getShoppingCartData();
     this.createTransaction();
     this.createForm();
+    this.callService();
   }
 
   createForm(): void {
@@ -155,10 +173,6 @@ export class NewRegisterMnpNetworkTypePageComponent implements OnInit, OnDestroy
     this.homeService.goToHome();
   }
 
-  ngOnDestroy(): void {
-    this.transactionService.save(this.transaction);
-  }
-
   private createTransaction(): void {
     this.transaction = {
       data: {
@@ -166,6 +180,77 @@ export class NewRegisterMnpNetworkTypePageComponent implements OnInit, OnDestroy
         action: null,
       }
     };
+  }
+
+  callService(): void {
+    this.pageLoadingService.openLoading();
+
+    const trade: any = this.priceOption.trade;
+    const privilege: any = this.priceOption.privilege;
+
+    this.promotionShelveService.getPromotionShelve(
+      {
+        packageKeyRef: trade.packageKeyRef || privilege.packageKeyRef,
+        orderType: 'New Registration',
+        billingSystem: BillingSystemType.IRB
+      },
+      +privilege.minimumPackagePrice, +privilege.maximumPackagePrice)
+      .then((promotionShelves: any) => {
+        this.promotionShelves = this.promotionShelveService.defaultBySelected(promotionShelves, this.transaction.data.mainPackage);
+        if (this.promotionShelves) {
+          this.checkTranslateLang(this.translateService.currentLang);
+        }
+      })
+      .then(() => this.pageLoadingService.closeLoading());
+  }
+
+  checkTranslateLang(lang: any): void {
+    this.mapKeyTranslateSelectPackageTitle(this.promotionShelves).then(translateKey => {
+      if (lang === 'EN' || lang.lang === 'EN' || lang === 'en' || lang.lang === 'en') {
+        if (translateKey && lang && lang.translations) {
+          const merge = { ...translateKey, ...lang.translations } || {};
+          this.translateService.setTranslation('EN', merge);
+        } else {
+          this.getTranslateLoader('device-order', 'EN').then(resp => {
+            const merge = { ...translateKey, ...resp } || {};
+            this.translateService.setTranslation('EN', merge);
+          });
+        }
+      }
+    });
+  }
+
+  mapKeyTranslateSelectPackageTitle(promotionShelves: PromotionShelve[]): Promise<any> {
+    const map = new Map();
+    const TranslateKey = {};
+    promotionShelves.filter(promotionShelve => {
+      promotionShelve.promotions.map(key => key.items.map(item => {
+        let replaceTitle = item.title.replace('บ.', '฿');
+        replaceTitle = replaceTitle.replace('บาท', '฿');
+        replaceTitle = replaceTitle.replace('แพ็กเกจ', 'Package');
+        map.set([item.title.trim()], replaceTitle.trim() || item.title.trim());
+      }));
+    });
+    map.forEach((value: any, key: any) => {
+      TranslateKey[key] = value;
+    });
+    if (TranslateKey) {
+      return Promise.resolve(TranslateKey);
+    } else {
+      return Promise.resolve(null);
+    }
+  }
+
+  getTranslateLoader(moduleName: string, lang: string): Promise<any> {
+    if (!moduleName || !lang) {
+      return;
+    }
+    const fileLang = `i18n/${moduleName}.${lang}.json`.toLowerCase();
+    return this.http.get(fileLang).toPromise();
+  }
+
+  ngOnDestroy(): void {
+    this.transactionService.save(this.transaction);
   }
 
 }
