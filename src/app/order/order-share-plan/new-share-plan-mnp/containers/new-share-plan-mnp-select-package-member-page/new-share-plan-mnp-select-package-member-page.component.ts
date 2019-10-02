953580c9f3477a2574fa4cf546297f9fae064302
pornnapa sritubtim <pornns49@aware.postbox.in.th>
2019-10-02 10:11:16 +0700

    [ADD] mock select package member page

--- src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-select-package-member-page/new-share-plan-mnp-select-package-member-page.component.ts
+++ src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-select-package-member-page/new-share-plan-mnp-select-package-member-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HomeService } from 'mychannel-shared-libs';
 import { ROUTE_NEW_SHARE_PLAN_MNP_NETWORK_TYPE_PAGE, ROUTE_NEW_SHARE_PLAN_MNP_CONFIRM_USER_INFORMATION_PAGE } from '../../constants/route-path.constant';
+import { WIZARD_ORDER_NEW_REGISTER } from 'src/app/order/constants/wizard.constant';
 
 @Component({
   selector: 'app-new-share-plan-mnp-select-package-member-page',
@@ -9,6 +10,8 @@ import { ROUTE_NEW_SHARE_PLAN_MNP_NETWORK_TYPE_PAGE, ROUTE_NEW_SHARE_PLAN_MNP_CO
   styleUrls: ['./new-share-plan-mnp-select-package-member-page.component.scss']
 })
 export class NewSharePlanMnpSelectPackageMemberPageComponent implements OnInit {
+
+  wizards: string[] = WIZARD_ORDER_NEW_REGISTER;
 
   constructor(
     private router: Router,
