11e010875adad20755901119f1e1bce6c93c3ff1
Maruko0.0 <n.arunpoon@gmail.com>
2019-10-03 11:06:55 +0700

    [Add] Agreement sign page

--- src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-agreement-sign-page/new-register-mnp-agreement-sign-page.component.ts
+++ src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-agreement-sign-page/new-register-mnp-agreement-sign-page.component.ts
@@ -1,14 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+import { ShoppingCartService } from 'src/app/device-order/services/shopping-cart.service';
+import { HomeService, TokenService, ShoppingCart, CaptureAndSign } from 'mychannel-shared-libs';
+import { Transaction, Customer } from 'src/app/shared/models/transaction.model';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
+import { AisNativeDeviceService } from 'src/app/shared/services/ais-native-device.service';
+import { WIZARD_DEVICE_ORDER_AIS_DEVICE_SHARE_PLAN } from 'src/app/device-order/constants/wizard.constant';
+import { AgreementSignConstant } from '../../constants/agreement-sign.constant';
+import { ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_ECONTACT_PAGE,
+         ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_FACE_CAPTURE_PAGE
+       } from '../../constants/route-path.constant';
 
 @Component({
   selector: 'app-new-register-mnp-agreement-sign-page',
   templateUrl: './new-register-mnp-agreement-sign-page.component.html',
   styleUrls: ['./new-register-mnp-agreement-sign-page.component.scss']
 })
-export class NewRegisterMnpAgreementSignPageComponent implements OnInit {
+export class NewRegisterMnpAgreementSignPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  wizards: string[] = WIZARD_DEVICE_ORDER_AIS_DEVICE_SHARE_PLAN;
+  shoppingCart: ShoppingCart;
+  transaction: Transaction;
+  captureAndSign: CaptureAndSign;
+
+  idCardValid: boolean;
+  apiSigned: string;
+  conditionText: string;
+
+  constructor(
+    private router: Router,
+    private homeService: HomeService,
+    private transactionService: TransactionService,
+    private tokenService: TokenService,
+    private shoppingCartService: ShoppingCartService
+  ) {
+    this.transaction = this.transactionService.load();
+    this.apiSigned = 'SignaturePad';
+  }
+
   ngOnInit(): void {
+    this.shoppingCart = this.shoppingCartService.getShoppingCartData();
+    const customer: Customer = this.transaction.data.customer;
+    this.conditionText = AgreementSignConstant.NEW_REGISTER_SIGN;
+    this.captureAndSign = {
+      allowCapture: true,
+      imageSmartCard: customer.imageSmartCard,
+      imageSignature: customer.imageReadSmartCard
+    };
+
+  }
+
+  onCompleted(captureAndSign: CaptureAndSign): void {
+    const customer: Customer = this.transaction.data.customer;
+    customer.imageSignatureSmartCard = captureAndSign.imageSignature;
+    customer.imageSmartCard = captureAndSign.imageSmartCard;
+  }
+
+  onError(valid: boolean): void {
+    this.idCardValid = valid;
+  }
+
+  onBack(): void {
+    this.router.navigate([ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_ECONTACT_PAGE]);
+  }
+
+  onNext(): void {
+    this.router.navigate([ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_FACE_CAPTURE_PAGE]);
+  }
+
+  onHome(): void {
+    this.homeService.goToHome();
+  }
+
+  ngOnDestroy(): void {
+    this.transactionService.update(this.transaction);
   }
 
 }
