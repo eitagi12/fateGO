071cdda4eecc0113afed0b9199a1d549492ebf2d
Nunthawat Lhasiw <nuntl685@prodg.postbox.in.th>
2019-10-09 17:22:01 +0700

    Modified UI verify instant sim

--- src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-verify-instant-sim-page/new-register-mnp-verify-instant-sim-page.component.ts
+++ src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-verify-instant-sim-page/new-register-mnp-verify-instant-sim-page.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { SimSerial, HomeService, AlertService, PageLoadingService, ShoppingCart } from 'mychannel-shared-libs';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { SimSerial, HomeService, AlertService, PageLoadingService, ShoppingCart, SimSerialComponent, AisNativeService } from 'mychannel-shared-libs';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
@@ -14,20 +14,32 @@ import {
   ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_SELECT_PACKAGE_PAGE,
   ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_SELECT_NUMBER_PAGE
 } from '../../constants/route-path.constant';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-new-register-mnp-verify-instant-sim-page',
   templateUrl: './new-register-mnp-verify-instant-sim-page.component.html',
   styleUrls: ['./new-register-mnp-verify-instant-sim-page.component.scss']
 })
-export class NewRegisterMnpVerifyInstantSimPageComponent implements OnInit, OnDestroy {
+export class NewRegisterMnpVerifyInstantSimPageComponent implements OnInit, OnDestroy, AfterViewInit {
   wizards: string[] = WIZARD_DEVICE_ORDER_AIS_DEVICE_SHARE_PLAN;
-
+  serialForm: FormGroup;
   transaction: Transaction;
   simSerial: SimSerial;
   simSerialValid: boolean;
   shoppingCart: ShoppingCart;
   translationSubscribe: Subscription;
+  keyinSimSerial: boolean;
+  isHandsetDiscount: boolean;
+  isEasyApp: boolean;
+
+  @ViewChild('mcSimSerial')
+  mcSimSerial: SimSerialComponent;
+  serial: any;
+
+  @ViewChild('serial')
+  serialField: ElementRef;
+
   constructor(private router: Router,
     private homeService: HomeService,
     private transactionService: TransactionService,
@@ -35,15 +47,66 @@ export class NewRegisterMnpVerifyInstantSimPageComponent implements OnInit, OnDe
     private alertService: AlertService,
     private http: HttpClient,
     private shoppingCartService: ShoppingCartService,
-    private translationService: TranslateService) {
+    private translationService: TranslateService,
+    private fb: FormBuilder,
+    private aisNativeService: AisNativeService) {
     this.transaction = this.transactionService.load();
   }
 
   ngOnInit(): void {
+    this.createForm();
     delete this.transaction.data.simCard;
     this.shoppingCart = Object.assign(this.shoppingCartService.getShoppingCartData(), {
       mobileNo: ''
     });
+  }
+
+  private createForm(): void {
+    this.serialForm = this.fb.group({
+      serial: ['', [Validators.required, Validators.pattern(/\d{13}/)]]
+    });
+  }
+
+  public checkSimSerial(): void {
+    this.keyinSimSerial = true;
+    const serial = this.serialForm.controls['serial'].value;
+    this.getMobileNoBySim(serial);
+  }
+
+  private getMobileNoBySim(serial: any): void {
+    this.pageLoadingService.openLoading();
+    this.http.get(`/api/customerportal/validate-verify-instant-sim?serialNo=${serial}`).toPromise()
+      .then((resp: any) => {
+        const simSerial = resp.data || [];
+        this.simSerialValid = true;
+        this.onSubmit();
+        this.simSerial = {
+          mobileNo: simSerial.mobileNo,
+          simSerial: serial
+        };
+        this.transaction.data.simCard = {
+          mobileNo: this.simSerial.mobileNo,
+          simSerial: this.simSerial.simSerial,
+          persoSim: false
+        };
+        this.pageLoadingService.closeLoading();
+      }).catch((resp: any) => {
+        this.simSerialValid = false;
+        this.simSerial = undefined;
+        const error = resp.error || [];
+        this.pageLoadingService.closeLoading();
+        this.alertService.notify({
+          type: 'error',
+          html: this.translationService.instant(error.resultDescription.replace(/<br>/, ' '))
+        });
+        this.onSubmit();
+      });
+  }
+  private onSubmit(): void {
+    this.serialForm.patchValue({
+      serial: ''
+    });
+    this.serialField.nativeElement.focus();
   }
 
   onCheckSimSerial(serial: string): void {
@@ -91,6 +154,11 @@ export class NewRegisterMnpVerifyInstantSimPageComponent implements OnInit, OnDe
     });
   }
 
+  onOpenScanBarcode(): void {
+    this.aisNativeService.scanBarcode();
+    this.keyinSimSerial = false;
+  }
+
   onBack(): void {
     this.router.navigate([ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_SELECT_NUMBER_PAGE]);
   }
@@ -101,6 +169,10 @@ export class NewRegisterMnpVerifyInstantSimPageComponent implements OnInit, OnDe
 
   onHome(): void {
     this.homeService.goToHome();
+  }
+
+  ngAfterViewInit(): void {
+    this.serialField.nativeElement.focus();
   }
 
   ngOnDestroy(): void {
