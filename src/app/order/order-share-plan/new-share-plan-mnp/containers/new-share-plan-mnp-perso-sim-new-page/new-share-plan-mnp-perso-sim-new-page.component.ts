e46dd475dade146a27bf0507f0647589dcbf4807
petcs529 <petcs529@vanpl.postbox.in.th>
2019-10-15 17:21:51 +0700

    agreement-sign-page and perso-sim-page

--- src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-perso-sim-new-page/new-share-plan-mnp-perso-sim-new-page.component.ts
+++ src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-perso-sim-new-page/new-share-plan-mnp-perso-sim-new-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WIZARD_ORDER_NEW_SHARE_PLAN_MNP } from 'src/app/order/constants/wizard.constant';
 import { Router } from '@angular/router';
-import { ROUTE_NEW_SHARE_PLAN_MNP_PERSO_SIM_MNP_PAGE } from '../../constants/route-path.constant';
+import { ROUTE_NEW_SHARE_PLAN_MNP_PERSO_SIM_MNP_PAGE, ROUTE_NEW_SHARE_PLAN_MNP_AGREEMENT_SIGN_PAGE } from '../../constants/route-path.constant';
 
 @Component({
   selector: 'app-new-share-plan-mnp-perso-sim-new-page',
@@ -17,6 +17,10 @@ export class NewSharePlanMnpPersoSimNewPageComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+  }
+
+  onBack(): void {
+    this.router.navigate([ROUTE_NEW_SHARE_PLAN_MNP_AGREEMENT_SIGN_PAGE]);
   }
 
   onNext(): void {
