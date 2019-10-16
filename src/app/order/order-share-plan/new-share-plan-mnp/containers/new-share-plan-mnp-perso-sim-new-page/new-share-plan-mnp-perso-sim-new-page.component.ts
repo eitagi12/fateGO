43d604d5b119192e6a30e9b96d75d648f48e64a5
petcs529 <petcs529@vanpl.postbox.in.th>
2019-10-16 17:23:29 +0700

    header-icon perso sim new page

--- src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-perso-sim-new-page/new-share-plan-mnp-perso-sim-new-page.component.ts
+++ src/app/order/order-share-plan/new-share-plan-mnp/containers/new-share-plan-mnp-perso-sim-new-page/new-share-plan-mnp-perso-sim-new-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { WIZARD_ORDER_NEW_SHARE_PLAN_MNP } from 'src/app/order/constants/wizard.constant';
 import { Router } from '@angular/router';
 import { ROUTE_NEW_SHARE_PLAN_MNP_PERSO_SIM_MNP_PAGE, ROUTE_NEW_SHARE_PLAN_MNP_AGREEMENT_SIGN_PAGE } from '../../constants/route-path.constant';
+import { HomeService } from 'mychannel-shared-libs';
 
 @Component({
   selector: 'app-new-share-plan-mnp-perso-sim-new-page',
@@ -13,10 +14,15 @@ export class NewSharePlanMnpPersoSimNewPageComponent implements OnInit {
   wizards: string[] = WIZARD_ORDER_NEW_SHARE_PLAN_MNP;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private homeService: HomeService
   ) { }
 
   ngOnInit(): void {
+  }
+
+  onHome(): void {
+    this.homeService.goToHome();
   }
 
   onBack(): void {
