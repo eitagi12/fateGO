f637637d4978b01de805fde4b870d83511066bed
Danai Tuwakam <danat529@vanpl.postbox.in.th>
2019-10-25 10:09:10 +0700

    vlidate-customer change api/customerportal/validate-customer-new-register and check age lower 17

--- src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-validate-customer-page/new-register-mnp-validate-customer-page.component.ts
+++ src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-validate-customer-page/new-register-mnp-validate-customer-page.component.ts
@@ -2,14 +2,17 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PriceOption } from 'src/app/shared/models/price-option.model';
 import { Transaction, TransactionType, TransactionAction } from 'src/app/shared/models/transaction.model';
 import { Router } from '@angular/router';
-import { HomeService, PageLoadingService } from 'mychannel-shared-libs';
+import { HomeService, PageLoadingService, TokenService, AlertService } from 'mychannel-shared-libs';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
 import { HttpClient } from '@angular/common/http';
 import { PriceOptionService } from 'src/app/shared/services/price-option.service';
 import { ROUTE_BUY_PRODUCT_CAMPAIGN_PAGE } from 'src/app/buy-product/constants/route-path.constant';
-import { ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_VALIDATE_CUSTOMER_ID_CARD_PAGE,
-         ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_VALIDATE_CUSTOMER_KEY_IN_PAGE,
-         ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_PAYMENT_DETAIL_PAGE } from '../../constants/route-path.constant';
+import {
+  ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_VALIDATE_CUSTOMER_ID_CARD_PAGE,
+  ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_VALIDATE_CUSTOMER_KEY_IN_PAGE,
+  ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_PAYMENT_DETAIL_PAGE
+} from '../../constants/route-path.constant';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-new-register-mnp-validate-customer-page',
@@ -28,8 +31,11 @@ export class NewRegisterMnpValidateCustomerPageComponent implements OnInit, OnDe
     private http: HttpClient,
     private pageLoadingService: PageLoadingService,
     private transactionService: TransactionService,
-    private priceOptionService: PriceOptionService
+    private tokenService: TokenService,
+    private priceOptionService: PriceOptionService,
+    private alertService: AlertService
   ) {
+    this.transaction = this.transactionService.load();
     this.priceOption = this.priceOptionService.load();
   }
 
@@ -49,31 +55,154 @@ export class NewRegisterMnpValidateCustomerPageComponent implements OnInit, OnDe
   }
 
   onBack(): void {
+    const productDetail: any = JSON.parse(localStorage.getItem('productDetail'));
+    window.location.href = `/sales-portal/buy-product/brand/${productDetail.brand}/${productDetail.model}`;
     this.router.navigate([ROUTE_BUY_PRODUCT_CAMPAIGN_PAGE], { queryParams: this.priceOption.queryParams });
   }
 
   onNext(): void {
-
     this.pageLoadingService.openLoading();
-    this.http.get(`/api/customerportal/newRegister/${this.identity}/queryCustomerInfo`)
-      .toPromise()
-      .then((resp: any) => {
-        this.mapCustomer(resp.data);
-        return this.http.get(`/api/customerportal/newRegister/${this.identity}/blackListLimit`).toPromise();
-      })
-      .then((resp: any) => {
+    this.toBillingInformation();
+    this.validateCustomer().then((data: any) => {
+      this.transaction.data = {
+        ...this.transaction.data,
+        order: data.order.data,
+      };
+      this.mapCustomer(data.customer.data);
+    }).then(() => {
+      if (this.transaction.transactionId) {
         this.router.navigate([ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_PAYMENT_DETAIL_PAGE]);
-      })
-      .catch((resp: any) => {
-        this.router.navigate([ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_VALIDATE_CUSTOMER_KEY_IN_PAGE], {
-          queryParams: {
-            idCardNo: this.identity
-          }
-        });
-      })
-      .then(() => {
         this.pageLoadingService.closeLoading();
+      } else {
+        const transactionObject: any = this.buildTransaction(this.transaction.data.transactionType);
+        this.http.post(`/api/salesportal/device-order/create-transaction`, transactionObject)
+          .toPromise().then((response: any) => {
+            this.pageLoadingService.closeLoading();
+            if (response.data.isSuccess) {
+              this.transaction = transactionObject;
+              this.router.navigate([ROUTE_DEVICE_ORDER_AIS_SHARE_PLAN_NEW_REGISTER_MNP_PAYMENT_DETAIL_PAGE]);
+            } else {
+              throw new Error('ระบบไม่สามารถแสดงข้อมูลได้ในขณะนี้');
+            }
+          }).catch((error: any) => {
+            this.pageLoadingService.closeLoading();
+            throw new Error(error);
+          });
+      }
+    });
+  }
+
+  validateCustomer(): any {
+    return this.http.get(`/api/customerportal/newRegister/${this.identity}/queryCustomerInfo`)
+      .toPromise().then((customerInfo: any) => {
+        const transactionType: string = 'NewRegisterAIS';
+        const cardType: string = this.mapCardType(customerInfo.idCardType);
+        // tslint:disable-next-line: max-line-length
+        return this.http.get(`/api/customerportal/validate-customer-new-register?identity=${this.identity}&idCardType=${cardType}&transactionType=${transactionType}`)
+          .toPromise().then((customer: any) => {
+            return this.http.get(`/api/customerportal/newRegister/${this.identity}/queryBillingAccount`).toPromise()
+              .then(() => {
+                return this.http.get(`/api/customerportal/currentDate`)
+                  .toPromise().then((sysdate: any) => {
+                    if (sysdate) {
+                      const isLowerAge: boolean = this.isLowerAge(customer.data.birthdate, sysdate);
+                      if (!isLowerAge) {
+                        this.alertService.error('อายุไม่ถึง 17 ปี');
+                        throw new Error('อายุไม่ถึง 17 ปี');
+                      } else {
+                        const body: any = this.getRequestAddDeviceSellingCart({ customer: customer });
+                        return this.http.post(`/api/salesportal/add-device-selling-cart`, body)
+                          .toPromise().then((order: any) => {
+                            return {
+                              order,
+                              customer,
+                              customerInfo,
+                            };
+                          });
+                      }
+                    }
+                  });
+              });
+          });
       });
+  }
+
+  toBillingInformation(): any {
+    this.http.get('/api/customerportal/validate-customer-new-register', {
+      params: {
+        identity: this.identity
+      }
+    })
+      .toPromise().then((resp: any) => {
+        const data: any = resp.data || {};
+        return Promise.resolve(data);
+      })
+      .then((customer) => {
+        this.transaction.data.customer = customer;
+        return this.http.get(`/api/customerportal/newRegister/${this.identity}/queryBillingAccount`)
+          .toPromise().then((resp: any) => {
+            const data: any = resp.data || {};
+            return this.http.post('/api/customerportal/verify/billingNetExtreme', {
+              businessType: '1',
+              listBillingAccount: data.billingAccountList
+            })
+              .toPromise().then((respBillingNetExtreme: any) => {
+                return {
+                  billCycles: data.billingAccountList,
+                  billCyclesNetExtreme: respBillingNetExtreme.data
+                };
+              })
+              .catch(() => {
+                return { billCycles: data.billingAccountList };
+              });
+          });
+      })
+      .then((billingInformation: any) => {
+        this.transaction.data.billingInformation = billingInformation;
+      });
+  }
+
+  getRequestAddDeviceSellingCart(bodyRequest: any): any {
+    const productInfo: any = JSON.parse(localStorage.getItem('productInfo'));
+    const productDetail: any = JSON.parse(localStorage.getItem('productDetail'));
+    const user: any = this.tokenService.getUser();
+    try {
+      const customer: any = bodyRequest.customer.data;
+      const trade: any = this.priceOption.trade;
+      const locationCode: any = user.locationCode;
+      return {
+        soCompany: productInfo.company || 'AWN',
+        locationSource: locationCode,
+        locationReceipt: locationCode,
+        productType: productDetail.productType || 'DEVICE',
+        productSubType: productDetail.productSubType || 'HANDSET',
+        brand: productDetail.brand,
+        model: productDetail.model,
+        color: productInfo.colorName,
+        priceIncAmt: trade.normalPrice,
+        priceDiscountAmt: trade.discount.amount,
+        grandTotalAmt: '',
+        userId: user.username,
+        cusNameOrder: `${customer.firstName || ''} ${customer.lastName || ''}`.trim(),
+        preBookingNo: '',
+        depositAmt: '',
+        reserveNo: ''
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public isLowerAge(birthdate: string, currentDate?: Date): boolean {
+    const b: moment.Moment = moment(birthdate, 'DD/MM/YYYY');
+    const c: moment.Moment = moment(currentDate).add(543, 'years');
+    if (b.isValid()) {
+      const age: any = c.diff(b, 'years');
+      const isLegal: any = (age >= 17);
+      return isLegal;
+    } else {
+      throw new Error('กรอกวันเกิดไม่ถูกต้อง กรุณากรอกใหม่อีกครั้ง');
+    }
   }
 
   onHome(): void {
@@ -94,40 +223,93 @@ export class NewRegisterMnpValidateCustomerPageComponent implements OnInit, OnDe
     delete this.transaction.data.customer;
   }
 
+  buildTransaction(transactionType: string): any {
+    const user: any = this.tokenService.getUser();
+    const customer: any = this.transaction.data.customer;
+    const order: any = this.transaction.data.order;
+
+    const transaction: any = {
+      transactionId: this.generateTransactionId(),
+      data: {
+        customer: customer,
+        order: order,
+        status: {
+          code: '001',
+          description: 'pending'
+        },
+        transactionType: transactionType,
+        billingInformation: this.transaction.data.billingInformation
+      },
+      create_date: Date.now(),
+      create_by: user.username,
+      issueBy: user.username
+    };
+    return transaction;
+  }
+
+  public generateTransactionId(): any {
+    let emptyString: string = '';
+    const alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
+    while (emptyString.length < 2) {
+      emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    const randomAlphabet: string = emptyString;
+    const today: any = moment().format('YYYYMMD');
+    const randomNumber: string = Math.floor(Math.random() * 1000000).toString();
+    const transactionId: string = randomAlphabet + today + randomNumber;
+    return transactionId;
+  }
+
   private mapCustomer(customer: any): void {
-    const fullName = (customer.name || ' ').split(' ');
-    const address = customer.address || {};
     this.transaction.data.customer = {
       idCardNo: this.identity,
       idCardType: customer.idCardType,
-      titleName: customer.accntTitle,
-      firstName: fullName[0],
-      lastName: fullName[1],
+      titleName: customer.titleName,
+      firstName: customer.firstName,
+      lastName: customer.lastName,
       birthdate: customer.birthdate,
       gender: customer.gender,
-      homeNo: address.homeNo || address.houseNo,
-      moo: address.moo,
-      mooBan: address.mooban,
-      buildingName: address.buildingName,
-      floor: address.floor,
-      room: address.room,
-      street: address.street || address.streetName,
-      soi: address.soi,
-      tumbol: address.tumbol,
-      amphur: address.amphur,
-      province: address.province || address.provinceName,
+      homeNo: customer.homeNo,
+      moo: customer.moo,
+      mooBan: customer.mooban,
+      buildingName: customer.buildingName,
+      floor: customer.floor,
+      room: customer.room,
+      street: customer.street || '',
+      soi: customer.soi,
+      tumbol: customer.tumbol,
+      amphur: customer.amphur,
+      province: customer.province || customer.provinceName,
       firstNameEn: '',
       lastNameEn: '',
       issueDate: customer.birthdate,
       expireDate: null,
-      zipCode: address.zipCode,
+      zipCode: customer.zipCode,
       mainMobile: customer.mainMobile,
       mainPhone: customer.mainPhone,
       billCycle: customer.billCycle,
-      caNumber: customer.accntNo,
+      caNumber: customer.caNumber,
       imageSignature: '',
       imageSmartCard: '',
       imageReadSmartCard: '',
     };
+  }
+
+  private mapCardType(idCardType: string): string {
+    idCardType = idCardType ? idCardType : 'ID_CARD';
+    const mapCardType: any = {
+      CERT_FOUND: 'หนังสือจัดตั้งสมาคม / มูลนิธิ',
+      EMB_LET: 'หนังสือออกจากสถานทูต',
+      GOV_LET: 'หนังสือออกจากหน่วยราชการ',
+      HILL_CARD: 'บัตรประจำตัวคนบนที่ราบสูง',
+      ID_CARD: 'บัตรประชาชน',
+      IMM_CARD: 'บัตรประจำตัวคนต่างด้าว',
+      MONK_CERT: 'ใบสุทธิพระ',
+      PASSPORT: 'หนังสือเดินทาง',
+      ROY_LET: 'หนังสือออกจากสำนักพระราชวัง',
+      STA_LET: 'หนังสือออกจากรัฐวิสาหกิจ',
+      TAX_ID: 'เลขที่ประจำตัวผู้เสียภาษีอากร'
+    };
+    return mapCardType[idCardType];
   }
 }
