88816c4447ce1c7e6146a13e30edaf6f745ddf6c
Nunthawat Lhasiw <nuntl685@prodg.postbox.in.th>
2019-11-29 11:12:15 +0700

    Edit wording and scan barcode

--- src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-verify-instant-sim-page/new-register-mnp-verify-instant-sim-page.component.ts
+++ src/app/device-order/ais/device-order-ais-share-plan/new-register-mnp/containers/new-register-mnp-verify-instant-sim-page/new-register-mnp-verify-instant-sim-page.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { SimSerial, HomeService, AlertService, PageLoadingService, ShoppingCart, SimSerialComponent, AisNativeService } from 'mychannel-shared-libs';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
+import { SimSerial, HomeService, AlertService, PageLoadingService, ShoppingCart, AisNativeService } from 'mychannel-shared-libs';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { WIZARD_DEVICE_ORDER_AIS_DEVICE_SHARE_PLAN } from 'src/app/device-order/constants/wizard.constant';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
 import { ShoppingCartService } from 'src/app/device-order/services/shopping-cart.service';
-import { environment } from 'src/environments/environment';
 import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
 import {
@@ -14,7 +13,6 @@ import {
 } from '../../constants/route-path.constant';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Transaction } from 'src/app/shared/models/transaction.model';
-
 @Component({
   selector: 'app-new-register-mnp-verify-instant-sim-page',
   templateUrl: './new-register-mnp-verify-instant-sim-page.component.html',
@@ -29,13 +27,6 @@ export class NewRegisterMnpVerifyInstantSimPageComponent implements OnInit, OnDe
   shoppingCart: ShoppingCart;
   translationSubscribe: Subscription;
   keyinSimSerial: boolean;
-  isHandsetDiscount: boolean;
-  isEasyApp: boolean;
-
-  @ViewChild('mcSimSerial')
-  mcSimSerial: SimSerialComponent;
-  serial: any;
-
   @ViewChild('serial')
   serialField: ElementRef;
 
@@ -60,19 +51,11 @@ export class NewRegisterMnpVerifyInstantSimPageComponent implements OnInit, OnDe
     });
   }
 
-  private createForm(): void {
-    this.serialForm = this.fb.group({
-      serial: ['', [Validators.required, Validators.pattern(/\d{13}/)]]
-    });
-  }
-
   public checkSimSerial(): void {
     this.keyinSimSerial = true;
     const serial = this.serialForm.controls['serial'].value;
     this.getMobileNoBySim(serial);
-  }
-
-  private getMobileNoBySim(serial: any): void {
+  }  private getMobileNoBySim(serial: any): void {
     this.pageLoadingService.openLoading();
     this.http.get(`/api/customerportal/validate-verify-instant-sim?serialNo=${serial}`).toPromise()
       .then((resp: any) => {
@@ -101,6 +84,7 @@ export class NewRegisterMnpVerifyInstantSimPageComponent implements OnInit, OnDe
         this.onSubmit();
       });
   }
+
   private onSubmit(): void {
     this.serialForm.patchValue({
       serial: ''
@@ -108,52 +92,20 @@ export class NewRegisterMnpVerifyInstantSimPageComponent implements OnInit, OnDe
     this.serialField.nativeElement.focus();
   }
 
-  onCheckSimSerial(serial: string): void {
-    let validateVerifyInstantSim;
-    if (environment.name === 'LOCAL' && serial === '9999999999999') {
-      validateVerifyInstantSim = Promise.resolve({
-        data: {
-          mobileNo: '0999999999'
-        }
-      });
-    } else {
-      validateVerifyInstantSim = this.http.get(`/api/customerportal/validate-verify-instant-sim?serialNo=${serial}`).toPromise();
-    }
-
-    this.pageLoadingService.openLoading();
-    validateVerifyInstantSim.then((resp: any) => {
-      const simSerial = resp.data || {};
-      this.simSerialValid = true;
-      this.simSerial = {
-        mobileNo: simSerial.mobileNo,
-        simSerial: serial
-      };
-      console.log('this.simSerial |>', this.simSerial);
-
-      this.transaction.data.simCard = {
-        mobileNo: this.simSerial.mobileNo,
-        simSerial: this.simSerial.simSerial,
-        persoSim: false
-      };
-      this.shoppingCart = Object.assign(this.shoppingCart, {
-        mobileNo: simSerial.mobileNo
-      });
-      this.pageLoadingService.closeLoading();
-    }).catch((resp: any) => {
-      this.simSerialValid = false;
-      this.simSerial = undefined;
-      const error = resp.error || [];
-      this.pageLoadingService.closeLoading();
-      this.alertService.notify({
-        type: 'error',
-        html: this.translationService.instant(error.resultDescription.replace(/<br>/, ' '))
-      });
+  private createForm(): void {
+    this.serialForm = this.fb.group({
+      serial: ['', [Validators.required, Validators.pattern(/\d{13}/)]]
     });
   }
 
   onOpenScanBarcode(): void {
     this.aisNativeService.scanBarcode();
     this.keyinSimSerial = false;
+    this.aisNativeService.getBarcode().subscribe((imeiFromBarcode) => {
+      this.serialForm.patchValue({
+        serial: imeiFromBarcode
+      });
+    });
   }
 
   onBack(): void {
