bbb33e895b227e24e2d49c6a80ae491174055a85
Danai Tuwakam <danat529@vanpl.postbox.in.th>
2019-12-23 15:00:45 +0700

    add page aggregate, queue, result

--- src/app/device-order/asp/device-order-asp-share-plan/new-register-mnp/containers/new-register-mnp-result-page/new-register-mnp-result-page.component.ts
+++ src/app/device-order/asp/device-order-asp-share-plan/new-register-mnp/containers/new-register-mnp-result-page/new-register-mnp-result-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
+import { Transaction } from 'src/app/shared/models/transaction.model';
 
 @Component({
   selector: 'app-new-register-mnp-result-page',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-register-mnp-result-page.component.scss']
 })
 export class NewRegisterMnpResultPageComponent implements OnInit {
+  transaction: Transaction;
+  isSuccess: boolean;
+  mobileNo: string;
+  mainSimSerail: string;
+  memberMobileNo: string;
+  memberSimSerail: string;
 
-  constructor() { }
+  constructor(private transactionService: TransactionService) {
+    this.transaction = this.transactionService.load();
+  }
 
   ngOnInit(): void {
+    const mainSim = this.transaction.data.simCard;
+    const memberSim = this.transaction.data.simCard.memberSimCard[0];
+
+    this.isSuccess = true;
+    this.mobileNo = mainSim.mobileNo;
+    this.mainSimSerail = mainSim.simSerial;
+    this.memberMobileNo = memberSim.mobileNo;
+    this.memberSimSerail = memberSim.simSerial;
+  }
+
+  onMainMenu(): void {
+    window.location.href = '/';
   }
 
 }
