e4a68e5686645a46559f19d21b13c63f872bfbfb
matsi masamin <matsim49@aware.postbox.in.th>
2020-01-06 15:19:03 +0700

    [create] module

---
+++ src/app/device-only/shop-premium/device-only-shop-premium.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DeviceOnlyShopPremiumRoutingModule } from './device-only-shop-premium-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DeviceOnlyShopPremiumRoutingModule
+  ],
+  declarations: []
+})
+export class DeviceOnlyShopPremiumModule { }
