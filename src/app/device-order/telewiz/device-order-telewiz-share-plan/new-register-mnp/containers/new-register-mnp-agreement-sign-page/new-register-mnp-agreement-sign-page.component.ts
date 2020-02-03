09c0ed6f20db03b9122596428a317940b902874c
Nunthawat Lhasiw <nuntl685@prodg.postbox.in.th>
2020-02-03 10:31:24 +0700

    Fix water mark position

--- src/app/device-order/telewiz/device-order-telewiz-share-plan/new-register-mnp/containers/new-register-mnp-agreement-sign-page/new-register-mnp-agreement-sign-page.component.ts
+++ src/app/device-order/telewiz/device-order-telewiz-share-plan/new-register-mnp/containers/new-register-mnp-agreement-sign-page/new-register-mnp-agreement-sign-page.component.ts
@@ -422,7 +422,8 @@ export class NewRegisterMnpAgreementSignPageComponent implements OnInit, OnDestr
           const watermarkWidth: number = watermarkHeight * watermarkRatio;
           const dxw = (canvas.width - watermarkWidth) / 2;
           const dyw = (canvas.height - watermarkHeight) / 2;
-          ctx.drawImage(watermark, dxw + 80, dyw + 40, watermarkWidth / 1.5, watermarkHeight / 1.5);
+          // ctx.drawImage(watermark, dxw + 80, dyw + 40, watermarkWidth / 1.5, watermarkHeight / 1.5);
+          ctx.drawImage(watermark, dxw + 230, dyw + 160, watermarkWidth / 2.5, watermarkHeight / 2.5);
         }
       }
     }
