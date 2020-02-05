d50e810795b93e8d797529adedc09d1a0c353e39
MAYA <nj23439@gmail.com>
2020-02-05 09:51:46 +0700

    Edit id capture

--- src/app/omni/omni-new-register/containers/omni-new-register-id-card-capture-page/omni-new-register-id-card-capture-page.component.ts
+++ src/app/omni/omni-new-register/containers/omni-new-register-id-card-capture-page/omni-new-register-id-card-capture-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, OnDestroy, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { WIZARD_OMNI_NEW_REGISTER } from 'src/app/omni/constants/wizard.constant';
 import { Router } from '@angular/router';
-import { HomeService, CaptureAndSign, TokenService, ChannelType, AlertService, Utils } from 'mychannel-shared-libs';
+import { HomeService, CaptureAndSign, TokenService, ChannelType, AlertService, Utils, AisNativeService, CaptureSignedWithCard, AWS_WATERMARK, User } from 'mychannel-shared-libs';
 import {
   ROUTE_OMNI_NEW_REGISTER_CUSTOMER_INFO_PAGE,
   ROUTE_OMNI_NEW_REGISTER_FACE_CAPTURE_PAGE,
@@ -9,6 +9,7 @@ import {
 } from 'src/app/omni/omni-new-register/constants/route-path.constant';
 import { Transaction, Customer, TransactionAction } from 'src/app/omni/omni-shared/models/transaction.model';
 import { TransactionService } from 'src/app/omni/omni-shared/services/transaction.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-omni-new-register-id-card-capture-page',
@@ -18,92 +19,256 @@ import { TransactionService } from 'src/app/omni/omni-shared/services/transactio
 export class OmniNewRegisterIdCardCapturePageComponent implements OnInit, OnDestroy {
 
   wizards: string[] = WIZARD_OMNI_NEW_REGISTER;
+  @ViewChild('signImage') signImage: ElementRef;
+  camera: EventEmitter<void> = new EventEmitter<void>();
+  user: User;
+  customer: Customer;
   transaction: Transaction;
   captureAndSign: CaptureAndSign;
-  apiSigned: string;
-  idCardValid: boolean;
-  openCamera: boolean = true;
-  camera: EventEmitter<void> = new EventEmitter<void>();
+  isDrawImageIdCard: boolean;
+  onChangeValid: boolean = false;
+  isNextValid: boolean;
+  // Signature
+  signedSubscription: Subscription;
+  signedWidthIdCardImageSubscription: Subscription;
+  commandSigned: any;
+  apiSigned: 'SignaturePad' | 'OnscreenSignpad';
+  isDrawingSignature: boolean = false;
+  imageSigned: boolean;
+  watermark: string = AWS_WATERMARK;
 
   constructor(
     private router: Router,
-    private homeService: HomeService,
     private transactionService: TransactionService,
     private tokenService: TokenService,
     private alertService: AlertService,
-    private utils: Utils,
+    private aisNativeDeviceService: AisNativeService,
+    private utils: Utils
   ) {
     this.transaction = this.transactionService.load();
-
-    if (this.tokenService.getUser().channelType === ChannelType.SMART_ORDER) {
-      this.apiSigned = 'OnscreenSignpad';
-    } else {
-      this.apiSigned = 'SignaturePad';
-    }
+    this.user = this.tokenService.getUser();
   }
 
   ngOnInit(): void {
+    this.getImageSignature();
+    this.getCaptureAndSign();
+    this.setShowImageFromTransaction();
+  }
+
+  getImageSignature(): void {
+    this.signedSubscription = this.aisNativeDeviceService.getSigned()
+      .subscribe((signature: string) => {
+        this.captureAndSign.imageSignature = signature;
+        this.onCheckCaptureAndSign();
+      });
+    if (this.isAisNative()) {
+      this.signedWidthIdCardImageSubscription = this.aisNativeDeviceService.getCaptureSignatureWithCardImage()
+        .subscribe((signatureWithCard: CaptureSignedWithCard) => {
+          this.captureAndSign.imageSignature = signatureWithCard.signature;
+          this.onCheckCaptureAndSign();
+        });
+    }
+  }
+
+  setShowImageFromTransaction(): void {
+    const customer: Customer = this.transaction.data.customer;
+    if (customer && customer.imageSignature && customer.imageSmartCard) {
+      this.onCheckCaptureAndSign();
+    }
+  }
+
+  getCaptureAndSign(): void {
     const customer: Customer = this.transaction.data.customer;
     this.captureAndSign = {
       allowCapture: true,
       imageSmartCard: customer.imageSmartCard,
-      imageSignature: customer.imageSignatureSmartCard
+      imageSignature: customer.imageSignature,
+      imageSignatureWidthCard: null
     };
   }
 
-  onCompleted(captureAndSign: CaptureAndSign): void {
-    // this.transaction.data.customer.imageSmartCard = captureAndSign.imageSmartCard;
-  const customer: Customer = this.transaction.data.customer;
-    customer.imageSignatureSmartCard = captureAndSign.imageSignature;
-    customer.imageSmartCard = captureAndSign.imageSmartCard;
-  }
-
-  onError(valid: boolean): void {
-    this.idCardValid = valid;
-  }
-
-  onBack(): void {
-    this.onClearIdCardImage();
-    // tslint:disable-next-line: max-line-length
-    if (this.transaction.data.customer.caNumber && this.transaction.data.customer.caNumber !== '' && this.transaction.data.action === TransactionAction.READ_CARD) {
-        this.router.navigate([ROUTE_OMNI_NEW_REGISTER_CUSTOMER_INFO_PAGE]);
-    // tslint:disable-next-line: max-line-length
-    } else if (this.transaction.data.customer.caNumber && this.transaction.data.customer.caNumber === '' && this.transaction.data.action === TransactionAction.READ_CARD) {
-      // this.router.navigate([ROUTE_OMNI_NEW_REGISTER_CUSTOMER_INFO_PAGE]); route to mc หน้ารับสินค้า
-  } else {
-      this.router.navigate([ROUTE_OMNI_NEW_REGISTER_VALIDATE_CUSTOMER_KEY_IN_PAGE]);
-    }
-  }
-
-  onNext(): void {
-    this.router.navigate([ROUTE_OMNI_NEW_REGISTER_FACE_CAPTURE_PAGE]);
-  }
-
-  onHome(): void {
-    this.homeService.goToHome();
-  }
-
-  onCameraCompleted(image: string): void {
-    this.transaction.data.customer.imageSmartCard = image;
-  }
-
-  onCameraError(error: string): void {
-    this.alertService.error(error);
-  }
-
-  onClearIdCardImage(): void {
-    this.transaction.data.customer.imageSmartCard = null;
+  isAllowCapture(): boolean {
+    return this.captureAndSign.allowCapture;
   }
 
   isAisNative(): boolean {
     return this.utils.isAisNative();
   }
 
-  onOpenCamera(): void {
-    this.openCamera = true;
+  hasImageSmartCard(): boolean {
+    return !!this.captureAndSign.imageSmartCard;
+  }
+
+  hasImageSignature(): boolean {
+    return !!this.captureAndSign.imageSignature;
+  }
+
+  onCameraCompleted(image: string): void {
+    this.captureAndSign.imageSmartCard = image;     // ได้รูปถ่ายบัตรปชช.
+    this.createCanvas();
+  }
+
+  onCameraError(error: string): void {
+    this.alertService.error(error);
+    this.imageSigned = false;
+  }
+
+  createCanvas(): void {
+    const imageCard = new Image();          // สร้าง imageCard เพื่อหา src ของรูปถ่ายบัตรปชช.
+    const signImage = new Image();          // สร้าง imageCard เพื่อหา src ของรูปลายเซ็น
+    const watermarkImage = new Image();     // สร้าง imageCard เพื่อหา src ของรูปลายน้ำ
+
+    // รูปถ่ายบัตรปชช, รูปลายเซ็น, รูปลายน้ำ
+    imageCard.src = (this.captureAndSign.imageSmartCard) ? 'data:image/png;base64,' + this.captureAndSign.imageSmartCard : '';
+    signImage.src = (this.captureAndSign.imageSignature) ? 'data:image/png;base64,' + this.captureAndSign.imageSignature : '';
+    watermarkImage.src = 'data:image/png;base64,' + AWS_WATERMARK;
+
+    // โหลดรูปภาพบัตรปชช.ที่ได้จากการวาด
+    imageCard.onload = () => {
+      this.drawIdCardWithSign(imageCard, signImage);
+    };
+    // โหลดรูปภาพเซ็นลายเซ็นที่ได้จากการวาด
+    signImage.onload = () => {
+      this.drawIdCardWithSign(imageCard, signImage);
+    };
+  }
+
+  drawIdCardWithSign(imageCard: any, signImage: any): void {
+    // สร้างตัวแปรเก็บชนิดรูปจากบัตรปชช./รูปลายเซ็นเป็น Canvas เพื่อให้เรียกใช้อันเดียวกับแท็กบน html
+    // สร้างตัวแปรเก็บตำแหน่ง แกน x, y ของรูปบัตรปชช./รูปลายเซ็น
+    const canvas: HTMLCanvasElement = (<HTMLCanvasElement>this.signImage.nativeElement);
+    const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+    this.isDrawImageIdCard = false;   // เช็คว่ามีการวาดรูปถ่ายบัตรปชช.หรือยัง
+
+    // ลบส่วนเกินของรูปภาพออกและทำให้ภาพเป็นพื้นหลังโปร่งใส
+    if (ctx) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    // หารูปถ่ายบัตรปชช.
+    if (new RegExp('data:image/png;base64,').test(imageCard.src)) {
+      canvas.width = imageCard.width;
+      canvas.height = imageCard.height;
+      this.isDrawImageIdCard = true;
+      ctx.drawImage(imageCard, 0, 0);
+    }
+
+    // เช็คว่าเซ็นลายเซ็นหรือยัง
+    if (this.imageSigned) {
+      // หารูปเซ็นลายเซ็น
+      if (new RegExp('data:image/png;base64,').test(signImage.src)) {
+        if (!this.isDrawImageIdCard) {
+          canvas.width = signImage.width;
+          canvas.height = signImage.height;
+        }
+
+        // ปรับตำแหน่งให้รูปลายเซ็นอยู่ตรงกลาง
+        const signImageRatio = (signImage.width / signImage.height) / 2;
+        const signImageHeight = signImage.height > canvas.height ? canvas.height : signImage.height;
+        const signImageWidth = signImageHeight * signImageRatio;
+
+        // หาจุดเริ่มต้นของภาพลายเซ็นที่จะวาด
+        const dxs = ((canvas.width - signImageWidth) / 2);
+        const dys = ((canvas.height - signImageHeight) / 2) / 6;
+
+        ctx.globalCompositeOperation = 'multiply';                            // ทำให้รูปลายเซ็นทับรูปถ่ายบัตรปชช.
+        ctx.drawImage(signImage, dxs, dys, signImageWidth, signImageHeight);  // เริ่มวาดรูปตั้งแต่จุดเริ่มต้นของภาพ
+      }
+    }
+    // ได้รูปภาพทีมีรูปถ่ายจากบัตรปชช.พร้อมลายเซ็นที่ทับกัน
+    this.captureAndSign.imageSignatureWidthCard = canvas.toDataURL('image/jpeg').replace(/^data:image\/jpeg;base64,/, '');
+    this.isDrawingSignature = true;
+  }
+
+  // เช็คว่าถ้ากดถ่ายรูปใหม่ จะต้องได้รูปถ่ายบัตรปชช. และรูปลายเซ็น
+  onCheckCaptureAndSign(): void {
+    if (this.isAisNative() && !this.hasImageSmartCard()) {
+      this.camera.next();
+    }
+
+    if ((this.isAllowCapture()) && (this.captureAndSign.imageSmartCard && this.captureAndSign.imageSignature)) {
+      this.createCanvas();
+      this.onChangeValid = true;
+      this.imageSigned = true;
+      this.isNextValid = true;
+    } else {
+      this.onChangeValid = false;
+    }
+  }
+
+  // เคลียร์รูปภาพตอนกดถ่ายรูปใหม่
+  onClearImage(): void {
+    this.captureAndSign.imageSmartCard = null;
+    this.captureAndSign.imageSignatureWidthCard = null;
+    this.captureAndSign.imageSignature = null;
+    this.isNextValid = false;
+    this.imageSigned = false;
+    this.clearCanvas();
+  }
+
+  // เคลียร์รูปภาพ(สร้าง canvas) ตอนกดถ่ายรูปใหม่
+  clearCanvas(): void {
+    const canvas: HTMLCanvasElement = (<HTMLCanvasElement>this.signImage.nativeElement);
+    const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.restore();
+  }
+
+  // get library signpad
+  onSigned(): void {
+    this.imageSigned = false;
+    this.user = this.tokenService.getUser();
+
+    // เรียก library signed ของ Native/PC
+    if (this.isAisNative()) {
+      this.aisNativeDeviceService.captureSignatureWithCardImage(null);
+      return;
+    } else {
+      this.apiSigned = ChannelType.SMART_ORDER === this.user.channelType ? 'OnscreenSignpad' : 'SignaturePad';
+      this.aisNativeDeviceService.openSigned(this.apiSigned).subscribe((command: any) => {
+        this.commandSigned = command;
+        if (command.error) {
+          return;
+        }
+      });
+    }
+  }
+
+  onClearIdCardImage(): void {
+    this.transaction.data.customer.imageSmartCard = null;
+  }
+
+  onBack(): void {
+    this.onClearIdCardImage();
+    // tslint:disable-next-line: max-line-length
+    if (this.transaction.data.customer.caNumber && this.transaction.data.customer.caNumber !== '' && this.transaction.data.action === TransactionAction.READ_CARD) {
+      this.router.navigate([ROUTE_OMNI_NEW_REGISTER_CUSTOMER_INFO_PAGE]);
+      // tslint:disable-next-line: max-line-length
+    } else if (this.transaction.data.customer.caNumber && this.transaction.data.customer.caNumber === '' && this.transaction.data.action === TransactionAction.READ_CARD) {
+      // this.router.navigate([ROUTE_OMNI_NEW_REGISTER_CUSTOMER_INFO_PAGE]); route to mc หน้ารับสินค้า
+    } else {
+      this.router.navigate([ROUTE_OMNI_NEW_REGISTER_VALIDATE_CUSTOMER_KEY_IN_PAGE]);
+    }
+  }
+
+  onNext(): void {
+    this.setImageSignature();
+    this.router.navigate([ROUTE_OMNI_NEW_REGISTER_FACE_CAPTURE_PAGE]);
+  }
+
+  setImageSignature(): void {
+    const customer: Customer = this.transaction.data.customer;
+    customer.imageSignature = this.captureAndSign.imageSignature;
+    customer.imageSmartCard = this.captureAndSign.imageSmartCard;
+    customer.imageSignatureSmartCard = this.captureAndSign.imageSignatureWidthCard;
   }
 
   ngOnDestroy(): void {
     this.transactionService.update(this.transaction);
+    this.signedSubscription.unsubscribe();
+    if (this.commandSigned) {
+      this.commandSigned.ws.send('CaptureImage');
+    }
   }
+
 }
