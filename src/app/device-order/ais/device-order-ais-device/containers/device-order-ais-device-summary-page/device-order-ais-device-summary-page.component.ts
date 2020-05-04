a16baf043484d33d2ba5b8d2823c5083b0ef8026
Matsi <matsimas@ais.co.th>
2020-05-04 20:59:05 +0700

    default asc code

--- src/app/device-order/ais/device-order-ais-device/containers/device-order-ais-device-summary-page/device-order-ais-device-summary-page.component.ts
+++ src/app/device-order/ais/device-order-ais-device/containers/device-order-ais-device-summary-page/device-order-ais-device-summary-page.component.ts
@@ -138,7 +138,6 @@ export class DeviceOrderAisDeviceSummaryPageComponent implements OnInit, OnDestr
           if (emResponse && emResponse.data) {
             const emId = emResponse.data.pin;
             this.sellerCode = emId;
-            this.employeeDetailForm.patchValue({ ascCode: this.sellerCode });
           }
           this.pageLoadingService.closeLoading();
         }).catch(() => {
@@ -152,6 +151,17 @@ export class DeviceOrderAisDeviceSummaryPageComponent implements OnInit, OnDestr
   createEmployeeForm(): void {
     this.employeeDetailForm = this.fb.group({
       ascCode: ['', Validators.compose([Validators.required, Validators.pattern(/^[0-9]+$/)])]
+    });
+
+    const seller =  this.transaction.data.seller && this.transaction.data.seller.sellerNo ? this.transaction.data.seller.sellerNo : '';
+    if (this.sellerCode || seller) {
+      this.employeeDetailForm.patchValue({ ascCode: this.sellerCode || seller });
+    }
+
+    this.employeeDetailForm.valueChanges.subscribe((value) => {
+      if (value.ascCode) {
+        this.sellerCode = value.ascCode;
+      }
     });
   }
 
